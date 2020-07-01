7cd503d4bb85473ce4758ecbb4a3dfe276345677
Chris Colvard <chris.colvard@gmail.com>
2020-07-01 16:17:08 -0400

    Prepend our view paths and automount this engine's routes All that should be needed is to add this engine to Hyku's Gemfile

--- lib/hyku_addons/engine.rb
+++ lib/hyku_addons/engine.rb
@@ -1,5 +1,18 @@
 module HykuAddons
   class Engine < ::Rails::Engine
     isolate_namespace HykuAddons
+
+    # Automount this engine
+    # Only do this because this is just for us and we don't need to allow control over the mount to the application
+    initializer 'hyku_additions.routes' do |app|
+      app.routes.append do
+        mount HykuAddons::Engine, at: '/'
+      end
+    end
+
+    config.after_initialize do
+      # Prepend our views so they have precedence
+      ActionController::Base.prepend_view_path(paths['app/views'].existent)
+    end
   end
 end
