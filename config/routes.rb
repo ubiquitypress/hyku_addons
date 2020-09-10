a38a2dea3f0f052b05b953036085886ba96cfac6
Chris Colvard <chris.colvard@gmail.com>
2020-09-10 15:31:59 -0400

    Remove example route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 HykuAddons::Engine.routes.draw do
-  post '/external_services/generate_doi', to: '/ubiquity/external_services#generate_doi', as: 'external_services_generate_doi'
 end
