3c8459ddfb0c2f20e67099ce94eeef299a888a7f
Chris Colvard <chris.colvard@gmail.com>
2020-09-10 10:17:09 -0400

    Create examples branch and remove examples in preparation of starting work

--- spec/routing/ubiquity/external_services_routing_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-RSpec.describe Ubiquity::ExternalServicesController, type: :routing do
-  routes { HykuAddons::Engine.routes }
-  it 'works' do
-    expect(post: "/external_services/generate_doi").to route_to("ubiquity/external_services#generate_doi")
-  end
-end
