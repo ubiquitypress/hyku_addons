89755238afcb2eff69715e342f0403c1aca1222f
edward <bauchiroad@gmail.com>
2020-10-09 14:47:25 +0100

    Updated rspec request test and added more rspec system test. Also deleted unused files

--- spec/requests/hyku_addons/account_settings_request_spec.rb
+++ spec/requests/hyku_addons/account_settings_request_spec.rb
@@ -24,19 +24,31 @@ RSpec.describe "::HykuAddons::AccountSettingsController", type: :request do
   end
 
   describe "Update single keys in settings hash" do
-    it "can update a single settings key eg  contact_email" do
-      patch update_single_admin_account_setting_url(id: account, partial_name: 'contact_email'), params: { "account" => { "settings" => { "contact_email" => "ab@abc.com" } } }
-      expect(response).to redirect_to admin_account_settings_url
-      follow_redirect!
+    context "can display edit page" do
+      it "GET edit page" do
+        get edit_admin_account_setting_url(id: account, partial_name: 'contact_email')
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "can update a single key" do
+      it "can update a single settings key eg  contact_email" do
+        patch update_single_admin_account_setting_url(id: account), params: { "account" => { "settings" => { "contact_email" => "bo@abc.com" } } }
+        account.reload
+        expect(account.settings['contact_email']).to eq 'bo@abc.com'
+        expect(response).to redirect_to admin_account_settings_url
+      end
     end
   end
 
   describe "Update all or multiple settings keys" do
     context "update fields" do
       it "can update multiple fields eg contact_email and wekkly_email_list" do
-        put admin_account_setting_url(account.id), params: { 'account' => { "settings" => { "contact_email" => "ab@abc.com", "weekly_email_list" => ['we@we.com'] } } }
+        put admin_account_setting_url(account.id), params: { 'account' => { "settings" => { "contact_email" => "ta@abc.com", "weekly_email_list" => ['we@we.com'] } } }
+        account.reload
+        expect(account.settings['contact_email']).to eq 'ta@abc.com'
+        expect(account.settings['weekly_email_list']).to include('we@we.com')
         expect(response).to redirect_to admin_account_settings_url
-        follow_redirect!
       end
     end
   end
--- spec/system/hyku_addons/hyku_addons_account_settings_spec.rb
+++ spec/system/hyku_addons/hyku_addons_account_settings_spec.rb
@@ -19,9 +19,56 @@ RSpec.describe 'AccountSettings', type: :system do
     it 'can display various edit links' do
       visit hyku_addons.admin_account_settings_url
       expect(page).to have_selector('h1')
-      expect(page).to have_content('Account Settings')
       expect(page).to have_selector 'h1', visible: true, text: /Account Settings/
       expect(page).to have_link('Edit email')
+    end
+  end
+
+  describe 'Single settings key' do
+    context "Display in-place edit form for single key" do
+      it 'Display in-place edit' do
+        visit hyku_addons.edit_admin_account_setting_url(id: account.id, partial_name: 'contact_email')
+        expect(page).to have_content('email')
+        expect(find_field('account[settings][contact_email]').value).to eq account.settings['contact_email']
+        expect(page).to have_link('Back')
+        expect(page).to have_selector(:link_or_button, 'Save changes')
+      end
+    end
+
+    context "Update single settings key" do
+      it 'save contact_email' do
+        visit hyku_addons.edit_admin_account_setting_url(id: account.id, partial_name: 'contact_email')
+        expect(find_field('account[settings][contact_email]').value).to eq account.settings['contact_email']
+        find_field('account[settings][contact_email]').set 'do@do.com'
+        click_button 'Save changes'
+        account.reload
+        expect(account.settings['contact_email']).to eq('do@do.com')
+      end
+    end
+  end
+
+  describe "Multiple settings Edit" do
+    context "Display page for all settings" do
+      it "Can visit page to edit multiple setting keys" do
+        visit hyku_addons.admin_account_settings_url
+        click_on 'Edit all fields'
+        expect(page).to have_content('Editing Account Settings')
+        expect(page).to have_link('Back')
+        expect(page).to have_selector(:link_or_button, 'Save changes')
+      end
+    end
+
+    context "Update multiple settings keys" do
+      it "can save multiple settings keys" do
+        visit hyku_addons.admin_account_settings_url
+        click_on 'Edit all fields'
+        find_field('account[settings][contact_email]').set 'do@do.com'
+        find_field('account[settings][weekly_email_list][]').set 'wema@we.yahoo.com;ba@bl.uk'
+        click_button 'Save changes'
+        account.reload
+        expect(account.settings['contact_email']).to eq('do@do.com')
+        expect(account.settings['weekly_email_list']).to eq ['wema@we.yahoo.com;ba@bl.uk']
+      end
     end
   end
 end
