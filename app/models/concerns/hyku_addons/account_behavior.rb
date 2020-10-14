59bcafbfd9ee59f4efc64bfffc06fe08d19e27f4
edward <bauchiroad@gmail.com>
2020-10-14 17:13:50 +0100

    fix rubocop errors

--- app/models/concerns/hyku_addons/account_behavior.rb
+++ app/models/concerns/hyku_addons/account_behavior.rb
@@ -10,22 +10,15 @@ module HykuAddons
 
       store_accessor :data, :is_parent
       store_accessor :settings, :contact_email, :weekly_email_list, :monthly_email_list, :yearly_email_list,
-                     :index_record_to_shared_search, :google_scholarly_work_types, 
+                     :index_record_to_shared_search, :google_scholarly_work_types,
                      :live, :enabled_doi, :gtm_id, :demo_gtm_id, :turn_off_fedora_collection_work_association,
                      :add_collection_list_form_display, :hide_form_relationship_tab, :shared_login,
-                     :work_type_list,  :email_hint_text, :email_format, :help_texts, :work_unwanted_fields,
+                     :work_type_list, :email_hint_text, :email_format, :help_texts, :work_unwanted_fields,
                      :required_json_property, :creator_fields, :contributor_fields, :metadata_labels,
-                     :institutional_relationship_picklist, :institutional_relationship, :contributor_roles, 
+                     :institutional_relationship_picklist, :institutional_relationship, :contributor_roles,
                      :creator_roles, :html_required, :licence_list, :sign_up_link, :allow_signup, :redirect_on
-   
 
       accepts_nested_attributes_for :datacite_endpoint, update_only: true
-
-      [].each  do |method_name|
-        define_method "#{method_name}"  do 
-          self.settings.dig('tenant_env', method_name)
-        end 
-      end 
     end
 
     def datacite_endpoint
