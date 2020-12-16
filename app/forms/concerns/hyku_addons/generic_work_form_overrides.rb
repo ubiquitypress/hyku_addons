e5fe1fb5fc20055cb43f51cf16b39cfde0106342
Chris Colvard <chris.colvard@gmail.com>
2020-12-16 17:09:48 -0500

    Remove blank json fields from posted params and remove _position fields

--- app/forms/concerns/hyku_addons/generic_work_form_overrides.rb
+++ app/forms/concerns/hyku_addons/generic_work_form_overrides.rb
@@ -19,17 +19,17 @@ module HykuAddons
       def build_permitted_params
         super.tap do |permitted_params|
           permitted_params << { creator: [:creator_organization_name, :creator_given_name,
-                                          :creator_family_name, :creator_name_type, :creator_orcid, :creator_isni,  :creator_ror, :creator_grid,
-                                          :creator_wikidata, :creator_position, creator_institutional_relationship: []] }
+                                          :creator_family_name, :creator_name_type, :creator_orcid, :creator_isni, :creator_ror, :creator_grid,
+                                          :creator_wikidata, creator_institutional_relationship: []] }
           permitted_params << { contributor: [:contributor_organization_name, :contributor_given_name,
                                               :contributor_family_name, :contributor_name_type, :contributor_orcid, :contributor_isni, :contributor_ror, :contributor_grid,
-                                              :contributor_wikidata, :contributor_position, :contributor_type, contributor_institutional_relationship: []] }
+                                              :contributor_wikidata, :contributor_type, contributor_institutional_relationship: []] }
           permitted_params << { date_published: [:date_published_year, :date_published_month, :date_published_day] }
-          permitted_params << { funder: [:funder_name, :funder_doi, :funder_position, :funder_isni, :funder_ror, funder_award: []] }
+          permitted_params << { funder: [:funder_name, :funder_doi, :funder_isni, :funder_ror, funder_award: []] }
           permitted_params << { event_date: [:event_date_year, :event_date_month, :event_date_day] }
           permitted_params << { editor: [:editor_given_name,
                                          :editor_family_name, :editor_name_type, :editor_orcid, :editor_isni,
-                                         :editor_position, :editor_organization_name, editor_institutional_relationship: []] }
+                                         :editor_organization_name, editor_institutional_relationship: []] }
           permitted_params << { current_he_institution: [:current_he_institution_name, :current_he_institution_isni, :current_he_institution_ror] }
           permitted_params << { date_accepted: [:date_accepted_year, :date_accepted_month, :date_accepted_day] }
           permitted_params << { date_submitted: [:date_submitted_year, :date_submitted_month, :date_submitted_day] }
