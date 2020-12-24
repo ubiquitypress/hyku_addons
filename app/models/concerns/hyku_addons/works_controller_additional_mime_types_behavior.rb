f78805367a9ef4f4dd9511bab2b9344ec73afb48
Paul Whitehead <prwhitehead@gmail.com>
2020-12-24 13:04:42 +0000

    Add controller RIS response format

---
+++ app/models/concerns/hyku_addons/works_controller_additional_mime_types_behavior.rb
@@ -0,0 +1,11 @@
+module HykuAddons
+  module WorksControllerAdditionalMimeTypesBehavior
+    def additional_response_formats(format)
+      format.ris do
+        render body: presenter.solr_document.export_as_ris, content_type: "application/x-research-info-systems"
+      end
+
+      super
+    end
+  end
+end
