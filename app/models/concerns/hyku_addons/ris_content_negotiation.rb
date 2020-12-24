c2cb10096a92f3c927d4264e04ff9bdd67a177f3
Paul Whitehead <prwhitehead@gmail.com>
2020-12-24 13:19:47 +0000

    Exporting as RIS from .ris format working

--- app/models/concerns/hyku_addons/ris_content_negotiation.rb
+++ app/models/concerns/hyku_addons/ris_content_negotiation.rb
@@ -5,30 +5,7 @@ module HykuAddons
     end
 
     def export_as_ris
-      json = Hyrax::GenericWorkPresenter::DELEGATED_METHODS.collect do |m|
-        [m, p.send(m)]
-      end.to_h.merge('has_model' => p.model.model_name).to_json
-
-      m = Bolognese::Metadata.new(input: json, from: 'ubiquity_generic_work')
-      m.ris
-    end
-
-    # def export_as_ttl
-    #   clean_graph.dump(:ttl)
-    # end
-
-    private
-
-    def clean_graph
-      @clean_graph ||= clean_graph_repository.find(id)
-    end
-
-    def clean_graph_repository
-      CleanGraphRepository.new(connection)
-    end
-
-    def connection
-      ActiveFedora.fedora.clean_connection
+      Hyrax::GenericWorkPresenter.new(self, nil, nil).export_as_ris
     end
   end
 end
