5da22b7c6a5559a23aad92c6684fe297653788f9
Paul Whitehead <prwhitehead@gmail.com>
2020-12-29 17:11:40 +0000

    Sort out the weird indentation - again

--- spec/services/bolognese/readers/ubiquity_generic_work_reader_spec.rb
+++ spec/services/bolognese/readers/ubiquity_generic_work_reader_spec.rb
@@ -9,14 +9,9 @@ RSpec.describe Bolognese::Readers::UbiquityGenericWorkReader do
   let(:publisher) { 'Schildts' }
   let(:description) { 'Swedish comic about the adventures of the residents of Moominvalley.' }
   let(:keyword) { 'Lighthouses' }
-	let(:created_date) { 1945 }
+  let(:created_date) { 1945 }
 
-  let(:model_class) do
-		Class.new(GenericWork) do
-			#include HykuAddons::GenericWorkPresenterBehavior
-		end
-	end
-
+  let(:model_class) { Class.new(GenericWork) }
   let(:work) { model_class.new(attributes) }
 
   let(:attributes) do
@@ -29,33 +24,33 @@ RSpec.describe Bolognese::Readers::UbiquityGenericWorkReader do
       publisher: [publisher],
       description: [description],
       keyword: [keyword],
-			date_created: [created_date]
+      date_created: [created_date]
     }
   end
 
-	let(:metadata_class) do
-		Class.new(Bolognese::Metadata) do
-			include Bolognese::Readers::UbiquityGenericWorkReader
-		end
-	end
+  let(:metadata_class) do
+    Class.new(Bolognese::Metadata) do
+      include Bolognese::Readers::UbiquityGenericWorkReader
+    end
+  end
 
-	let(:input) { work.attributes.merge(has_model: work.has_model.first).to_json }
-	let(:metadata) { metadata_class.new(input: input, from: "ubiquity_generic_work") }
+  let(:input) { work.attributes.merge(has_model: work.has_model.first).to_json }
+  let(:metadata) { metadata_class.new(input: input, from: "ubiquity_generic_work") }
 
-	it "reads a GenericWork" do
-		expect(metadata).to be_a(Bolognese::Metadata)
-	end
+  it "reads a GenericWork" do
+    expect(metadata).to be_a(Bolognese::Metadata)
+  end
 
-	describe "#read_ubiquity_generic_work" do
-		it "responds to the method" do
-			expect(metadata).to respond_to(:read_ubiquity_generic_work)
-		end
-	end
+  describe "#read_ubiquity_generic_work" do
+    it "responds to the method" do
+      expect(metadata).to respond_to(:read_ubiquity_generic_work)
+    end
+  end
 
-	context "crosswalks" do
-		let(:data) { metadata.ris.split("\r\n") }
+  context "crosswalks" do
+    let(:data) { metadata.ris.split("\r\n") }
 
-		it "correctly populates the export" do
+    it "correctly populates the export" do
       expect(data[0]).to eq("T1  - #{title}")
       expect(data[1]).to eq("DO  - https://doi.org/#{doi}")
       expect(data[2]).to eq("AB  - #{description}")
@@ -64,6 +59,6 @@ RSpec.describe Bolognese::Readers::UbiquityGenericWorkReader do
       expect(data[5]).to eq("PB  - #{publisher}")
       expect(data[6]).to eq("ER  - ")
     end
-	end
+  end
 end
 
