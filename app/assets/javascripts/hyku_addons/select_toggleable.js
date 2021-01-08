a45f93db2e9a14d4f155654806f9cb108a1afca8
Paul Whitehead <prwhitehead@gmail.com>
2021-01-08 13:52:17 +0000

    Get toggle working on load

--- app/assets/javascripts/hyku_addons/select_toggleable.js
+++ app/assets/javascripts/hyku_addons/select_toggleable.js
@@ -4,31 +4,48 @@ class Eventable {
   }
 
   registerEvents(){
-    console.log("registerEvents")
-    $("body").on("change", `[data-on-event=change]`, function(){
-      let eventName = $(this).data("send-event")
+    console.log("Eventable.registerEvents")
 
-      console.log(`trigger event: ${eventName}`)
+    $("body").on("change", `[data-on-change-event]`, function(){
+      let eventName = $(this).data("on-change-event")
+
+      console.log(`Eventable.trigger event: ${eventName}`)
       $("body").trigger(eventName, [$(this)])
     })
   }
 }
 
-class SelectToggleableListener {
+class SelectGroupToggle {
+  parentSelector = ".js-toggle"
   groupSelector = ".js-toggle-group"
+  controlSelector = ".js-toggle-control"
 
   constructor(){
+    this.onLoad()
     this.registerListeners()
   }
 
-  registerListeners(){
-    console.log("registerListeners")
+  onLoad(){
+    console.log("SelectGroupToggle.onLoad")
 
-    $("body").on("toggle_group", this.toggleSelectGroup.bind(this))
+    // Can't seen to get this to work via triggering the change event
+    $(this.controlSelector).each($.proxy(function(i, el){
+      this.toggleSelectGroup($(el))
+    }, this))
   }
 
-  toggleSelectGroup(event, target){
-    console.log(`Performing: ${event.type}`)
+  registerListeners(){
+    console.log("SelectGroupToggle.registerListeners")
+
+    $("body").on("toggle_group", this.onToggleGroupEvent.bind(this))
+  }
+
+  onToggleGroupEvent(_event, target){
+    this.toggleSelectGroup(target)
+  }
+
+  toggleSelectGroup(target){
+    console.log(`SelectGroupToggle.Receive Event: ${event.type}`)
 
     let val = target.val()
     $(this.groupSelector).hide()
@@ -38,5 +55,5 @@ class SelectToggleableListener {
 
 $(document).on("turbolinks:load", function(){
   const events = new Eventable()
-  const selectListener = new SelectToggleableListener()
+  const selectListener = new SelectGroupToggle()
 });
