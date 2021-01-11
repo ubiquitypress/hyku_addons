ec2a57f6463be01b2f9752cfd261d40bd0b88a28
Paul Whitehead <prwhitehead@gmail.com>
2021-01-11 14:24:04 +0000

    Adding/removing of required tags on toggle required tag

--- app/assets/javascripts/hyku_addons/toggle_required_listener.js
+++ app/assets/javascripts/hyku_addons/toggle_required_listener.js
@@ -2,6 +2,8 @@ class ToggleRequiredListener {
   setEventName = "set_required"
   unsetEventName = "unset_required"
   afterCreateRequiredSelector = "[data-required]"
+  requiredTagClass = "required-tag"
+  requiredTagSelector = `.${this.requiredTagClass}`
 
   constructor(){
     this.registerListeners()
@@ -15,14 +17,45 @@ class ToggleRequiredListener {
   onSetEvent(event, target){
     this.toggleRequired(target, true)
 
-    // SHOW SPAN.ERROR $$$$$$$$$$
+    this.addRequiredLabel(target)
   }
 
   onUnsetEvent(event, target){
     this.toggleRequired(target, false)
+
+    this.removeRequiredLabel(target)
   }
 
   toggleRequired(target, state) {
-    target.find(this.afterCreateRequiredSelector).prop("required", state)
+    this.requiredChildren(target).prop("required", state)
+  }
+
+  addRequiredLabel(target){
+    this.requiredChildren(target).each($.proxy(function(i, el){
+      let parent = $(el).closest(".form-group")
+
+      if (parent.find(`label ${this.requiredTagSelector}`).length > 0) {
+        return false
+      }
+
+      parent.find("label").append(this.createRequiredTag(el))
+    }, this))
+  }
+
+  removeRequiredLabel(target){
+    this.requiredChildren(target).each($.proxy(function(i, el){
+      let parent = $(el).closest(".form-group")
+
+      parent.find(`label ${this.requiredTagSelector}`).remove()
+    }, this))
+  }
+
+  requiredChildren(target) {
+    return target.find(this.afterCreateRequiredSelector)
+  }
+
+  // This is hardcoded as a form might not have a label to clone
+  createRequiredTag(target) {
+    return `<span class="label label-info ${this.requiredTagClass}">required</span>`
   }
 }
