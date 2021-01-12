c86f557a052b27686b2baebe9828198645acda0a
BertZZ <bertie.wooles@gmail.com>
2021-01-12 18:08:56 +0000

    Implements Email format and Sign Up toggle

---
+++ app/models/concerns/hyku_addons/user_email_format.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module HykuAddons
+  module UserEmailFormat
+    extend ActiveSupport::Concern
+
+    included do
+      validate :must_have_valid_email_format
+    end
+
+    def must_have_valid_email_format
+      accepted_email_formats = current_account.settings['email_format']
+      if accepted_email_formats.present?
+        email_format = '@' + email.split('@')[-1]
+        errors.add(:email, "Email must contain #{accepted_email_formats[0]}") unless accepted_email_formats.include? email_format
+      end
+    end
+
+    def current_account
+      Site.account
+    end
+  end
+end
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,27 +2,29 @@
 
 require 'rails_helper'
 
-RSpec.describe HykuAddons::RegistrationsControllerBehavior, type: :feature do
+RSpec.describe HykuAddons::RegistrationsController, type: :feature do
   let(:account) { FactoryBot.create(:account) }
-  let(:false_account) { FactoryBot.create(:account) }
 
   context 'with account signup enabled' do
     it "allows the user to create an account" do
+      account.settings['allow_signup'] = "true"
+      account.save!
+      account.reload
       visit '/users/sign_up'
       fill_in 'Your Name', with: "Test User"
       fill_in 'Email address', with: "test@test.com"
       fill_in 'user_password', with: "Potato123!"
       fill_in 'user_password_confirmation', with: "Potato123!"
       expect{ click_on 'Create account' }.to change{ User.count }.by(1)
+      expect(page).to have_content("Create a new account")
     end
-
   end
 
   context 'with account signup diabled' do
     it 'does not allow a user to create an account' do
-      false_account.settings['allow_signup'] = "false"
-      false_account.save!
-      false_account.reload
+      account.settings['allow_signup'] = "false"
+      account.save!
+      account.reload
       visit '/users/sign_up'
       expect(page).to have_content("Account registration is disabled")
     end
