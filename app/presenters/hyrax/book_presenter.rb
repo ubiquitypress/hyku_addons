950f5af698c60089d7ef1b3fd2171b923174e806
Eneko Taberna <eneko.taberna@gmail.com>
2021-01-13 16:53:39 +0100

    Adds Book and Exhibition item work types

--- app/presenters/hyrax/book_presenter.rb
+++ app/presenters/hyrax/book_presenter.rb
@@ -2,14 +2,16 @@
 # Generated via
 #  `rails generate hyrax:work BookContribution`
 module Hyrax
-  class DatasetPresenter < Hyrax::WorkShowPresenter
+  class BookPresenter < Hyrax::WorkShowPresenter
     # Adds behaviors for hyrax-doi plugin.
     include Hyrax::DOI::DOIPresenterBehavior
     # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
     include Hyrax::DOI::DataCiteDOIPresenterBehavior
     include ::HykuAddons::GenericWorkPresenterBehavior
 
-    DELEGATED_METHODS = [:institution, :org_unit, :refereed, :funder, :fndr_project_ref, :add_info, :date_published,
+    DELEGATED_METHODS = [:volume, :pagination, :issn, :eissn, :official_link,
+                         :issue, :article_num, :alternative_journal_title,
+                         :institution, :org_unit, :refereed, :funder, :fndr_project_ref, :add_info, :date_published,
                          :date_accepted, :date_submitted, :project_name, :rights_holder, :place_of_publication,
                          :abstract, :alternate_identifier, :related_identifier, :creator_display,
                          :library_of_congress_classification, :alt_title, :dewey,
