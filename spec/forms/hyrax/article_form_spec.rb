776be56fc30b3469d5a357112e8879640427ee75
Eneko Taberna <eneko.taberna@kashing.co.uk>
2021-01-14 18:06:38 +0100

    Rubocop fixes

--- spec/forms/hyrax/article_form_spec.rb
+++ spec/forms/hyrax/article_form_spec.rb
@@ -17,10 +17,10 @@ RSpec.describe Hyrax::ArticleForm do
 
       it "sets the terms" do
         expected_terms = %i[title resource_type creator alt_title contributor rendering_ids abstract date_published
-                                       institution org_unit project_name funder fndr_project_ref journal_title pagination article_num
-                                       publisher place_of_publication issn eissn date_accepted date_submitted official_link
-                                       related_url language license rights_statement rights_holder doi alternate_identifier
-                                       related_identifier refereed keyword dewey library_of_congress_classification add_info]
+                            institution org_unit project_name funder fndr_project_ref journal_title pagination article_num
+                            publisher place_of_publication issn eissn date_accepted date_submitted official_link
+                            related_url language license rights_statement rights_holder doi alternate_identifier
+                            related_identifier refereed keyword dewey library_of_congress_classification add_info]
         is_expected.to include(*expected_terms)
       end
     end
