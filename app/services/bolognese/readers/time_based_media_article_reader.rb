5c8ce964f0a5bbbb298955b702e76d25fac6242f
Paul Whitehead <prwhitehead@gmail.com>
2021-01-22 17:51:29 +0000

    Update class parent so it'll work at least

--- app/services/bolognese/readers/time_based_media_article_reader.rb
+++ app/services/bolognese/readers/time_based_media_article_reader.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Bolognese
   module Readers
-    class TimeBaseMediaArticleReader < BaseWorkReader
+    class TimeBaseMediaArticleReader < GenericWorkReader
     end
   end
 end
