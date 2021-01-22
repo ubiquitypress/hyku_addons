26df30f0924ac0f8895591f159a47b066d9a9a87
Paul Whitehead <prwhitehead@gmail.com>
2021-01-22 16:52:51 +0000

    Move some methods around

--- app/services/bolognese/readers/base_work_reader.rb
+++ app/services/bolognese/readers/base_work_reader.rb
@@ -10,12 +10,12 @@ module Bolognese
       end
 
       # Some attributes wont match those that are expected by bolognese. This is
-      # a hash map of hyku attributes to bolognese attributes, old => new
+      # a hash map of hyku to bolognese attributes, old => new
       def self.mismatched_attribute_map
         {}
       end
 
-      # A hash of keys and their arry of nested attributes that need to be opperated on.
+      # A hash of keys and their array of nested attributes that need to be opperated on.
       # Each of the hash keys will be used as the key name in @reader_attributes and each array index is used as a
       # normal method to be found with `meta_values(key_name)`.
       def self.nested_attributes
@@ -23,7 +23,7 @@ module Bolognese
       end
 
       # An array of methods that should be called after the inital attributes have been collected.
-      # These methods should modify the `@reader_attributes` variable directly and be suffixed with `!`.
+      # These methods should modify the `@reader_attributes` variable directly
       def self.after_actions
         %i[build_related_identifiers! update_mismatched_attributes!]
       end
@@ -32,6 +32,7 @@ module Bolognese
         options.except!(:doi, :id, :url, :sandbox, :validate, :ra)
         read_options = ActiveSupport::HashWithIndifferentAccess.new(options)
 
+        # Use an instance so that the method isn't passed around
         @meta = string.present? ? Maremma.from_json(string) : {}
 
         reader_attributes.merge(read_options)
@@ -39,35 +40,15 @@ module Bolognese
 
       protected
 
-        def read_types
-          hyrax_resource_type = read_meta('has_model') || "Work"
-          resource_type = read_meta('resource_type').presence || hyrax_resource_type
+        # Compose and return the collected hash of values
+        def reader_attributes
+          @reader_attributes = (self.class.special_terms + work_type_terms).map do |term|
+            [term.to_s, term_value(term)]
+          end.to_h
 
-          {
-            "resourceTypeGeneral" => "Other",
-            "resourceType" => resource_type,
-            "hyrax" => hyrax_resource_type
-          }
-        end
+          perform_after_actions!
 
-        # Process any special nested attributes, like the `container` param
-        def build_nested_attributes!
-          self.class.nested_attributes.each do |key, terms|
-            parsed_values = terms.map do |term|
-              next unless (value = term_value(term)).present?
-
-              [term, value]
-            end.compact.to_h
-
-            @reader_attributes.merge!(key => parsed_values)
-          end
-        end
-
-        # Some fields are required by both Hyku and Bolognese, but their names differ.
-        def update_mismatched_attributes!
-          self.class.mismatched_attribute_map.each do |old, new|
-            @reader_attributes[new] = @reader_attributes.delete(old)
-          end
+          @reader_attributes
         end
 
         def read_creator
@@ -113,18 +94,38 @@ module Bolognese
           normalize_doi(@meta.fetch('doi', nil)&.first)
         end
 
-      private
+        def read_types
+          hyrax_resource_type = read_meta('has_model') || "Work"
+          resource_type = read_meta('resource_type').presence || hyrax_resource_type
 
-        # Compose and return the collected hash of values
-        def reader_attributes
-          @reader_attributes = (self.class.special_terms + work_type_terms).map do |term|
-            [term.to_s, term_value(term)]
-          end.to_h
-
-          perform_after_actions!
-
-          @reader_attributes
+          {
+            "resourceTypeGeneral" => "Other",
+            "resourceType" => resource_type,
+            "hyrax" => hyrax_resource_type
+          }
         end
+
+        # Process any special nested attributes, like the `container` param
+        def build_nested_attributes!
+          self.class.nested_attributes.each do |key, terms|
+            parsed_values = terms.map do |term|
+              next unless (value = term_value(term)).present?
+
+              [term, value]
+            end.compact.to_h
+
+            @reader_attributes.merge!(key => parsed_values)
+          end
+        end
+
+        # Some fields are required by both Hyku and Bolognese, but their names differ.
+        def update_mismatched_attributes!
+          self.class.mismatched_attribute_map.each do |old, new|
+            @reader_attributes[new] = @reader_attributes.delete(old)
+          end
+        end
+
+      private
 
         # Perform any required actions after the attributes hash has been built, all actions need to updat the array
         def perform_after_actions!
