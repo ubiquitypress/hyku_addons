f2386a804c5ba3dfb431e1fde0c798224d5593e3
Paul Danelli <prdanelli@gmail.com>
2021-01-25 11:17:30 +0000

    Move methods to base reader to flatten hierarchy tree

--- app/services/bolognese/readers/time_based_media_article_reader.rb
+++ app/services/bolognese/readers/time_based_media_article_reader.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Bolognese
   module Readers
-    class TimeBaseMediaArticleReader < GenericWorkReader
+    class TimeBaseMediaArticleReader < BaseWorkReader
     end
   end
 end
