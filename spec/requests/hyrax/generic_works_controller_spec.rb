29cb634cf68e587e31df243f1df36ee1269072f9
Paul Danelli <prdanelli@gmail.com>
2021-01-26 10:12:38 +0000

    Try and work out which test is causing the CI error

--- spec/requests/hyrax/generic_works_controller_spec.rb
+++ spec/requests/hyrax/generic_works_controller_spec.rb
@@ -19,35 +19,35 @@ RSpec.describe Hyrax::GenericWorksController, type: :request, multitenant: true 
   end
 
   describe "#show" do
-    context "when the work is public" do
-      context "when requesting an HTML document" do
-        # Make sure normal request still work
-        it "returns the correct response" do
-          get main_app.polymorphic_path(work)
+    # context "when the work is public" do
+    #   context "when requesting an HTML document" do
+    #     # Make sure normal request still work
+    #     it "returns the correct response" do
+    #       get main_app.polymorphic_path(work)
 
-          expect(response).to be_successful
-          expect(response.header.fetch("Content-Type")).to include("text/html")
-          expect(response.body).to include(work.title.first)
-        end
-      end
+    #       expect(response).to be_successful
+    #       expect(response.header.fetch("Content-Type")).to include("text/html")
+    #       expect(response.body).to include(work.title.first)
+    #     end
+    #   end
 
-      context "when requesting a RIS document" do
-        it "returns the correct response" do
-          get main_app.polymorphic_path(work, format: :ris)
+    #   context "when requesting a RIS document" do
+    #     it "returns the correct response" do
+    #       get main_app.polymorphic_path(work, format: :ris)
 
-          expect(response).to be_successful
-          expect(response.header.fetch("Content-Type")).to include("application/x-research-info-systems")
-          expect(response.body).to include("T1  - #{work.title.first}")
-        end
-      end
-    end
+    #       expect(response).to be_successful
+    #       expect(response.header.fetch("Content-Type")).to include("application/x-research-info-systems")
+    #       expect(response.body).to include("T1  - #{work.title.first}")
+    #     end
+    #   end
+    # end
 
-    context "when the work is private" do
-      let(:work) { create(:work, visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE) }
+    # context "when the work is private" do
+    #   let(:work) { create(:work, visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE) }
 
-      it "shows a 404" do
-        expect { get main_app.polymorphic_path(work, format: :ris) }.to raise_error("ActionController::UnknownFormat")
-      end
-    end
+    #   it "shows a 404" do
+    #     expect { get main_app.polymorphic_path(work, format: :ris) }.to raise_error("ActionController::UnknownFormat")
+    #   end
+    # end
   end
 end
