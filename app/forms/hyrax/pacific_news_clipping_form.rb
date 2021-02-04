aa06fa82681abd70597ca1f628ed98087731b564
BertZZ <bertie.wooles@gmail.com>
2021-02-04 12:52:09 +0000

    removes org_unit from news clipping required

--- app/forms/hyrax/pacific_news_clipping_form.rb
+++ app/forms/hyrax/pacific_news_clipping_form.rb
@@ -8,14 +8,14 @@ module Hyrax
     # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
     include Hyrax::DOI::DataCiteDOIFormBehavior
     include ::HykuAddons::WorkForm
-    
+
     self.model_class = ::PacificNewsClipping
     add_terms %i[title alt_title resource_type creator contributor abstract
                  date_published challenged location outcome participant reading_level
                  photo_caption photo_description pagination is_included_in additional_links
                  rights_holder license doi subject keyword add_info]
 
-    self.required_fields = %i[title creator resource_type institution org_unit]
+    self.required_fields = %i[title creator resource_type institution]
 
     def self.build_permitted_params
       super.tap do |permitted_params|
