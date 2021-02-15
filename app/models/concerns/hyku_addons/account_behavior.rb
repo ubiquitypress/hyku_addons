5e4a179555f950b70a8206283d78064cc619cbf6
Paul Danelli <prdanelli@gmail.com>
2021-02-15 16:27:03 +0000

    Remove 'live' setting

--- app/models/concerns/hyku_addons/account_behavior.rb
+++ app/models/concerns/hyku_addons/account_behavior.rb
@@ -12,7 +12,7 @@ module HykuAddons
       store_accessor :data, :is_parent
       store_accessor :settings, :contact_email, :weekly_email_list, :monthly_email_list, :yearly_email_list,
                      :index_record_to_shared_search, :google_scholarly_work_types,
-                     :live, :enabled_doi, :gtm_id, :turn_off_fedora_collection_work_association,
+                     :enabled_doi, :gtm_id, :turn_off_fedora_collection_work_association,
                      :add_collection_list_form_display, :hide_form_relationship_tab, :shared_login,
                      :work_type_list, :email_hint_text, :email_format, :help_texts, :work_unwanted_fields,
                      :required_json_property, :creator_fields, :contributor_fields, :metadata_labels,
