a8c4b0b8844385d4f8b9941fa55483177c43161f
Paul Danelli <prdanelli@gmail.com>
2021-02-16 10:06:36 +0000

    Add WorksControllerBehaviour to new controllers

--- app/controllers/hyrax/thesis_or_dissertations_controller.rb
+++ app/controllers/hyrax/thesis_or_dissertations_controller.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
-# Generated via
-#  `rails generate hyrax:work ThesisOrDissertation`
+
 module Hyrax
-  # Generated controller for ThesisOrDissertation
   class ThesisOrDissertationsController < ApplicationController
-    # Adds Hyrax behaviors to the controller.
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
-    self.curation_concern_type = ::ThesisOrDissertation
+    include HykuAddons::WorksControllerBehavior
 
-    # Use this line if you want to use a custom presenter
+    self.curation_concern_type = ::ThesisOrDissertation
     self.show_presenter = Hyrax::ThesisOrDissertationPresenter
   end
 end
