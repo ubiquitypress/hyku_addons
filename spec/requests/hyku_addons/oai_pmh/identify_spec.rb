89ea3bb933dbddac45673e8eb3bb45c1579e61f0
Eneko Taberna <eneko.taberna@gmail.com>
2021-02-18 18:52:50 +0100

    Adds more tests for OAI endpoints (#96)

--- spec/requests/hyku_addons/oai_pmh/identify_spec.rb
+++ spec/requests/hyku_addons/oai_pmh/identify_spec.rb
@@ -1,50 +1,37 @@
 # frozen_string_literal: true
 require 'spec_helper'
+require File.expand_path('../../../helpers/user_with_work_context.rb', __dir__)
 
 RSpec.describe CatalogController, multitenant: true do
-  let(:xml) { Nokogiri::XML(response.body) }
+  include_context 'user with work context' do
+    let(:params) { { verb: 'Identify' } }
 
-  let(:user)       { create(:user) }
-  let(:account)    do
-    create(:account,
-           name:   'example',
-           oai_admin_email: 'some@example.com',
-           oai_prefix:      'hyku.example.com',
-           oai_sample_identifier: work.id)
-  end
-  let!(:work)      { create(:work, user: user) }
-  let(:identifier) { work.id }
-
-  before do
-    Site.update(account: account)
-    allow(Apartment::Tenant).to receive(:switch).with(account.tenant) do |&block|
-      block.call
+    before do
+      get oai_catalog_path(params)
     end
-    host! account.cname
-    get '/catalog/oai?verb=Identify'
-  end
 
-  it "contains repository name" do
-    expect(xml.at_xpath('//xmlns:repositoryName').text).to eql 'example'
-  end
+    it "contains repository name" do
+      expect(xml.at_xpath('//xmlns:repositoryName').text).to eql 'example'
+    end
 
-  it "contains base url" do
-    expect(xml.at_xpath('//xmlns:baseURL').text).to eql "http://#{account.cname}/catalog/oai?locale=en"
-  end
+    it "contains base url" do
+      expect(xml.at_xpath('//xmlns:baseURL').text).to eql "http://#{account.cname}/catalog/oai?locale=en"
+    end
 
-  it "contains admin email" do
-    expect(xml.at_xpath('//xmlns:adminEmail').text).to eql 'some@example.com'
-  end
+    it "contains admin email" do
+      expect(xml.at_xpath('//xmlns:adminEmail').text).to eql 'some@example.com'
+    end
 
-  it "contains repository prefix/identifier" do
-    expect(
-      xml.at_xpath('//oai-identifier:repositoryIdentifier', 'oai-identifier' => "http://www.openarchives.org/OAI/2.0/oai-identifier").text
-    ).to eql 'hyku.example.com'
-  end
+    it "contains repository prefix/identifier" do
+      expect(
+        xml.at_xpath('//oai-identifier:repositoryIdentifier', 'oai-identifier' => "http://www.openarchives.org/OAI/2.0/oai-identifier").text
+      ).to eql 'hyku.example.com'
+    end
 
-  it "contains sample identifier" do
-    expect(
-      xml.at_xpath('//oai-identifier:sampleIdentifier', 'oai-identifier' => "http://www.openarchives.org/OAI/2.0/oai-identifier").text
-    ).to eql "hyku.example.com:#{work.id}"
+    it "contains sample identifier" do
+      expect(
+        xml.at_xpath('//oai-identifier:sampleIdentifier', 'oai-identifier' => "http://www.openarchives.org/OAI/2.0/oai-identifier").text
+      ).to eql "hyku.example.com:#{work.id}"
+    end
   end
 end
