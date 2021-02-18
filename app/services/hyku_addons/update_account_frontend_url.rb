d006b04e4386dea15c844d9c7076000a65ddb593
Eneko Taberna <eneko.taberna@gmail.com>
2021-02-18 13:35:48 +0100

    Adds a rake task to update the frontend_url of accounts

---
+++ app/services/hyku_addons/update_account_frontend_url.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+require 'uri'
+
+# Update the frontend_url of an account.
+class HykuAddons::UpdateAccountFrontendUrl
+  attr_reader :account
+  attr_reader :frontend_url
+
+  ##
+  # @param account [Account]
+  # @param frontend_url [String] Must be a valid URI
+  def initialize(account, frontend_url)
+    @account = account
+    @frontend_url = frontend_url
+  end
+
+  ##
+  # @return [Boolean] true if validations pass and was able to update the account
+  def save
+    valid? && update_account_frontend_url
+  end
+
+  def valid?
+    @account&.valid? && valid_url?(@frontend_url)
+  end
+
+  ##
+  # update the frontend_url references of the @account
+  def update_account_frontend_url
+    @account.update frontend_url: @frontend_url
+  end
+
+  protected
+
+    def valid_url?(uri)
+      URI.parse(uri)&.host
+    rescue URI::InvalidURIError
+      false
+    end
+end
--- lib/tasks/account.rake
+++ lib/tasks/account.rake
@@ -6,5 +6,10 @@ namespace :hyku do
     task :create, [:name] => [:environment] do |_t, args|
       CreateAccount.new(Account.new(name: args[:name])).save
     end
+    desc 'Update the frontend url of an account'
+    task :frontend_url, [:tenant, :frontend_url] => [:environment] do |_t, args|
+      account = Account.find_by(tenant: args[:tenant])
+      UpdateAccountFrontendUrl.new(account, args[:frontend_url]).save
+    end
   end
 end
