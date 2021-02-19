e03117eb2e88353454fc499b3d4b2386e0239dfd
BertZZ <bertie.wooles@gmail.com>
2021-02-19 15:35:29 +0000

    appeases rubocop

--- app/helpers/hyku_addons/contributor_field_helper.rb
+++ app/helpers/hyku_addons/contributor_field_helper.rb
@@ -2,9 +2,9 @@
 module HykuAddons
   module ContributorFieldHelper
     def add_pacific_contributor_personal_fields(array)
-      array.insert(1,{ field_type: :text, field_slug: :contributor_middle_name })
-      array.insert(3,{ field_type: :text, field_slug: :contributor_suffix })
-      array.insert(4,{ field_type: :text, field_slug: :contributor_institution })
+      array.insert(1, field_type: :text, field_slug: :contributor_middle_name)
+      array.insert(3, field_type: :text, field_slug: :contributor_suffix)
+      array.insert(4, field_type: :text, field_slug: :contributor_institution)
       array
     end
   end
--- app/helpers/hyku_addons/creator_field_helper.rb
+++ app/helpers/hyku_addons/creator_field_helper.rb
@@ -2,9 +2,9 @@
 module HykuAddons
   module CreatorFieldHelper
     def add_pacific_creator_personal_fields(array)
-      array.insert(1,{ field_type: :text, field_slug: :creator_middle_name })
-      array.insert(3,{ field_type: :text, field_slug: :creator_suffix })
-      array.insert(4,{ field_type: :text, field_slug: :creator_institution , field_args: { data: { required: true } }} )
+      array.insert(1, field_type: :text, field_slug: :creator_middle_name)
+      array.insert(3, field_type: :text, field_slug: :creator_suffix)
+      array.insert(4, field_type: :text, field_slug: :creator_institution, field_args: { data: { required: true } })
       array
     end
   end
