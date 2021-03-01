c89dae5009049b5b4df19d2e452b4096e20c0626
Paul Danelli <prdanelli@gmail.com>
2021-03-01 16:02:06 +0000

    Add spec layout

---
+++ spec/helpers/revised_admin_set_work_form_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "HykuAddons::RevisedAdminSetWorkFormHelper" do
+  describe "form_tabs_for" do
+    let(:work) { GenericWork.new(title: ["Moomin"]) }
+    let(:form) { Hyrax::GenericWorkForm.new(work, nil, nil) }
+    let(:helper) do
+      _view.tab do |v|
+        v.extend(ApplicationHelper)
+        v.extend(HyraxHelper)
+        v.extend(Hyrax::DOI::WorkFormHelper)
+        v.assign(view_assigns)
+      end
+    end
+
+    describe "when the feature is disabled" do
+      before do
+        Flipflop::FeatureSet.current.replace do
+          Flipflop.configure do
+            feature :revised_admin_set_layout, default: false
+          end
+        end
+      end
+    end
+
+    describe "when the feature is enabled" do
+      before do
+        Flipflop::FeatureSet.current.replace do
+          Flipflop.configure do
+            feature :revised_admin_set_layout, default: true
+          end
+        end
+      end
+
+    end
+  end
+end
