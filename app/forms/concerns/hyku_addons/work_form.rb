385443c7931326582de48bc5c87e74d03bee43e4
Paul Danelli <prdanelli@gmail.com>
2021-03-02 15:34:57 +0000

    Passing params from js modal to work form

--- app/forms/concerns/hyku_addons/work_form.rb
+++ app/forms/concerns/hyku_addons/work_form.rb
@@ -113,5 +113,14 @@ module HykuAddons
         ]
       end
     end
+
+
+    def initialize(model, current_ability, controller)
+      if Flipflop.enabled?(:simplified_admin_set_selection) && controller.params['admin_set_id'].present?
+        model.admin_set_id = controller.params['admin_set_id']
+      end
+
+      super(model, current_ability, controller)
+    end
   end
 end
