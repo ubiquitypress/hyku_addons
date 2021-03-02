e20c71c2cf82616f48f167262d51dcfb596e300d
Paul Danelli <prdanelli@gmail.com>
2021-03-02 09:29:23 +0000

    Basic flipflop/html structure

--- config/features.rb
+++ config/features.rb
@@ -3,4 +3,8 @@ Flipflop.configure do
   feature :oai_endpoint,
           default: true,
           description: "Enable OAI-PMH endpoint for harvesting."
+
+  feature :revised_admin_set_layout,
+          default: false,
+          description: "Move the admin set to the works modal and hide the relationships tab on the deposit form"
 end
