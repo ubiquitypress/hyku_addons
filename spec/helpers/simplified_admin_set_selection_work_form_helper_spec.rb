5c69cd147ecce4459d7e23eefcc368c4bfde852c
Paul Danelli <prdanelli@gmail.com>
2021-03-04 13:56:26 +0000

    Adding more specs

--- spec/helpers/simplified_admin_set_selection_work_form_helper_spec.rb
+++ spec/helpers/simplified_admin_set_selection_work_form_helper_spec.rb
@@ -3,12 +3,15 @@
 require "rails_helper"
 
 RSpec.describe HykuAddons::SimplifiedAdminSetSelectionWorkFormHelper do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { create(:user) }
   let(:model_class) do
     Class.new(GenericWork) do
       include ::HykuAddons::GenericWorkOverrides
     end
   end
-  let(:work) { model_class.new(title: ["Moomin"]) }
+  let(:work) { model_class.new(title: ["Moomin"], depositor: user.user_key) }
   let(:form_class) do
     Class.new(Hyrax::GenericWorkForm) do
       include ::HykuAddons::GenericWorkFormOverrides
@@ -17,7 +20,7 @@ RSpec.describe HykuAddons::SimplifiedAdminSetSelectionWorkFormHelper do
   let(:form) { form_class.new(work, nil, nil) }
   let(:helper) { _view }
 
-  describe "form_tabs_for" do
+  describe "#form_tabs_for" do
     context "when the feature is disabled" do
       before do
         allow(Flipflop).to receive(:enabled?).with(:simplified_admin_set_selection).and_return(false)
@@ -37,6 +40,53 @@ RSpec.describe HykuAddons::SimplifiedAdminSetSelectionWorkFormHelper do
 
       it "includes relationships" do
         expect(helper.form_tabs_for(form: form)).not_to include("relationships")
+      end
+    end
+  end
+
+  describe "#available_admin_sets" do
+    it "returns an array" do
+      expect(helper.available_admin_sets).to be_an(Array)
+    end
+  end
+
+  describe "#can_edit?" do
+    before do
+      # We need to signin the user so that `current_user` is accessible to us
+      sign_in(user)
+    end
+
+    it "calls current_ability" do
+      expect(helper.current_ability).to receive(:can?).with(:edit, form.model)
+      helper.can_edit?(form.model)
+    end
+  end
+
+  describe "#depositor?" do
+    context "when the depositor is signed in" do
+      before do
+        sign_in(user)
+      end
+
+      it "calls user_key on current_user" do
+        expect(helper.current_user).to receive(:user_key)
+        helper.depositor?(form.depositor)
+      end
+
+      it "is true" do
+        expect(helper.depositor?(form.depositor)).to be_truthy
+      end
+    end
+
+    context "when the depositor is not signed in" do
+      let(:admin) { create(:admin) }
+
+      before do
+        sign_in(admin)
+      end
+
+      it "is false" do
+        expect(helper.depositor?(form.depositor)).to be_falsey
       end
     end
   end
