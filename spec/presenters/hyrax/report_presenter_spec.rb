e3d50369254ff0e79d3fa18e1379f83e59c40c33
Chris Colvard <chris.colvard@gmail.com>
2021-03-08 10:59:58 -0500

    Fix presenter tests

--- spec/presenters/hyrax/report_presenter_spec.rb
+++ spec/presenters/hyrax/report_presenter_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Hyrax::ReportPresenter do
   let(:work) { build(:generic_work) }
 
   let(:additional_properties) do
-    %i[title resource_type creator_display alt_title contributor_display rendering_ids abstract
+    %i[title resource_type creator creator_json alt_title contributor_display rendering_ids abstract
        date_published institution org_unit project_name funder fndr_project_ref series_name
        editor_display volume edition publisher place_of_publication isbn issn eissn date_accepted
        date_submitted official_link related_url language license rights_statement rights_holder doi
