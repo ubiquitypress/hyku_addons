5a9ee9d4e8ba4fe3a17590f0830a36f909b42573
Chris Colvard <chris.colvard@gmail.com>
2021-03-08 10:59:58 -0500

    Fix presenter tests

--- spec/presenters/hyrax/thesis_or_dissertation_presenter_spec.rb
+++ spec/presenters/hyrax/thesis_or_dissertation_presenter_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Hyrax::ThesisOrDissertationPresenter do
   let(:work) { build(:generic_work) }
 
   let(:additional_properties) do
-    %i[title resource_type creator_display alt_title contributor_display rendering_ids abstract
+    %i[title resource_type creator creator_json alt_title contributor_display rendering_ids abstract
        date_published institution org_unit project_name funder fndr_project_ref version_number publisher
        current_he_institution date_accepted date_submitted official_link related_url language license
        rights_statement rights_holder doi qualification_name qualification_level alternate_identifier
