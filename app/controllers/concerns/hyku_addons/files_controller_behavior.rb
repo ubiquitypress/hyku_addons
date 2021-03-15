b8092381498e3bdc87fa1f9afe9071296a5b084c
Paul Danelli <prdanelli@gmail.com>
2021-03-15 11:48:29 +0000

    Rubocop

--- app/controllers/concerns/hyku_addons/files_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/files_controller_behavior.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module HykuAddons
   module FilesControllerBehavior
     extend ActiveSupport::Concern
@@ -9,22 +10,24 @@ module HykuAddons
       }, status: 200
     end
 
-    def file_set
-      @file_set ||= begin
-        return unless file_presenter&.current_ability&.can?(:read, file_document.id)
+    private
 
-        FileSet.find(file_document.id)
+      def file_set
+        @file_set ||= begin
+          return unless file_presenter&.current_ability&.can?(:read, file_document.id)
+
+          FileSet.find(file_document.id)
+        end
       end
-    end
 
-    def file_presenter
-      @file_presenter ||= Hyrax::FileSetPresenter.new(file_document, current_ability, request)
-    end
-
-    def file_document
-      @file_document ||= begin
-        repository.search(Hyrax::FileSetSearchBuilder.new(self).with(params.slice(:id))).documents.first
+      def file_presenter
+        @file_presenter ||= Hyrax::FileSetPresenter.new(file_document, current_ability, request)
       end
-    end
+
+      def file_document
+        @file_document ||= begin
+          repository.search(Hyrax::FileSetSearchBuilder.new(self).with(params.slice(:id))).documents.first
+        end
+      end
   end
 end
