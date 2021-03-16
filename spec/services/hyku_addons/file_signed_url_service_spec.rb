2a15dba6d69161661e10932983cf9b520d2ff1ff
Paul Danelli <prdanelli@gmail.com>
2021-03-16 13:12:00 +0000

    Finish up specs

--- spec/services/hyku_addons/file_signed_url_service_spec.rb
+++ spec/services/hyku_addons/file_signed_url_service_spec.rb
@@ -3,22 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe HykuAddons::FileSignedUrlService do
+  let(:test_url) { "http://test123.com/file.jpg" }
   let(:hash) { "742e7a47-6780-4780-b561-4bd89738c56f" }
   let(:file) { double(Hydra::PCDM::File, { digest: "test:file:#{hash}" }) }
 
   describe "#new" do
-    it "requires a file argument" do
+    it "requires a valid file argument" do
       expect { described_class.new }.to raise_error(ArgumentError)
-    end
-
-    it "requires a Hydra::PCDM::File or instance responding to digest" do
+      expect { described_class.new(file: Class.new) }.to raise_error(ArgumentError)
       expect { described_class.new(file: file) }.not_to raise_error
     end
   end
 
   describe "#perform" do
     subject { described_class.new(file: file) }
-    let(:test_url) { "http://test123.com/file.jpg" }
     let(:bucket_mock) { double(Google::Cloud::Storage::Bucket, file: file_mock) }
     let(:file_mock) { double(Google::Cloud::Storage::File, signed_url: test_url) }
     let(:storage_mock) { double(Google::Cloud::Storage, bucket: bucket_mock) }
