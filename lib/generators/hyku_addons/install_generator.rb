3011ecf9596276ef37dc06c0c9055fdbf984ad61
Paul Danelli <prdanelli@gmail.com>
2021-03-16 16:06:40 +0000

    Change the installer to assume hyrax-doi is always present

--- lib/generators/hyku_addons/install_generator.rb
+++ lib/generators/hyku_addons/install_generator.rb
@@ -47,7 +47,7 @@ module HykuAddons
     end
 
     def inject_into_helper
-      insert_into_file(Rails.root.join('app', 'helpers', 'hyrax_helper.rb'), after: 'include Hyrax::HyraxHelperBehavior') do
+      insert_into_file(Rails.root.join('app', 'helpers', 'hyrax_helper.rb'), after: 'include Hyrax::DOI::HelperBehavior') do
         "\n" \
         "  # Helpers provided by hyku_addons plugin.\n" \
         "  include HykuAddons::HelperBehavior"
