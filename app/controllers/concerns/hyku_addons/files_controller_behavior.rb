b2dd471f1ec871dadb29b8e2e8eabacc69153285
Paul Danelli <prdanelli@gmail.com>
2021-03-16 17:32:30 +0000

    Update app/controllers/concerns/hyku_addons/files_controller_behavior.rb
    Update to remove unnecessary method.
    
    Co-authored-by: Chris Colvard <chris.colvard@gmail.com>

--- app/controllers/concerns/hyku_addons/files_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/files_controller_behavior.rb
@@ -5,18 +5,10 @@ module HykuAddons
 
     # /api/v1/tenant/:tenant_id/files/:file_set_id/work
     def work
-      render plain: file_set.parent.attributes.dig("id"), status: 200
+      render plain: file_presenter.parent&.id, status: 200
     end
 
     private
-
-      def file_set
-        @file_set ||= begin
-          return unless file_presenter&.current_ability&.can?(:read, file_document.id)
-
-          FileSet.find(file_document.id)
-        end
-      end
 
       def file_presenter
         @file_presenter ||= Hyrax::FileSetPresenter.new(file_document, current_ability, request)
