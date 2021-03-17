442f78b2c04854018c1c2be12223080ba60bd21c
BertZZ <bertie.wooles@gmail.com>
2021-03-17 17:00:13 +0000

    Fixes tests, adds tests and changes conditional

--- config/features.rb
+++ config/features.rb
@@ -8,9 +8,9 @@ Flipflop.configure do
           default: false,
           description: "The admin set to deposit works is selected from the add work modal and the relationships tab on the deposit form is hidden for depositors."
 
-  feature :hide_collections_link,
+  feature :show_collections_link,
           default: false,
-          description: "Hides collections tab on sidebar for non admin users"
+          description: "Shows collections tab on sidebar for non admin users"
 
   feature :simplified_deposit_form,
           default: false,
