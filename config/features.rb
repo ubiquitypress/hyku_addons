8a81d5ad8bfc6a0265c1f063ee8f2c70b95defe1
BertZZ <bertie.wooles@gmail.com>
2021-03-17 14:17:42 +0000

    Adds feature flipper to hide collection link on sidebar

--- config/features.rb
+++ config/features.rb
@@ -7,4 +7,8 @@ Flipflop.configure do
   feature :simplified_admin_set_selection,
           default: false,
           description: "The admin set to deposit works is selected from the add work modal and the relationships tab on the deposit form is hidden for depositors."
+
+  feature :hide_collections_link,
+          default: false,
+          description: "Hides collections tab on sidebar for non admin users"
 end
