afc41181f8614cb8bf1494904162d409bb3f5008
Paul Danelli <prdanelli@gmail.com>
2021-03-23 12:24:46 +0000

    Spec result output including new creator orcid

--- spec/services/bolognese/writers/hyku_addons_work_form_fields_writer_spec.rb
+++ spec/services/bolognese/writers/hyku_addons_work_form_fields_writer_spec.rb
@@ -21,10 +21,6 @@ RSpec.describe Bolognese::Writers::HykuAddonsWorkFormFieldsWriter do
       it { expect(result["title"]).to eq ["As Paisagens Sonora, Olfactiva e Culinária em Alice’s Adventures in Wonderland (1865), de Lewis Carroll"] }
       it { expect(result["doi"]).to eq ["10.5334/as.1"] }
 
-      it { expect(result["creator"]).to be_an(Array) }
-      it { expect(result["creator"].first["creator_given_name"]).to eq "Rogério Miguel" }
-      it { expect(result["creator"].first["creator_family_name"]).to eq "Puga" }
-
       it { expect(result["date_created"]).to be_an(Array) }
       it { expect(result["date_created"].first["date_created_year"]).to be 2020 }
       it { expect(result["date_created"].first["date_created_month"]).to be 2 }
@@ -39,6 +35,10 @@ RSpec.describe Bolognese::Writers::HykuAddonsWorkFormFieldsWriter do
       it { expect(result["date_published"].first["date_published_year"]).to be 2020 }
       it { expect(result["date_published"].first["date_published_month"]).to be 1 }
       it { expect(result["date_published"].first["date_published_day"]).to be 1 }
+
+      it { expect(result["creator"]).to be_an(Array) }
+      it { expect(result["creator"].first["creator_given_name"]).to eq "Rogério Miguel" }
+      it { expect(result["creator"].first["creator_family_name"]).to eq "Puga" }
     end
 
     describe "10.7554-elife.63646.xml, a journal doi with multiple complete creators" do
@@ -49,6 +49,41 @@ RSpec.describe Bolognese::Writers::HykuAddonsWorkFormFieldsWriter do
       it { expect(result["publisher"]).to eq ["eLife Sciences Publications, Ltd"] }
       it { expect(result["title"]).to eq ["SARS-CoV-2 S protein:ACE2 interaction reveals novel allosteric targets"] }
       it { expect(result["doi"]).to eq ["10.7554/elife.63646"] }
+
+      it { expect(result["description"].first).to include("The spike (S) protein is the main handle for SARS-CoV-2") }
+      it { expect(result["keywords"]).to be_nil }
+
+      it { expect(result["date_created"]).to be_an(Array) }
+      it { expect(result["date_created"].first["date_created_year"]).to be 2021 }
+      it { expect(result["date_created"].first["date_created_month"]).to be 2 }
+      it { expect(result["date_created"].first["date_created_day"]).to be 8 }
+
+      it { expect(result["date_updated"]).to be_an(Array) }
+      it { expect(result["date_updated"].first["date_updated_year"]).to be 2021 }
+      it { expect(result["date_updated"].first["date_updated_month"]).to be 3 }
+      it { expect(result["date_updated"].first["date_updated_day"]).to be 4 }
+
+      it { expect(result["date_published"]).to be_an(Array) }
+      it { expect(result["date_published"].first["date_published_year"]).to be 2021 }
+      it { expect(result["date_published"].first["date_published_month"]).to be 1 }
+      it { expect(result["date_published"].first["date_published_day"]).to be 1 }
+
+      it { expect(result["contributor"]).to be_an(Array) }
+      it { expect(result["contributor"].size).to eq 0 }
+
+      it { expect(result["creator"]).to be_an(Array) }
+      it { expect(result["creator"].size).to eq 12 }
+
+      # Just do the first couple as if they work, the rest are fine too as we have the size checked above
+      it { expect(result["creator"][0]["creator_name_type"]).to eq "Personal" }
+      it { expect(result["creator"][0]["creator_given_name"]).to eq "Palur V" }
+      it { expect(result["creator"][0]["creator_family_name"]).to eq "Raghuvamsi" }
+      it { expect(result["creator"][0]["creator_orcid"]).to eq "https://orcid.org/0000-0002-0897-6935" }
+
+      it { expect(result["creator"][1]["creator_name_type"]).to eq "Personal" }
+      it { expect(result["creator"][1]["creator_given_name"]).to eq "Nikhil K" }
+      it { expect(result["creator"][1]["creator_family_name"]).to eq "Tulsian" }
+      it { expect(result["creator"][1]["creator_orcid"]).to eq "https://orcid.org/0000-0001-6577-6748" }
     end
   end
 end
