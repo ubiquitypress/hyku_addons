a6491f07c11cad45ab134d35fd9fdd7e13042d3e
Paul Danelli <prdanelli@gmail.com>
2021-03-23 10:58:47 +0000

    Setup development debugging endpoint for json and xml output

--- app/controllers/concerns/hyku_addons/doi_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/doi_controller_behavior.rb
@@ -10,9 +10,13 @@ module HykuAddons
           # TODO: Make this respond to json instead of js
           format.js { render json: json_response, status: :ok }
 
-          # NOTE: This is temporary, just so we have a URL to debug
-          # format.html { render xml: raw_response.string, status: :ok }
-          format.html { render json: json_response, status: :ok }
+          if Rails.env.development?
+            format.html { render json: json_response, status: :ok }
+
+            # NOTE: Use this to see the raw XML returned, useful for creating fixtures for specs
+            # http://repo.lvh.me:3000/doi/autofill.xml?curation_concern=generic_work&doi=10.7554/elife.63646
+            format.xml { render xml: raw_response.string, status: :ok }
+          end
         end
 
       rescue ::Hyrax::DOI::NotFoundError => e
@@ -28,11 +32,15 @@ module HykuAddons
         end
 
         def formatted_work
-          meta = Bolognese::Metadata.new(input: doi)
+          meta = raw_response
 
           raise ::Hyrax::DOI::NotFoundError, "DOI (#{doi}) could not be found." if meta.blank? || meta.doi.blank?
 
           meta.hyku_addons_work_form_fields
+        end
+
+        def raw_response
+          Bolognese::Metadata.new(input: doi)
         end
 
         def curation_concern
