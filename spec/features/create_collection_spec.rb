54f043f373560374b60b8265572005e4e9077c70
BertZZ <bertie.wooles@gmail.com>
2021-03-25 16:48:54 +0000

    Appease Rubocop

--- spec/features/create_collection_spec.rb
+++ spec/features/create_collection_spec.rb
@@ -74,13 +74,13 @@ RSpec.feature 'Create a Collection', js: false, clean: true do
       user.remove_role(:admin)
       allow(Flipflop).to receive(:enabled?).with(:show_repository_objects_links).and_return(false)
       visit '/dashboard'
-      expect(page).not_to have_link( "Collections")
+      expect(page).not_to have_link("Collections")
     end
 
     it "Does display the collections link when the setting is on" do
       allow(Flipflop).to receive(:enabled?).with(:show_repository_objects_links).and_return(true)
       visit '/dashboard'
-      expect(page).to have_link( "Collections")
+      expect(page).to have_link("Collections")
     end
   end
 end
