e7baf002031ac40dfbd51076e4472533ea0bbbdb
BertZZ <bertie.wooles@gmail.com>
2021-03-26 16:17:08 +0000

    Uses factory bot for user creation and adds admin test contexts

--- spec/features/create_collection_spec.rb
+++ spec/features/create_collection_spec.rb
@@ -5,12 +5,7 @@ include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Create a Collection', js: false, clean: true do
-  let(:user_attributes) do
-    { email: 'test@example.com' }
-  end
-  let(:user) do
-    User.new(user_attributes) { |u| u.save(validate: false) }
-  end
+  let(:user) { FactoryBot.create(:admin) }
   let(:collection_type) { Hyrax::CollectionType.create(title: 'test_collection_type') }
 
   before do
@@ -67,11 +62,24 @@ RSpec.feature 'Create a Collection', js: false, clean: true do
   end
 
   context "when user is not an admin" do
+    let(:user) { FactoryBot.create(:user) }
     it "Does not display the collections link when the setting is off" do
-      user.remove_role(:admin) # Ensure user is not an admin for this test
       allow(Flipflop).to receive(:enabled?).with(:show_repository_objects_links).and_return(false)
       visit '/dashboard'
       expect(page).not_to have_link("Collections")
+    end
+
+    it "Does display the collections link when the setting is on" do
+      allow(Flipflop).to receive(:enabled?).with(:show_repository_objects_links).and_return(true)
+      visit '/dashboard'
+      expect(page).to have_link("Collections")
+    end
+  end
+  context "when user is an admin" do
+    it "Displays the collections link when the setting is off" do
+      allow(Flipflop).to receive(:enabled?).with(:show_repository_objects_links).and_return(false)
+      visit '/dashboard'
+      expect(page).to have_link("Collections")
     end
 
     it "Does display the collections link when the setting is on" do
