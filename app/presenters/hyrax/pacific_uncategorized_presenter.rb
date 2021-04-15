972472d85e7ce76d372c92c1f5233bdd3f939cfc
BertZZ <bertie.wooles@gmail.com>
2021-04-15 16:11:39 +0100

    Adds version number to uncategorised presenter

--- app/presenters/hyrax/pacific_uncategorized_presenter.rb
+++ app/presenters/hyrax/pacific_uncategorized_presenter.rb
@@ -6,7 +6,7 @@ module Hyrax
 
     def self.delegated_methods
       %i[title alt_title resource_type creator contributor abstract page_display_order_number official_link
-         date_published duration version pagination is_included_in volume_number issue journal_title publisher isbn
+         date_published duration version_number pagination is_included_in volume_number issue journal_title publisher isbn
          issn additional_links rights_holder license location doi degree org_unit subject keyword refereed irb_status
          irb_number add_info].freeze
     end
