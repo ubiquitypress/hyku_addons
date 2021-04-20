0fdf7fb11c1921c88025f0c2182a021755a0edfc
Paul Danelli <prdanelli@gmail.com>
2021-04-20 17:29:14 +0100

    Specing out the functionality

--- app/actors/hyku_addons/actors/member_collection_from_admin_set_actor.rb
+++ app/actors/hyku_addons/actors/member_collection_from_admin_set_actor.rb
@@ -1,29 +1,30 @@
-# frozen_string_litera: true
+# frozen_string_literal: true
 
 module HykuAddons
   module Actors
     class MemberCollectionFromAdminSetActor < Hyrax::Actors::AbstractActor
       def create(env)
-        ensure_collection!(env)
+        ensure_collection!(env) if enabled?
+
         next_actor.create(env)
       end
 
       def update(env)
-        ensure_collection!(env)
+        ensure_collection!(env) if enabled?
+
         next_actor.update(env)
       end
 
       protected
 
+      def enabled?
+        Flipflop.enabled?(:simplified_admin_set_selection)
+      end
+
       def ensure_collection!(env)
-        return unless Flipflop.enabled?(:simplified_admin_set_selection)
-
-        # byebug
-        # admin_set_id = params.dig("admin_set_id")
-        # model = f.object.model.class.to_s.underscore
-        # title = AdminSet.find(admin_set_id).title.first
-        # collection_id = Collection.where(title: title).first&.id
-
+        title = AdminSet.find(env.attributes.dig("admin_set_id")).title.first
+        collection = Collection.where(title: title)
+        env.curation_concern.member_of_collections << collection
       end
     end
   end
--- spec/actors/hyku_addons/actors/member_collection_from_admin_set_actor_spec.rb
+++ spec/actors/hyku_addons/actors/member_collection_from_admin_set_actor_spec.rb
@@ -3,21 +3,28 @@
 require "rails_helper"
 
 RSpec.describe HykuAddons::Actors::MemberCollectionFromAdminSetActor do
+  let(:rand) { SecureRandom.uuid }
+  let(:attributes) { {} }
+  let(:flipflop_name) { :simplified_admin_set_selection }
   let(:user) { build(:user) }
   let(:ability) { ::Ability.new(user) }
-  let(:work) { build(:generic_work) }
-  let(:admin_set) { build(:admin_set, id: "admin_set_1") }
-  let(:attributes) { {} }
+  let(:work) { create(:generic_work) }
   let(:env_class) { Hyrax::Actors::Environment }
   let(:env) { env_class.new(work, ability, attributes) }
   let(:terminator) { Hyrax::Actors::Terminator.new }
+
+  let(:admin_set) { create(:admin_set, id: "admin_set_1", title: ["Test Name #{rand}"]) }
+  # let(:admin_set_2) { create(:admin_set, id: "admin_set_2", title: ["Test Name Two"]) }
+
+  # TODO: Update this to use the `collection_lw` because this legacy factory is super slow
+  let(:collection) { create(:collection, title: ["Test Name #{rand}"]) }
 
   # This is an alternative to the subject(:middleware) below, but won't test the middleware chain.
   # Use it with: `actor.create(env)`
   #
   # let(:actor) { described_class.new(terminator) }
 
-  subject(:middleware) do
+  let(:middleware) do
     stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
       middleware.use described_class
     end
@@ -27,14 +34,56 @@ RSpec.describe HykuAddons::Actors::MemberCollectionFromAdminSetActor do
 
   before do
     allow(Flipflop).to receive(:enabled?).and_call_original
-    allow(Flipflop).to receive(:enabled?).with(:simplified_admin_set_selection).and_return(true)
+    allow(Flipflop).to receive(:enabled?).with(flipflop_name).and_return(true)
   end
 
-  describe "#create" do
-    it "called the terminator" do
-      expect(terminator).to receive(:create).with(env_class)
+  context "when the flipflop is enabled" do
+    let(:attributes) { { admin_set_id: admin_set.id } }
 
-      middleware.create(env)
+    describe "#create" do
+      it "called the terminator" do
+        expect(terminator).to receive(:create).with(env_class)
+
+        middleware.create(env)
+      end
+
+      context "" do
+        let(:rand) { SecureRandom.uuid }
+
+        # Create these two at the same time and ensure their titles match
+        before do
+          collection
+          admin_set
+        end
+
+        it "adds the work to the collection" do
+          expect(work.member_of_collections).to be_empty
+          middleware.create(env)
+          expect(work.member_of_collections).not_to be_empty
+          expect(work.member_of_collections).to include(collection)
+        end
+      end
+
+    end
+  end
+
+  context "when the flipflop is disabled" do
+    before do
+      allow(Flipflop).to receive(:enabled?).with(flipflop_name).and_return(false)
+    end
+
+    describe "create" do
+      it "calls the termintor" do
+        allow(terminator).to receive(:create)
+        middleware.create(env)
+        expect(terminator).to have_received(:create).with(env_class)
+      end
+
+      it "doesn't call ensure_collection!" do
+        allow(middleware).to receive(:ensure_collection!)
+        middleware.create(env)
+        expect(middleware).not_to have_received(:ensure_collection!)
+      end
     end
   end
 end
