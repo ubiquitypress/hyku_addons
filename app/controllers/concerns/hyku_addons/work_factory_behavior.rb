103f9e4def01ed8780a9312395c9a73ee5fa385f
BertZZ <bertie.wooles@gmail.com>
2021-04-20 11:08:53 +0100

    Fixes Rubocop and nesting errors

--- app/controllers/concerns/hyku_addons/work_factory_behavior.rb
+++ app/controllers/concerns/hyku_addons/work_factory_behavior.rb
@@ -6,7 +6,7 @@ module HykuAddons
       work = Hyrax::Hirmeos::Client::Work.new
       work.title = resource.title
       work.uri = [{ uri: resource_url(resource), canonical: true },
-                  { uri: frontend_resource_url(resource)},
+                  { uri: frontend_resource_url(resource) },
                   { uri: "urn:uuid:#{resource.id}" }]
       work.type = "repository-work"
       work
@@ -16,15 +16,15 @@ module HykuAddons
       work_types = current_account.google_scholarly_work_types
       work_types ||= []
       if work_types.include?(work.work_type) ? build_scholarly_link(work) : build_non_scholarly_link(work)
+      end
     end
 
     def self.build_scholarly_link(work)
-      "https://#{current_account.cname.split("dashboard.").last}/work/sc/#{work.id}"
+      "https://#{current_account.cname.split('dashboard.').last}/work/sc/#{work.id}"
     end
 
     def self.build_non_scholarly_link(work)
-      "https://#{current_account.cname.split("dashboard.").last}/work/ns/#{work.id}"
+      "https://#{current_account.cname.split('dashboard.').last}/work/ns/#{work.id}"
     end
   end
-end
 end
