4ff9194b02d7f45c2bcf7f392124785de78d9580
Eneko Taberna <eneko.taberna@gmail.com>
2021-04-28 18:09:10 +0200

    Adds basic validaiton queries

---
+++ app/models/concerns/hyku_addons/bulkrax_entry_behavior.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Customer organization account
+module HykuAddons
+  module BulkraxEntryBehavior
+    extend ActiveSupport::Concern
+
+    included do
+      has_one :latest_status,
+              ->{ order(created_at: :desc) },
+              class_name: "Bulkrax::Status",
+              as: :statusable
+      # scope :with_issues_on_field, lambda{ |field| where("error_backtrace LIKE ?", "%#{field}%") }
+    end
+  end
+end
---
+++ app/queries/hyku_addons/base_query.rb
@@ -0,0 +1,15 @@
+module HykuAddons
+  class BaseQuery
+    attr_reader :relation
+
+    def self.call(relation = nil, args)
+      new(relation).call(*args)
+    end
+
+    def initialize(relation = nil)
+      raise ArgumentError, "You must provide a base relation" unless relation.present?
+      @relation = relation
+    end
+  end
+end
+
