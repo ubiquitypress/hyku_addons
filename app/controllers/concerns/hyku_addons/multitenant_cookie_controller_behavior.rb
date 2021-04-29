baba355c83db52ff754bac7539edbb544c583f7f
Eneko Taberna <eneko.taberna@gmail.com>
2021-04-29 13:07:55 +0200

    Appease rubocop

--- app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
@@ -15,9 +15,7 @@ module HykuAddons
         # Looks like we don't need this. I leave it here for now in case the nearby future proves me wrong.
         # request.env['rack.session.options'][:key] = "#{current_account&.name}_hyku_session"
         tenant_host = current_account&.frontend_url&.presence || current_account&.cname&.presence
-        if tenant_host.present?
-          request.env['rack.session.options'][:domain] = ".#{tenant_host}"
-        end
+        request.env['rack.session.options'][:domain] = ".#{tenant_host}" if tenant_host.present?
         true
       end
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,9 +3,7 @@
 Warden::Manager.before_logout do |user, auth, _opts|
   current_account = Account.find_by_cname auth.request.host
   tenant_host = current_account&.frontend_url&.presence || current_account&.cname&.presence
-  if tenant_host.present?
-    auth.request.env['rack.session.options'][:domain] = ".#{tenant_host}"
-  end
+  auth.request.env['rack.session.options'][:domain] = ".#{tenant_host}" if tenant_host.present?
   HykuAddons::JwtCookiesService.new(user, auth).remove_jwt_cookies
 end
 
