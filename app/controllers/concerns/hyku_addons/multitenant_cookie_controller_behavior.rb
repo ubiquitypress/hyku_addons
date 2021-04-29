fe94de248e51cc4b48cba3420960d32899bd51d0
Eneko Taberna <eneko.taberna@gmail.com>
2021-04-29 13:05:43 +0200

    Fix super admin cookie domains, force tenant cookie domain to account frontend_url or cname

--- app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
@@ -14,7 +14,11 @@ module HykuAddons
       def set_tenant_cookie_options
         # Looks like we don't need this. I leave it here for now in case the nearby future proves me wrong.
         # request.env['rack.session.options'][:key] = "#{current_account&.name}_hyku_session"
-        request.env['rack.session.options'][:domain] = ".#{current_account&.frontend_url || current_account&.cname}"
+        tenant_host = current_account&.frontend_url&.presence || current_account&.cname&.presence
+        if tenant_host.present?
+          request.env['rack.session.options'][:domain] = ".#{tenant_host}"
+        end
+        true
       end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 #
 Warden::Manager.before_logout do |user, auth, _opts|
+  current_account = Account.find_by_cname auth.request.host
+  tenant_host = current_account&.frontend_url&.presence || current_account&.cname&.presence
+  if tenant_host.present?
+    auth.request.env['rack.session.options'][:domain] = ".#{tenant_host}"
+  end
   HykuAddons::JwtCookiesService.new(user, auth).remove_jwt_cookies
 end
 
