aa0c2cfbabb81b4fdb023fb388e2bb99503745f3
Eneko Taberna <eneko.taberna@gmail.com>
2021-04-30 12:24:20 +0200

    Appease rubocop

--- app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
@@ -16,14 +16,12 @@ module HykuAddons
         # Looks like we don't need this. I leave it here for now in case the nearby future proves me wrong.
         # request.env['rack.session.options'][:key] = "#{current_account&.name}_hyku_session"
         tenant_host = current_account&.frontend_url&.presence || current_account&.cname&.presence
-        byebug
         request.env['rack.session.options'][:domain] = ".#{tenant_host}" if tenant_host.present?
         true
       end
 
-
-    def log_tenant_cookie_options
-      Rails.logger.info "Cookie domain: #{request.env['rack.session.options'][:domain]}"
-    end
+      def log_tenant_cookie_options
+        Rails.logger.info "Cookie domain: #{request.env['rack.session.options'][:domain]}"
+      end
   end
 end
