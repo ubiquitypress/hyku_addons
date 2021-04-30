a7e2415e743b18ea815cfcf7486b3b8bc7dc2961
Eneko Taberna <eneko.taberna@gmail.com>
2021-04-30 12:14:00 +0200

    Rolls back cookie domain experiments

--- app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/multitenant_cookie_controller_behavior.rb
@@ -7,6 +7,7 @@ module HykuAddons
 
     included do
       before_action :set_tenant_cookie_options
+      after_action :log_tenant_cookie_options
     end
 
     private
@@ -15,8 +16,14 @@ module HykuAddons
         # Looks like we don't need this. I leave it here for now in case the nearby future proves me wrong.
         # request.env['rack.session.options'][:key] = "#{current_account&.name}_hyku_session"
         tenant_host = current_account&.frontend_url&.presence || current_account&.cname&.presence
+        byebug
         request.env['rack.session.options'][:domain] = ".#{tenant_host}" if tenant_host.present?
         true
       end
+
+
+    def log_tenant_cookie_options
+      Rails.logger.info "Cookie domain: #{request.env['rack.session.options'][:domain]}"
+    end
   end
 end
