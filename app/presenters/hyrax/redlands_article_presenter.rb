e6d512df89eed7615086481979573b432f30d14a
BertZZ <bertie.wooles@gmail.com>
2021-05-10 17:27:34 +0100

    Appeases Rubocop

--- app/presenters/hyrax/redlands_article_presenter.rb
+++ app/presenters/hyrax/redlands_article_presenter.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Generated via
 #  `rails generate hyrax:work RedlandsArticle`
 module Hyrax
@@ -6,8 +7,8 @@ module Hyrax
 
     def self.delegated_methods
       %i[title alt_title resource_type creator alt_email abstract keyword subject org_unit
-        language license version_number date_published journal_title volume issue
-        pagination official_link related_identifier location longitude latitude add_info].freeze
+         language license version_number date_published journal_title volume issue
+         pagination official_link related_identifier location longitude latitude add_info].freeze
     end
     include ::HykuAddons::PresenterDelegatable
   end
