c6a496dff628d1f146bd85a36c275e6d00945ffa
Paul Danelli <prdanelli@gmail.com>
2021-05-13 15:42:16 +0100

    Set specs as optional

--- spec/features/create_image_spec.rb
+++ spec/features/create_image_spec.rb
@@ -8,16 +8,20 @@ require File.expand_path('../helpers/create_work_user_context', __dir__)
 include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
-# RSpec.feature 'Create a Image', js: false do
-#   include_context 'create work user context' do
-#     let(:work_type) { "image" }
+RSpec.feature 'Create a Image', js: false do
+  before do
+    optional "fails randomnly on CI" if ENV["CI"]
+  end
 
-#     scenario do
-#       visit_new_work_page
-#       add_files_to_work
-#       add_metadata_to_work
-#       apply_work_visibility
-#       check_agreement_and_submit
-#     end
-#   end
-# end
+  include_context 'create work user context' do
+    let(:work_type) { "image" }
+
+    scenario do
+      visit_new_work_page
+      add_files_to_work
+      add_metadata_to_work
+      apply_work_visibility
+      check_agreement_and_submit
+    end
+  end
+end
--- spec/features/create_pacific_image_spec.rb
+++ spec/features/create_pacific_image_spec.rb
@@ -7,16 +7,20 @@ require File.expand_path('../helpers/create_pacific_work_user_context', __dir__)
 include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
-# RSpec.feature 'Create a PacificImage', js: false do
-#   include_context 'create pacific work user context' do
-#     let(:work_type) { "pacific_image" }
+RSpec.feature 'Create a PacificImage', js: false do
+  before do
+    optional "fails randomnly on CI" if ENV["CI"]
+  end
 
-#     scenario do
-#       visit_new_work_page
-#       add_files_to_work
-#       add_metadata_to_work
-#       apply_work_visibility
-#       check_agreement_and_submit
-#     end
-#   end
-# end
+  include_context 'create pacific work user context' do
+    let(:work_type) { "pacific_image" }
+
+    scenario do
+      visit_new_work_page
+      add_files_to_work
+      add_metadata_to_work
+      apply_work_visibility
+      check_agreement_and_submit
+    end
+  end
+end
