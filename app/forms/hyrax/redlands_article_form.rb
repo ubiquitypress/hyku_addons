3e221a12232f6a100004c1be3ae085724414661a
BertZZ <bertie.wooles@gmail.com>
2021-05-14 15:32:00 +0100

    Implements deposit form feedback for Redlands Article

--- app/forms/hyrax/redlands_article_form.rb
+++ app/forms/hyrax/redlands_article_form.rb
@@ -7,9 +7,9 @@ module Hyrax
     include ::HykuAddons::WorkForm
 
     self.model_class = ::RedlandsArticle
-    add_terms %i[title alt_title resource_type creator contributor alt_email abstract keyword subject
+    add_terms %i[title alt_title resource_type creator alt_email abstract keyword subject
                  org_unit language license version_number date_published journal_title
-                 volume issue pagination official_link related_identifier location
+                 volume issue pagination official_link related_identifier contributor location
                  longitude latitude add_info]
     self.terms -= %i[rights_statement publisher related_url]
     self.required_fields = %i[title creator alt_email resource_type abstract keywork subject
