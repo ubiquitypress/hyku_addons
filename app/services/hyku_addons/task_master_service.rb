d019fd2ba11e9d4b861b2073d544da0fd7c31a4a
Paul Danelli <prdanelli@gmail.com>
2021-05-17 10:50:24 +0100

    Setup basic functions

--- app/services/hyku_addons/task_master_service.rb
+++ app/services/hyku_addons/task_master_service.rb
@@ -33,17 +33,40 @@ module HykuAddons
   class TaskMasterService
     KEY_FILE_CONTENT = ENV["PUBSUB_KEY"] # content of the service account key file
     PROJECT_ID = "up-tools"
-    TOPICS = [
-      "repository--work-submission",
-      "repository--tenant-submission",
-      "repository--file-submission"
-    ].freeze
+    # repository--<type>-<action>: repository--work-submission
+    TOPICS = %i[submission update destroy].freeze
 
     def initialize(work_id)
       @work = ActiveFedora::Base.find(work_id)
     end
 
     def perform
+      byebug
+    end
+
+    protected
+
+    def work_metadata
+      {
+        'tenant': Site.instance.account.tenant,
+        'uuid': @work.id,
+        'metadata': {
+          'title': @work.title.first,
+          'etc': {}
+        }
+      }.to_json
+    end
+
+    def file_metadata(fileset)
+      {
+        'uuid': filset.id,
+        'work': @work.id,
+        'name': fileset.label,
+        'metadata': {
+          'title': filset.title.first,
+          'etc': {}
+        }
+      }
     end
   end
 end
---
+++ spec/services/hyku_addons/task_master_service_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HykuAddons::TaskMasterService do
+  context "" do
+    it "" do
+    end
+  end
+end
