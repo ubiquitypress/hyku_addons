4cdcf76c3d19cd100fc7907eaafe15e3e0fb50ce
Bertie Wooles <bertie.wooles@ubiquitypress.com>
2021-05-17 17:00:24 +0100

    Creates Redlands Book (#283)
    * Creates Redlands Book
    
    * Adds Magic Comment

---
+++ app/actors/hyrax/actors/redlands_book_actor.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work RedlandsBook`
+module Hyrax
+  module Actors
+    class RedlandsBookActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
