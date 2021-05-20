3d44660c3278e23fc8b3aeb36af5222bbfe054cd
Paul Danelli <prdanelli@gmail.com>
2021-05-20 14:23:12 +0100

    Fix specs

--- spec/models/concerns/hyku_addons/task_master/file_set_behavior_spec.rb
+++ spec/models/concerns/hyku_addons/task_master/file_set_behavior_spec.rb
@@ -3,42 +3,32 @@
 require 'spec_helper'
 
 RSpec.describe HykuAddons::TaskMaster::FileSetBehavior do
-  subject(:file) { create(:file_set, user: user, title: ['A Contained PDF FileSet'], label: 'filename.pdf') }
+  subject(:file_set) { create(:file_set, user: user, title: ['A Contained PDF FileSet'], label: 'filename.pdf') }
   let(:user) { create(:user) }
-  let(:work) { create(:task_master_work, :with_one_file) }
-  let(:model_class) do
-    # rubocop:disable Lint/UnneededDisable
-    # rubocop:disable RSpec/DescribedClass
-    FileSet.class_eval do
-      include HykuAddons::TaskMaster::FileSetBehavior
-    end
-    # rubocop:enable RSpec/DescribedClass
-    # rubocop:enable Lint/UnneededDisable
-  end
+  let(:work) { create(:task_master_work) }
 
   before do
-    work.ordered_members << subject
+    work.ordered_members << file_set
     work.save
   end
 
   describe "#to_task_master" do
     it "returns an object" do
-      expect(subject.to_task_master).to be_a(Hash)
-      expect(subject.to_task_master[:uuid]).to eq file.id
-      expect(subject.to_task_master[:work]).to eq work.id
+      expect(file_set.to_task_master).to be_a(Hash)
+      expect(file_set.to_task_master[:uuid]).to eq file_set.id
+      expect(file_set.to_task_master[:work]).to eq work.id
     end
   end
 
   describe "#task_master_uuid" do
-    it "is the file tenant" do
-      expect(subject.task_master_uuid).to eq file.id
+    it "is the file set ID" do
+      expect(file_set.task_master_uuid).to eq file_set.id
     end
   end
 
   describe "#task_master_type" do
     it "is file" do
-      expect(subject.task_master_type).to eq "file"
+      expect(file_set.task_master_type).to eq "file"
     end
   end
 end
-
