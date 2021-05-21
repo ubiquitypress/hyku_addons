4945fc01269056d567b5a4a00dd2490899a6443d
Bertie Wooles <bertie.wooles@ubiquitypress.com>
2021-05-21 12:29:00 +0100

    Creates Redlands Reports  (#291)
    * Adds required tag to date fields
    
    * Moves Thesis search view and creates Redlands Conferences work type
    
    * Refactors Redlands date fields
    
    * Apease Rubocop
    
    * Update app/views/redlands_books/edit_fields/_date_published.html.erb
    
    Co-authored-by: Eneko Taberna <eneko.taberna@gmail.com>
    
    * Update app/views/redlands_articles/edit_fields/_date_published.html.erb
    
    Co-authored-by: Eneko Taberna <eneko.taberna@gmail.com>
    
    * Update app/views/hyrax/redlands_conferences_reports_and_papers/_redlands_conferences_reports_and_paper.html.erb
    
    Co-authored-by: Eneko Taberna <eneko.taberna@gmail.com>
    
    Co-authored-by: Eneko Taberna <eneko.taberna@gmail.com>

---
+++ app/actors/hyrax/actors/redlands_conferences_reports_and_paper_actor.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work RedlandsConferencesReportsAndPaper`
+module Hyrax
+  module Actors
+    class RedlandsConferencesReportsAndPaperActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
---
+++ app/forms/hyrax/redlands_conferences_reports_and_paper_form.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work RedlandsConferencesReportsAndPaper`
+module Hyrax
+  # Generated form for RedlandsConferencesReportsAndPaper
+  class RedlandsConferencesReportsAndPaperForm < Hyrax::Forms::WorkForm
+    include ::HykuAddons::WorkForm
+
+    self.model_class = ::RedlandsConferencesReportsAndPaper
+    add_terms %i[title alt_title resource_type creator alt_email abstract keyword subject
+                 org_unit language license publisher date_published event_location official_link
+                 contributor location longitude latitude add_info]
+    self.terms -= %i[rights_statement related_url]
+    self.required_fields = %i[title creator alt_email resource_type abstract keyword subject
+                              org_unit language license version_number date_published journal_title]
+
+    include Hyrax::DOI::DOIFormBehavior
+    include Hyrax::DOI::DataCiteDOIFormBehavior
+
+    def primary_terms
+      %i[title alt_title resource_type creator alt_email abstract keyword subject
+         org_unit language license publisher date_published event_location]
+    end
+
+    def self.build_permitted_params
+      super.tap do |permitted_params|
+        permitted_params << common_fields
+        permitted_params << [:alt_title, :alt_email, :volume, :journal_title, :org_unit, :subject, :keyword,
+                             :language, :event_location, :location, :longitude, :latitude, :license]
+      end
+    end
+  end
+end
