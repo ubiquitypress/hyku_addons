05efd4d76354a35ed0f295dd7769c02d466899fc
Bertie Wooles <bertie.wooles@ubiquitypress.com>
2021-05-21 16:42:54 +0100

    Adds Student Work and missing resource type authorities (#293)
    * Adds Student Work and missing resource type lists
    
    * Fixes Rubocop issues
    
    * removes full stop from magic comment

---
+++ app/actors/hyrax/actors/redlands_student_work_actor.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work RedlandsStudentWork`
+module Hyrax
+  module Actors
+    class RedlandsStudentWorkActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
---
+++ app/forms/hyrax/redlands_student_work_form.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work RedlandsStudentWork`
+module Hyrax
+  # Generated form for RedlandsStudentWork
+  class RedlandsStudentWorkForm < Hyrax::Forms::WorkForm
+    include ::HykuAddons::WorkForm
+
+    self.model_class = ::RedlandsStudentWork
+    add_terms %i[title alt_title resource_type creator alt_email abstract keyword subject
+                 qualification_name org_unit language license publisher date_published contributor location
+                 longitude latitude advisor add_info]
+    self.terms -= %i[rights_statement related_url]
+    self.required_fields = %i[title alt_title resource_type creator alt_email abstract keyword subject
+                              org_unit language license publisher date_published]
+
+    include Hyrax::DOI::DOIFormBehavior
+    include Hyrax::DOI::DataCiteDOIFormBehavior
+
+    def primary_terms
+      %i[title alt_title resource_type creator alt_email abstract keyword subject
+         qualification_name org_unit language license publisher date_published]
+    end
+
+    def self.build_permitted_params
+      super.tap do |permitted_params|
+        permitted_params << common_fields
+        permitted_params << [:alt_title, :alt_email, :volume, :journal_title, :org_unit, :subject, :keyword,
+                             :language, :location, :longitude, :latitude, :license, :advisor, :qualification_name]
+      end
+    end
+  end
+end
