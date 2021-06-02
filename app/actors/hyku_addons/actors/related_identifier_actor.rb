ecb6677e5de5c8d579fb227f795514757049ca84
BertZZ <bertie.wooles@gmail.com>
2021-06-02 15:41:41 +0100

    Appease Rubocop

--- app/actors/hyku_addons/actors/related_identifier_actor.rb
+++ app/actors/hyku_addons/actors/related_identifier_actor.rb
@@ -2,7 +2,6 @@
 module HykuAddons
   module Actors
     class RelatedIdentifierActor < Hyrax::Actors::AbstractActor
-
       def create(env)
         add_related_identifier_data(env) && next_actor.create(env)
       end
@@ -13,18 +12,18 @@ module HykuAddons
 
       private
 
-      def add_related_identifier_data(env)
-        return unless env.curation_concern.class.to_s.include? "Redlands"
-        identifier_hash = JSON.parse(env.attributes[:related_identifier].first).first
-        env.attributes[:related_identifier] = [add_related_metadata(identifier_hash)]
-      end
+        def add_related_identifier_data(env)
+          return unless env.curation_concern.class.to_s.include? "Redlands"
+          identifier_hash = JSON.parse(env.attributes[:related_identifier].first).first if env.attributes[:related_identifier]
+          env.attributes[:related_identifier] = [add_related_metadata(identifier_hash)]
+        end
 
-      def add_related_metadata(identifier_hash)
-        return unless identifier_hash["related_identifier"].present?
-        identifier_hash["related_identifier_type"] = "PMID"
-        identifier_hash["relation_type"] = "HasMetadata"
-        [identifier_hash].to_json
-      end
+        def add_related_metadata(identifier_hash)
+          return unless identifier_hash["related_identifier"].present?
+          identifier_hash["related_identifier_type"] = "PMID"
+          identifier_hash["relation_type"] = "HasMetadata"
+          [identifier_hash].to_json
+        end
     end
   end
 end
