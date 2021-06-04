ef6e27ac3d617caadc636a9d1a9bd0c5dd6ebb7c
BertZZ <bertie.wooles@gmail.com>
2021-06-04 13:55:55 +0100

    Strengthens guards to stop works not saving

--- app/actors/hyku_addons/actors/related_identifier_actor.rb
+++ app/actors/hyku_addons/actors/related_identifier_actor.rb
@@ -14,12 +14,12 @@ module HykuAddons
 
         def add_related_identifier_data(env)
           return unless env.curation_concern.class.to_s.include? "Redlands"
-          identifier_hash = JSON.parse(env.attributes[:related_identifier].first).first if env.attributes[:related_identifier]
+          identifier_hash = JSON.parse(env.attributes[:related_identifier].first).first if env.attributes[:related_identifier].present?
           env.attributes[:related_identifier] = Array.wrap(add_related_metadata(identifier_hash))
         end
 
         def add_related_metadata(identifier_hash)
-          return unless identifier_hash["related_identifier"].present?
+          return unless identifier_hash.present? && identifier_hash["related_identifier"].present?
           identifier_hash["related_identifier_type"] = "PMID"
           identifier_hash["relation_type"] = "HasMetadata"
           Array.wrap(identifier_hash).to_json
