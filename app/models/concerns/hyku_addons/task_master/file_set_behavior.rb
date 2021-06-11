edc57f1f6608d0a6b925d5f798dd220c08beec9a
Paul Danelli <prdanelli@gmail.com>
2021-06-11 15:15:01 +0100

    Use different hook for task master file sets, so that we have out metadata available

--- app/models/concerns/hyku_addons/task_master/file_set_behavior.rb
+++ app/models/concerns/hyku_addons/task_master/file_set_behavior.rb
@@ -7,6 +7,10 @@ module HykuAddons
 
       include HykuAddons::TaskMaster::Publishable
 
+      included do
+        after_update_index :task_master_publish_file_size
+      end
+
       def task_master_type
         "file"
       end
@@ -20,7 +24,7 @@ module HykuAddons
           uuid: task_master_uuid,
           work: work_id,
           name: label,
-          metadata: attributes
+          metadata: attributes.merge(file_set_charcterizations)
         }
       end
 
@@ -31,8 +35,30 @@ module HykuAddons
 
       protected
 
+        # FileSets will not have their metadata until after this has already ran 5 times (after 5 updates inside of
+        # the actor stack. It's better to use the FileSet after_update_index hook which runs after metadata is added
+        def publish_upsert; end
+
+        def task_master_publish_file_size
+          return unless analysed?
+
+          publish(task_master_type, "upsert", to_task_master)
+        end
+
+        def file_set_charcterizations
+          characterization_terms.map do |term|
+            next unless characterization_proxy.respond_to?(term)
+
+            [term, characterization_proxy.send(term)]
+          end.compact.to_h
+        end
+
         def work_id
           member_of_works&.first&.id
+        end
+
+        def analysed?
+          original_file.present? && original_file.file_size.present?
         end
     end
   end
