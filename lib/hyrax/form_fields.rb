4792e1b85d8fa007e6b084bea8d8bc6df3f1d854
Chris Colvard <chris.colvard@gmail.com>
2021-06-15 16:37:34 -0400

    Model, form, and indexer mixins to load configuration from metadata yaml

---
+++ lib/hyrax/form_fields.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Hyrax
+  ##
+  # @api public
+  #
+  # @param [Symbol] schema_name
+  #
+  # @since 3.0.0
+  def self.FormFields(schema_name, **options)
+    Hyrax::FormFields.new(schema_name, **options)
+  end
+
+  ##
+  # @api private
+  #
+  # @see .FormFields
+  class FormFields < Module
+    attr_reader :name
+
+    ##
+    # @api private
+    #
+    # @param [Symbol] schema_name
+    # @param [#form_definitions_for] definition_loader
+    #
+    # @note use Hyrax::FormFields(:my_schema) instead
+    def initialize(schema_name, definition_loader: SimpleSchemaLoader.new)
+      @name = schema_name
+      @definition_loader = definition_loader
+    end
+
+    ##
+    # @return [Hash{Symbol => Hash{Symbol => Object}}]
+    def form_field_definitions
+      @definition_loader.form_definitions_for(schema: name)
+    end
+
+    ##
+    # @return [String]
+    def inspect
+      "#{self.class}(#{@name})"
+    end
+
+    private
+
+      def included(descendant)
+        super
+
+        form_field_definitions.each do |field_name, options|
+          descendant.terms += [field_name.to_sym]
+          descendant.required_terms += [field_name.to_sym] if options[:required]
+          descendant.primary_terms += [field_name.to_sym] if options[:primary]
+        end
+      end
+  end
+end
