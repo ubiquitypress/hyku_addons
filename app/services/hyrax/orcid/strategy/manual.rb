2e80397f21d811adebe19feae7444bbc7bdb2e18
Paul Danelli <prdanelli@gmail.com>
2021-06-16 17:51:12 +0100

    Typo

--- app/services/hyrax/orcid/strategy/manual.rb
+++ app/services/hyrax/orcid/strategy/manual.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# This strategy exists so that the user doens't need to remove the authorisation to prevent works being synced,
-# or notifications being added to their account, which could be annoying, but still be able to have their
+# This strategy exists so that the user doesn't need to remove the authorisation to prevent works being synced,
+# or notifications being added to their account - which could be annoying - but still be able to have their
 # personal/professional history shown on the public profile,
 module Hyrax
   module Orcid
