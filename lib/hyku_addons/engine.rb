92c381df41c62d57110b8a9a453299c74d29a463
Eneko Taberna <eneko.taberna@gmail.com>
2021-06-17 17:41:58 +0200

    Fixes exception on highlights screen. TODO: Delete this after merging the caching (#352)

--- lib/hyku_addons/engine.rb
+++ lib/hyku_addons/engine.rb
@@ -579,6 +579,18 @@ module HykuAddons
       Hyrax::DOI::HyraxDOIController.include HykuAddons::DOIControllerBehavior
       ::ApplicationController.include HykuAddons::MultitenantLocaleControllerBehavior
       ::Hyku::API::V1::FilesController.include HykuAddons::FilesControllerBehavior
+      ::Hyku::API::V1::HighlightsController.class_eval do
+        def index
+          @collections = collections(rows: 6)
+          @recent_documents = recent_documents(rows: 6)
+          @featured_works_list = FeaturedWorkList.new.featured_works.select { |fw| current_ability.can? :read, fw.work_id }
+          @featured_works = Hyrax::PresenterFactory.build_for(ids: @featured_works_list.map(&:work_id),
+                                                              presenter_class: Hyrax::WorkShowPresenter,
+                                                              presenter_args: current_ability)
+          collection_search_builder = Hyrax::CollectionSearchBuilder.new(self).with_access(:read).rows(1_000_000)
+          @collection_docs = repository.search(collection_search_builder).documents
+        end
+      end
       ::ActiveJob::Base.include HykuAddons::ImportMode
     end
 
