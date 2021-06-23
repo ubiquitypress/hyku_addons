2349764c7df6a21ae2814247a4b4cefbc459a2b4
BertZZ <bertie.wooles@gmail.com>
2021-06-23 13:58:27 +0100

    Refactors creator to make more fields default

--- app/helpers/hyku_addons/creator_field_helper.rb
+++ app/helpers/hyku_addons/creator_field_helper.rb
@@ -3,32 +3,42 @@
 module HykuAddons
   module CreatorFieldHelper
     def add_pacific_creator_personal_fields(array)
-      array.insert(1, field_type: :text, field_slug: :creator_middle_name)
-      array.insert(3, field_type: :text, field_slug: :creator_suffix)
-      array.insert(4, field_type: :text, field_slug: :creator_institution, field_args: { data: { required: true } })
+      array[4] = { field_type: :text, field_slug: :creator_institution, field_args: { data: { required: true } } }
       array
     end
 
     def add_redlands_creator_personal_fields(array, service_options)
-      array.delete_at(3) # removes institutional relationship from Redlands worktype
+      array.delete_at(7) # removes institutional relationship from Redlands worktype
       array[1] = { field_type: :text, field_slug: :creator_given_name }
-      array.insert(2, field_type: :text, field_slug: :creator_middle_name)
-      array.insert(3, field_type: :text, field_slug: :creator_suffix)
       array.insert(4, field_type: :select, field_slug: :creator_role, select_options: service_options, field_args: { cloneable: true, include_blank: "Please Select..." })
       array.insert(5, field_type: :text, field_slug: :creator_institution, field_args: { cloneable: true })
       array
     end
 
+    def add_anschutz_creator_personal_fields(array, service_options)
+      array.delete_at(7) # removes institutional relationship from Anschutz worktype
+      array[1] = { field_type: :text, field_slug: :creator_given_name }
+      array.insert(4, field_type: :select, field_slug: :creator_role, select_options: service_options, field_args: { cloneable: true, include_blank: "Please Select..." })
+      array[5] = { field_type: :text, field_slug: :creator_institution, field_args: { cloneable: true } }
+      array
+    end
+
+
     def remove_redlands_creator_organisational_fields(array)
+      array.delete_at(4) # Removes isni from Redlands worktype
       array.delete_at(3) # removes wikidata from Redlands worktype
       array.delete_at(2) # removes grid from Redlands worktype
       array
     end
 
+    def remove_anschutz_creator_organisational_fields(array)
+      array.delete_at(4) # Removes isni from Anschutz worktype
+      array
+    end
+
     def add_uva_creator_personal_fields(array)
-      array.delete_at(3) # removes institutional relationship from UVA worktype
+      array.delete_at(7) # removes institutional relationship from UVA worktype
       array.insert(0, field_type: :text, field_slug: :creator_computing_id)
-      array.insert(4, field_type: :text, field_slug: :creator_institution)
       array.insert(5, field_type: :text, field_slug: :creator_department)
       array
     end
