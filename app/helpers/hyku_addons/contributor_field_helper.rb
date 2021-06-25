4e2d33accd07b7e051362ec2fd965ef86b70f328
BertZZ <bertie.wooles@gmail.com>
2021-06-25 14:44:45 +0100

    Keeps ISNI in main creator block and adds methods to remove where not needed

--- app/helpers/hyku_addons/contributor_field_helper.rb
+++ app/helpers/hyku_addons/contributor_field_helper.rb
@@ -3,6 +3,7 @@
 module HykuAddons
   module ContributorFieldHelper
     def add_redlands_contributor_personal_fields(array, service_options)
+      array.delete_at(7) # removes ISNI from Redlands
       array.delete_at(6) # removes institutional relationship from Redlands worktype
       array[1] = { field_type: :text, field_slug: :contributor_given_name }
       array.insert(4, field_type: :select, field_slug: :contributor_role, select_options: service_options, field_args: { cloneable: true, include_blank: "Please Select..." })
@@ -19,9 +20,14 @@ module HykuAddons
     end
 
     def remove_redlands_contributor_organisational_fields(array)
-      array.delete_at(3) # removes wikidata from Redlands worktype
-      array.delete_at(2) # removes grid from Redlands worktype
+      array.delete_at(4) # removes isni from redlands worktypes
+      array.delete_at(3) # removes wikidata from Redlands worktypes
+      array.delete_at(2) # removes grid from Redlands worktypes
       array
+    end
+
+    def remove_anschutz_contributor_organisational_fields(array)
+      array.delete_at(4) # removes isni
     end
 
     def add_uva_contributor_personal_fields(array)
