f496675b6bb6b3a39a46c0ac77858fb875d98bf1
Eneko Taberna <eneko.taberna@gmail.com>
2021-06-30 17:01:40 +0200

    Fixes failling tests

--- spec/mailers/per_tenant_smtp_mailers_spec.rb
+++ spec/mailers/per_tenant_smtp_mailers_spec.rb
@@ -16,21 +16,31 @@ RSpec.describe Hyrax::ContactMailer, type: :feature do
 
   describe "reset_password_instructions" do
     context "with per tenant SMTP" do
-      let!(:account) { Account.create(name: 'test', tenant: 'test', cname: 'test.lvh.me', locale_name: 'test') }
+      let!(:account) do
+        Account.create(name: 'test', tenant: 'test', cname: 'test.lvh.me', locale_name: 'test',
+                       settings: { smtp_settings: smtp_settings })
+      end
       let(:mail) { described_class.contact(contact_form) }
+      let(:smtp_settings) do
+        {
+          from: 'test@test.edu',
+          address: 'test.edu',
+          user_name: 'username',
+          password: 'password',
+          domain: 'test.custom_domain.com'
+        }
+      end
 
       before do
-        allow(Settings).to receive(:smtp_settings).and_return(HykuAddons::Engine.root.join('spec', 'fixtures', 'settings', 'test-TEST.yml'))
         account.save(validate: false)
         account.switch!
-        debugger
         allow(Site).to receive(:contact_email).and_return('me@example.com')
         mail.deliver
       end
 
       it "replaces the email headers" do
         expect(mail.from).to eq ["test@test.edu"]
-        settings = mail.delivery_method.settings
+        settings = mail.delivery_method.settings.with_indifferent_access
         expect(settings[:address]).to eq "test.edu"
         expect(settings[:user_name]).to eq "username"
         expect(settings[:password]).to eq "password"
