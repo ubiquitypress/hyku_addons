ae04d9d8fe153a68ac7974f6b951506ec4e4ddde
BertZZ <bertie.wooles@gmail.com>
2021-07-09 11:37:59 +0100

    [3] Remove contributor block

--- app/forms/hyrax/anschutz_work_form.rb
+++ app/forms/hyrax/anschutz_work_form.rb
@@ -7,7 +7,7 @@ module Hyrax
     include ::HykuAddons::WorkForm
     self.model_class = ::AnschutzWork
     add_terms %i[title alt_title creator date_published date_published_text abstract resource_type license
-                 place_of_publication language subject_text mesh add_info contributor publisher source
+                 place_of_publication language subject_text mesh add_info publisher source
                  journal_frequency funding_description citation table_of_contents
                  references extent medium library_of_congress_classification committee_member
                  time rights_statement qualification_subject_text qualification_grantor qualification_level qualification_name]
