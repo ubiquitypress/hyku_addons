d670036f1a5774928f0bbad3c5e9f024ae212d80
BertZZ <bertie.wooles@gmail.com>
2021-07-27 13:39:23 +0100

    Adds Denver Thesis

---
+++ app/presenters/hyrax/denver_thesis_dissertation_capstone_presenter.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work DenverThesisDissertationCapstone`
+module Hyrax
+  class DenverThesisDissertationCapstonePresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::WorkPresenterBehavior
+
+    def self.delegated_methods
+      %i[title resource_type creator abstract keyword subject degree qualification_level
+         qualification_name advisor committee_member org_unit
+         date_published pagination alternate_identifier library_of_congress_classification
+         related_identifier publisher place_of_publication licence rights_holder rights_statement
+         contributor table_of_contents references medium extent language].freeze
+      end
+    include ::HykuAddons::PresenterDelegatable
+  end
+end
