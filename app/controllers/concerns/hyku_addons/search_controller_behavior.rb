c483cdaaf723a2425e59c626184316b912fe9dff
Paul Danelli <prdanelli@gmail.com>
2021-08-16 13:31:42 +0100

    Group the helper class and function block so its easier to read.

--- app/controllers/concerns/hyku_addons/search_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/search_controller_behavior.rb
@@ -7,17 +7,19 @@ module HykuAddons
 
     def facet
       if params[:id] == 'all'
-        language_service = HykuAddons::LanguageService.new
         # Set facet.limit to -1 for all facets when sending solr request so all facet values get returned
         solr_params = search_builder.with(params).to_h
         solr_params.each_key { |k| solr_params[k] = -1 if k.match?(/^f\..+\.limit$/) }
         @response = repository.search(solr_params)
         facets = @response.aggregations.transform_values { |v| hash_of_terms_ordered_by_hits(v.items) }
+        
+        language_service = HykuAddons::LanguageService.new
         facets['language_sim'] = facets['language_sim'].map do |id, count|
           [language_service.label(id), count]
         rescue KeyError
           nil
         end.compact.to_h
+        
         render json: facets
       else
         super
