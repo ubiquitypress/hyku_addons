a36fdc320f868ee24daaad403679a933afb0ce54
Eneko Taberna <eneko.taberna@gmail.com>
2021-08-27 13:09:47 +0200

    Customizes Mailboxer email templates

--- lib/hyku_addons/engine.rb
+++ lib/hyku_addons/engine.rb
@@ -550,6 +550,28 @@ module HykuAddons
             Rails.application.routes.url_helpers.send(key + "_url", document.id, host: Site.instance.account.cname, protocol: :https)
           end
       end
+
+      Mailboxer::MessageMailer.class_eval do
+        # Sends an email for indicating a new message for the receiver
+        def new_message_email(message, receiver)
+          @message  = message
+          @receiver = receiver
+          set_subject(message)
+          mail to: receiver.send(Mailboxer.email_method, message),
+               subject: t('mailboxer.message_mailer.subject_new', subject: @subject, tenant_name: Site.instance.application_name),
+               template_name: 'hyku_addons_new_message_email'
+        end
+
+        # Sends an email for indicating a reply in an already created conversation
+        def reply_message_email(message, receiver)
+          @message  = message
+          @receiver = receiver
+          set_subject(message)
+          mail to: receiver.send(Mailboxer.email_method, message),
+               subject: t('mailboxer.message_mailer.subject_reply', subject: @subject, tenant_name: Site.instance.application_name),
+               template_name: 'hyku_addons_reply_message_email'
+        end
+      end
     end
 
     # Use #to_prepare because it reloads where after_initialize only runs once
