4f1eaa83c1d40bd05ac618b291f3e3ac934fa418
Eneko Taberna <eneko.taberna@gmail.com>
2021-09-10 15:43:11 +0200

    Adds a comment

--- app/jobs/concerns/hyku_addons/cleanup_account_job_behavior.rb
+++ app/jobs/concerns/hyku_addons/cleanup_account_job_behavior.rb
@@ -7,6 +7,9 @@ module HykuAddons
       cleanup_solr(account)
       cleanup_fedora(account)
       cleanup_redis(account)
+      # Load the UUID on a variable before the account gets destroyed.
+      # We need to destroy the account before the tenant DB or the account after_destroy callbacks
+      # will not have the needed DB tables to make their actions.
       tenant = account.tenant
       account.destroy
       cleanup_database(tenant)
@@ -17,7 +20,7 @@ module HykuAddons
       def cleanup_database(tenant)
         Apartment::Tenant.drop(tenant)
       rescue StandardError
-        nil # ignore if account.tenant missing
+        nil
       end
   end
 end
