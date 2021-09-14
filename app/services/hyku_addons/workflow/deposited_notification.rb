91bc3dfdd8d9561e9c53217002f551ff788a854f
BertZZ <bertie.wooles@gmail.com>
2021-09-14 17:08:44 +0100

    Changes Classes into modules

--- app/services/hyku_addons/workflow/deposited_notification.rb
+++ app/services/hyku_addons/workflow/deposited_notification.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module HykuAddons
   module Workflow
-    class DepositedNotification < AbstractNotification
+    module DepositedNotification
       private
 
       def message
