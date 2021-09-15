74bb5c87b356a3a5dfd642278fe702c782d71bf6
Eneko Taberna <eneko.taberna@gmail.com>
2021-09-15 14:58:41 +0200

    REPO-1367: Adds email from alias to SMTP configuration (#464)

--- app/lib/hyku_addons/per_tenant_smtp_interceptor.rb
+++ app/lib/hyku_addons/per_tenant_smtp_interceptor.rb
@@ -3,26 +3,33 @@
 module HykuAddons
   class PerTenantSmtpInterceptor
     def self.available_smtp_fields
-      %w[from user_name password address domain port authentication enable_starttls_auto].freeze
+      %w[from from_alias user_name password address domain port authentication enable_starttls_auto].freeze
     end
 
     def self.delivering_email(message)
       Account.find_by(tenant: Apartment::Tenant.current)&.switch!
       return unless (mailer_settings = Settings.smtp_settings).present?
 
+      mailer_config = configure_mailer_tenant_settings(mailer_settings, message)
+      message.delivery_method.settings.merge!(mailer_config.compact.to_h)
+    end
+
+    def self.configure_mailer_tenant_settings(mailer_settings, message)
       if (from = mailer_settings.from).present?
-        message.from = from
+        message.from = if (from_alias = mailer_settings.from_alias).present?
+                         "#{from_alias} <#{from}>"
+                       else
+                         from
+                       end
         message.reply_to = from
         message.return_path = from
         message.smtp_envelope_from = from
       end
 
-      data = (HykuAddons::PerTenantSmtpInterceptor.available_smtp_fields - ['from']).map do |key|
+      (HykuAddons::PerTenantSmtpInterceptor.available_smtp_fields - ['from']).map do |key|
         value = mailer_settings.try(key)
         [key.to_sym, value] if value.present?
       end
-
-      message.delivery_method.settings.merge! data.compact.to_h
     end
   end
 end
