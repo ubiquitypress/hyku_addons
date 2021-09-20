8fee5e8dabf05ba6501bac1e7afae93ea74a7767
Paul Danelli <prdanelli@gmail.com>
2021-09-20 15:56:11 +0100

    Add spec for helper method

--- app/helpers/hyku_addons/helper_behavior.rb
+++ app/helpers/hyku_addons/helper_behavior.rb
@@ -9,19 +9,6 @@ module HykuAddons
     include HykuAddons::SimplifiedAdminSetSelectionWorkFormHelper
     include HykuAddons::SimplifiedDepositFormHelper
     include HykuAddons::NotesTabFormHelper
-    include Hyrax::Orcid::HelperBehavior
-
-    # Override the Hyrax::Orcid JSON Fields creator/contributor helper, method which uses different participant name keys
-    def participant_to_string(type, participants)
-      return "-" if participants.blank?
-
-      participants
-        .first
-        .then { |s| JSON.parse(s) }
-        .each_with_object([]) do |hash, array|
-          array << hash.slice("#{type}_given_name", "#{type}_family_name").values.join(" ")
-        end
-        .then { |a| a.compact.reject(&:blank?).join(", ") }
-    end
+    include HykuAddons::OrcidHelperBehavior
   end
 end
---
+++ app/helpers/hyku_addons/orcid_helper_behavior.rb
@@ -0,0 +1,23 @@
+# frozen_string_literals: true
+
+module HykuAddons
+  module OrcidHelperBehavior
+    extend ActiveSupport::Concern
+
+    include Hyrax::Orcid::HelperBehavior
+
+    # Override the Hyrax::Orcid JSON Fields creator/contributor helper, method which uses different participant name keys
+    def participant_to_string(type, participants)
+      return "-" if participants.blank?
+
+      participants
+        .first
+        .then { |s| JSON.parse(s) }
+        .each_with_object([]) do |hash, array|
+          array << hash.slice("#{type}_given_name", "#{type}_family_name").values.join(" ")
+        end
+        .then { |a| a.compact.reject(&:blank?).join(", ") }
+    end
+  end
+end
+
