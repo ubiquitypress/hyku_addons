15ef7142e209f5e6d8f0eb59e699f62b4f737e8f
edward <bauchiroad@gmail.com>
2021-09-27 17:03:15 +0100

    restore removed line from migration

--- db/migrate/20200929084240_add_parent_id_and_data_settings_column_to_account.rb
+++ db/migrate/20200929084240_add_parent_id_and_data_settings_column_to_account.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class AddParentIdAndDataSettingsColumnToAccount < ActiveRecord::Migration[5.2]
   def change
+    add_reference :accounts, :parent, foreign_key: { to_table: :accounts }
     add_column :accounts, :settings, :jsonb, default: {}
     add_column :accounts, :data, :jsonb, default: {}
     add_index  :accounts, :settings, using: :gin
