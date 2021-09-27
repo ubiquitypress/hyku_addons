d670ed6d3cfa22187d3aa0fc416dc73c7078cd57
edward <bauchiroad@gmail.com>
2021-09-27 13:57:42 +0100

    incorporate Paul's feedback

--- db/migrate/20200929084240_add_parent_id_and_data_settings_column_to_account.rb
+++ db/migrate/20200929084240_add_parent_id_and_data_settings_column_to_account.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class AddParentIdAndDataSettingsColumnToAccount < ActiveRecord::Migration[5.2]
   def change
-    # add_reference :accounts, :parent, foreign_key: { to_table: :accounts }
     add_column :accounts, :settings, :jsonb, default: {}
     add_column :accounts, :data, :jsonb, default: {}
     add_index  :accounts, :settings, using: :gin
--- spec/models/account_cross_search_spec.rb
+++ spec/models/account_cross_search_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 
 RSpec.describe AccountCrossSearch, type: :model do
   it do
-    is_expected.to belong_to(:full_account)
-      .class_name('Account')
+    is_expected.to belong_to(:full_account).class_name('Account')
 
-    is_expected.to belong_to(:search_account)
-      .class_name('Account')
+    is_expected.to belong_to(:search_account).class_name('Account')
   end
 end
