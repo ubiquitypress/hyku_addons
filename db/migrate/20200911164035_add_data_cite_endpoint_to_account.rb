d846d8007aa1f8ef38f050657889fd0a1281cea7
Paul Danelli <prdanelli@gmail.com>
2021-09-29 16:50:46 +0100

    Make sure that migrations don't fail when running

--- db/migrate/20200911164035_add_data_cite_endpoint_to_account.rb
+++ db/migrate/20200911164035_add_data_cite_endpoint_to_account.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddDataCiteEndpointToAccount < ActiveRecord::Migration[5.2]
   def change
-    add_reference :accounts, :datacite_endpoint, index: true
+    add_reference :accounts, :datacite_endpoint, index: true unless column_exists?(:accounts, :datacite_endpoint_id)
   end
 end
