7e26c8b11882311797fbd70fc2a9105f5b64815b
BertZZ <bertie.wooles@gmail.com>
2021-10-12 13:02:38 +0100

    Adds UNA Chapters and Book Sections

--- lib/hyku_addons/engine.rb
+++ lib/hyku_addons/engine.rb
@@ -459,6 +459,7 @@ module HykuAddons
         config.register_curation_concern :una_archival_item
         config.register_curation_concern :una_article
         config.register_curation_concern :una_book
+        config.register_curation_concern :una_chapters_and_book_section
         config.register_curation_concern :uva_work
 
         config.license_service_class = HykuAddons::LicenseService
