c8b2f91fb6c074ed9c1137f791138155c958ddc4
BertZZ <bertie.wooles@gmail.com>
2021-10-13 10:24:37 +0100

    Refactors book form to make model consistant

--- app/forms/hyrax/una_book_form.rb
+++ app/forms/hyrax/una_book_form.rb
@@ -9,11 +9,11 @@ module Hyrax
                  table_of_contents publisher place_of_publication date_published
                  edition isbn official_link series_name related_identifier
                  contributor location longitude latitude add_info buy_book]
-
     self.terms -= %i[source rights_statement]
     self.required_fields = %i[title resource_type creator alt_email abstract
                               keyword subject org_unit language license version_number
                               table_of_contents publisher place_of_publication date_published ]
+
     include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
