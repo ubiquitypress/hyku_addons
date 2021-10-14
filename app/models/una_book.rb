7c1b6034d8c361b08ce8c288ea819823b188bf1e
BertZZ <bertie.wooles@gmail.com>
2021-10-14 13:38:00 +0100

    Adds space

--- app/models/una_book.rb
+++ app/models/una_book.rb
@@ -58,7 +58,7 @@ class UnaBook < ActiveFedora::Base
   self.indexer = UnaBookIndexer
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
-  validates :title, presence: { message: "Your work must have a title."}
+  validates :title, presence: { message: "Your work must have a title." }
 
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
