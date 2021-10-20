4a94c9b2691db10aa2b0407f3cebf36ba59099d4
edward <bauchiroad@gmail.com>
2021-10-20 16:03:27 +0100

    move rescue block to ReindexModelJob from ReindexAvailableWorksJob

--- app/jobs/hyku_addons/reindex_available_works_job.rb
+++ app/jobs/hyku_addons/reindex_available_works_job.rb
@@ -1,31 +1,27 @@
 # frozen_string_literal: true
 module HykuAddons
   class ReindexAvailableWorksJob < ApplicationJob
-    rescue_from Ldp::Gone, RSolr::Error::Http, RSolr::Error::ConnectionRefused do |exception|
-      Rails.logger.debug exception.inspect
-      retry_job(wait: 5.minutes)
-    end
-
     # cnames is an array
     def perform(cnames)
       cnames.each do |name|
         cname = cnames.delete(name)
-        fetch_work_save_cname(cname)
+        reindex(cname)
       end
     end
 
     private
 
-      def fetch_work_save_cname(cname)
+      def reindex(cname)
         AccountElevator.switch!(cname)
         available_works = Site.first.available_works | ['Collection']
 
         available_works.each do |model_class|
           klass = model_class.constantize
-          contains_work = klass.count.positive?
 
-          Rails.logger.debug "==== Queue ReindexModelJob for #{klass} ===" if contains_work
-          HykuAddons::ReindexModelJob.perform_later(model_class, cname) if contains_work
+          next unless klass.count.positive?
+
+          Rails.logger.debug "==== Queue ReindexModelJob for #{klass} ==="
+          HykuAddons::ReindexModelJob.perform_later(model_class, cname)
         end
       end
   end
--- app/jobs/hyku_addons/reindex_model_job.rb
+++ app/jobs/hyku_addons/reindex_model_job.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 module HykuAddons
   class ReindexModelJob < ApplicationJob
+    rescue_from Ldp::Gone, RSolr::Error::Http, RSolr::Error::ConnectionRefused do |exception|
+      Rails.logger.debug exception.inspect
+      retry_job(wait: 5.minutes)
+    end
+
     def perform(klass, cname)
       AccountElevator.switch!(cname)
 
