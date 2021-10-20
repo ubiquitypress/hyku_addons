5ea43f57bf1dda9e651e23cb723b3e3b1e731705
mankind <bauchiroad@gmail.com>
2021-10-20 09:46:48 +0100

    Redirect from shared-search result to the right tenant (#518)
    * Redirect from shared-search result to the right tenant
    
    * update engine.rb
    
    * fix nil error in rspec
    
    * refactor code based on PR feedback
    
    * refactor how id is accessed in the hash

--- spec/indexers/work_indexer_spec.rb
+++ spec/indexers/work_indexer_spec.rb
@@ -38,4 +38,23 @@ RSpec.describe WorkIndexer do
       expect(solr_document.fetch('editor_display_ssim')).to eq ["University of Cambridge"]
     end
   end
+
+  context "account_cname_tesim" do
+    let(:account) { create(:account, cname: 'hyky-test.me') }
+
+    before do
+      allow(Apartment::Tenant).to receive(:switch!).with(account.tenant) do |&block|
+        block&.call
+      end
+
+      Apartment::Tenant.switch!(account.tenant) do
+        Site.update(account: account)
+        work
+      end
+    end
+
+    it "indexer has the account_cname" do
+      expect(solr_document.fetch("account_cname_tesim")).to eq(account.cname)
+    end
+  end
 end
