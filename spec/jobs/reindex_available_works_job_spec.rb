bd9e6e1e270deb4548e39c5b94b938aabda27ee9
edward <bauchiroad@gmail.com>
2021-10-21 10:47:00 +0100

    Add test for the jobs

---
+++ spec/jobs/reindex_available_works_job_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+RSpec.describe HykuAddons::ReindexAvailableWorksJob, perform_enqueued_jobs: true do
+  let(:account) { create(:account) }
+  let(:work) { create(:work) }
+  let(:site) { instance_double(Site) }
+
+  before do
+    ActiveJob::Base.queue_adapter = :test
+    allow(site).to receive(:available_works).and_return([work.class.to_s])
+
+    allow(Apartment::Tenant).to receive(:switch!).with(account.tenant) do |&block|
+      Site.update(account: account)
+      block&.call
+    end
+
+    Apartment::Tenant.switch!(account.tenant) { work }
+  end
+
+  it "#perform_later can enqueue model for reindex" do
+    expect { described_class.perform_later([account.cname]) }.to have_enqueued_job(described_class)
+  end
+
+  it "#perform_now can enqueue model for reindex" do
+    ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+    expect { described_class.perform_now([account.cname]) }.not_to have_enqueued_job(described_class)
+  end
+end
