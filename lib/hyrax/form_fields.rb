42b1b576b1033c283e42d0dc9b4321c6fcd868a3
Paul Danelli <prdanelli@gmail.com>
2021-10-22 17:59:31 +0100

    Reset all form fields to only use those required by hyrax so that we don't assume creator etc are required for a work type

--- lib/hyrax/form_fields.rb
+++ lib/hyrax/form_fields.rb
@@ -16,6 +16,14 @@ module Hyrax
   #
   # @see .FormFields
   class FormFields < Module
+    # These are the internal fields that Hyrax uses to create its forms, however they also set
+    # a bunch of actual input fields, which we may not want and can't remove later.
+    INTERNAL_FIELDS = %i[representative_id thumbnail_id rendering_ids files
+                    visibility_during_embargo embargo_release_date visibility_after_embargo
+                    visibility_during_lease lease_expiration_date visibility_after_lease
+                    visibility ordered_member_ids source in_works_ids
+                    member_of_collection_ids admin_set_id].freeze
+
     attr_reader :name
 
     ##
@@ -35,10 +43,13 @@ module Hyrax
     def form_field_definitions
       definitions = @definition_loader.form_definitions_for(schema: name)
       attributes = @definition_loader.attributes_config_for(schema: name)
+
       definitions.each_key do |d|
         next unless attributes[d].key?('subfields')
+
         definitions[d]['subfields'] = attributes[d]['subfields'].transform_values { |subfield| subfield['form'] }
       end
+
       definitions
     end
 
@@ -52,6 +63,10 @@ module Hyrax
 
       def included(descendant)
         super
+
+        # Reset the fields as we might not want all fo the Hyrax defaults.
+        # To reset the default behavior and default fields, remove this line.
+        descendant.terms = INTERNAL_FIELDS
 
         form_field_definitions.each do |field_name, options|
           descendant.terms += [field_name.to_sym]
