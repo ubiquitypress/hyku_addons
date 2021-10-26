ef4715a342d7bda81e6e2ae3fc58e8778b9367b0
Bertie Wooles <bertie.wooles@ubiquitypress.com>
2021-10-26 15:05:55 +0100

    Update una_presentation_presenter.rb

--- app/presenters/hyrax/una_presentation_presenter.rb
+++ app/presenters/hyrax/una_presentation_presenter.rb
@@ -8,7 +8,7 @@ module Hyrax
       %i[title alt_title resource_type creator alt_email abstract keyword
          subject org_unit language license publisher date_published event_location
          official_link contributor location longitude latitude add_info
-         event_title event_location event_date].freeze
+         event_title event_location event_date mentor].freeze
     end
     include ::HykuAddons::PresenterDelegatable
   end
