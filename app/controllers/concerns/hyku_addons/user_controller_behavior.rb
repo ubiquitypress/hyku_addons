63b1b46d16929852f1713b636d43bc6f9d3c8770
Bertie Wooles <bertie.wooles@gmail.com>
2021-11-09 14:00:34 +0000

    Adds guard clause and cleans up error messaging on show

--- app/controllers/concerns/hyku_addons/user_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/user_controller_behavior.rb
@@ -7,10 +7,10 @@ module HykuAddons
 
     def show
       user = User.find_by(email: params[:email])
-      @user = user if user.display_profile == true
-      render json: { status: 404, code: 'not_found', message: "This User is private" } if @user.nil?
+      @user = user if user.present? && user.display_profile == true
+      render json: { status: 404, code: 'not_found', message: "This User is either private or not found" } if @user.nil?
     rescue ActiveRecord::RecordNotFound
-      render json: { error: { status: '400', code: 'not_found', message: "Could not find user with 'id'=#{params[:id]}" } }
+      render json: { error: { status: '400', code: 'not_found', message: "Could not find user with 'email'=#{params[:email]}" } }
     end
   end
 end
