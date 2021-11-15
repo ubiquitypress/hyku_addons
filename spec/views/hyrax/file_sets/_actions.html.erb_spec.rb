0bc083c1dc00046090b0a106abcc2f3f4da33551
thenapking <tom.giles@gmail.com>
2021-11-15 13:55:13 +0000

    Use download permission instead

--- spec/views/hyrax/file_sets/_actions.html.erb_spec.rb
+++ spec/views/hyrax/file_sets/_actions.html.erb_spec.rb
@@ -13,81 +13,78 @@ RSpec.describe 'hyrax/file_sets/_actions.html.erb', type: :view do
     allow(view).to receive(:can?).with(:download, file_set.id).and_return(false)
   end
 
-  context 'with edit permission' do
+  context "with edit permission" do
     before do
       allow(view).to receive(:can?).with(:edit, file_set.id).and_return(true)
+      render 'hyrax/file_sets/actions', file_set: file_set
     end
 
-    context 'when the file is a pdf' do
-      before do
-        allow(file_set).to receive(:pdf?).and_return(true)
-        render 'hyrax/file_sets/actions', file_set: file_set
-      end
-
-      it 'allows the user to edit the file' do
-        expect(rendered).to have_css('a#file_edit')
-      end
-
-      it 'allows the user to view the file versions' do
-        expect(rendered).to have_css('a#file_versions')
-      end
-
-      it 'allows the user to read and annotate the file' do
-        expect(rendered).to have_css('a#file_read')
-        expect(rendered).to have_selector("a[href=\"https://via.hypothes.is/viewer/web/viewer.html?http://test.host/downloads/file_set_id\"]")
-      end
+    it "allows the user to edit the file" do
+      expect(rendered).to have_css("a#file_edit")
     end
 
-    context 'when the file is a not  pdf' do
-      before do
-        allow(file_set).to receive(:pdf?).and_return(false)
-        render 'hyrax/file_sets/actions', file_set: file_set
-      end
-
-      it 'allows the user to edit the file' do
-        expect(rendered).to have_css('a#file_edit')
-      end
-
-      it 'allows the user to view the file versions' do
-        expect(rendered).to have_css('a#file_versions')
-      end
-
-      it 'does not allow the user to read and annotate the file' do
-        expect(rendered).not_to have_css('a#file_read')
-      end
+    it "allows the user to view the file versions" do
+      expect(rendered).to have_css("a#file_versions")
     end
   end
 
-  context 'with destroy permission' do
+  context "with destroy permission" do
     before do
       allow(view).to receive(:can?).with(:destroy, file_set.id).and_return(true)
       render 'hyrax/file_sets/actions', file_set: file_set
     end
 
-    it 'allows the user to edit the file' do
-      expect(rendered).to have_css('a#file_destroy')
+    it "allows the user to edit the file" do
+      expect(rendered).to have_css("a#file_destroy")
     end
   end
 
-  context 'with download permission' do
+  context "with download permission" do
     before do
       allow(view).to receive(:can?).with(:download, file_set.id).and_return(true)
-      render 'hyrax/file_sets/actions', file_set: file_set
     end
 
-    it "includes google analytics data in the download link" do
-      expect(rendered).to have_css('a#file_download')
-      expect(rendered).to have_selector("a[data-label=\"#{file_set.id}\"]")
+    context "when the file is a pdf" do
+      before do
+        allow(file_set).to receive(:pdf?).and_return(true)
+        render 'hyrax/file_sets/actions', file_set: file_set
+      end
+
+      it "includes google analytics data in the download link" do
+        expect(rendered).to have_css("a#file_download")
+        expect(rendered).to have_selector("a[data-label=\"#{file_set.id}\"]")
+      end
+
+      it "allows the user to read and annotate the file" do
+        expect(rendered).to have_css("a#file_read")
+        expect(rendered).to have_selector("a[href=\"https://via.hypothes.is/viewer/web/viewer.html?http://test.host/downloads/file_set_id\"]")
+      end
+    end
+
+    context "when the file is a not  pdf" do
+      before do
+        allow(file_set).to receive(:pdf?).and_return(false)
+        render 'hyrax/file_sets/actions', file_set: file_set
+      end
+
+      it "includes google analytics data in the download link" do
+        expect(rendered).to have_css("a#file_download")
+        expect(rendered).to have_selector("a[data-label=\"#{file_set.id}\"]")
+      end
+
+      it "does not allow the user to read and annotate the file" do
+        expect(rendered).not_to have_css("a#file_read")
+      end
     end
   end
 
-  context 'with no permission' do
+  context "with no permission" do
     before do
       render 'hyrax/file_sets/actions', file_set: file_set
     end
 
     it "renders nothing" do
-      expect(rendered).to eq('')
+      expect(rendered).to eq("")
     end
   end
 end
