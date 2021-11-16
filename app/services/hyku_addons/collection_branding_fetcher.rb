6c98e48d9a06df9c3576c4248f4756504431319b
edward <bauchiroad@gmail.com>
2021-11-16 17:27:54 +0000

    add collection banner to api

---
+++ app/services/hyku_addons/collection_branding_fetcher.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module HykuAddons
+  class CollectionBrandingFetcher
+    def self.collection_banner(id)
+      @_collection_banner = collection_branding_info(id).where(role: 'banner')
+    end
+
+    private
+      def self.collection_branding_info(id)
+        @_collection_branding_info = CollectionBrandingInfo.where(collection_id: id.to_s)
+      end
+  end
+end
