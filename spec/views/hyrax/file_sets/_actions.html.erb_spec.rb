9efd0f974f8543d56cae50744a52c96a52fbf766
thenapking <tom.giles@gmail.com>
2021-11-16 09:31:37 +0000

    Add FlipFlop

--- spec/views/hyrax/file_sets/_actions.html.erb_spec.rb
+++ spec/views/hyrax/file_sets/_actions.html.erb_spec.rb
@@ -44,36 +44,55 @@ RSpec.describe 'hyrax/file_sets/_actions.html.erb', type: :view do
       allow(view).to receive(:can?).with(:download, file_set.id).and_return(true)
     end
 
-    context "when the file is a pdf" do
+    context "with annotation disabled" do
       before do
-        allow(file_set).to receive(:pdf?).and_return(true)
-        render 'hyrax/file_sets/actions', file_set: file_set
+        allow(Flipflop).to receive(:enabled?).with(:annotation).and_return(false)
       end
 
       it "includes google analytics data in the download link" do
-        expect(rendered).to have_css("a#file_download")
-        expect(rendered).to have_selector("a[data-label=\"#{file_set.id}\"]")
-      end
-
-      it "allows the user to read and annotate the file" do
-        expect(rendered).to have_css("a#file_read")
-        expect(rendered).to have_selector("a[href=\"https://via.hypothes.is/viewer/web/viewer.html?http://test.host/downloads/file_set_id\"]")
-      end
-    end
-
-    context "when the file is a not  pdf" do
-      before do
-        allow(file_set).to receive(:pdf?).and_return(false)
         render 'hyrax/file_sets/actions', file_set: file_set
-      end
 
-      it "includes google analytics data in the download link" do
         expect(rendered).to have_css("a#file_download")
         expect(rendered).to have_selector("a[data-label=\"#{file_set.id}\"]")
       end
 
       it "does not allow the user to read and annotate the file" do
         expect(rendered).not_to have_css("a#file_read")
+      end
+    end
+
+    context "with annotation enabled" do
+      before do
+        allow(Flipflop).to receive(:enabled?).with(:annotation).and_return(true)
+      end
+
+      context "when the file is a pdf" do
+        before do
+          allow(file_set).to receive(:pdf?).and_return(true)
+        end
+
+        it "allows the user to read and annotate the file" do
+          render 'hyrax/file_sets/actions', file_set: file_set
+
+          expect(rendered).to have_css("a#file_read")
+          expect(rendered).to have_selector("a[href=\"https://via.hypothes.is/viewer/web/viewer.html?http://test.host/downloads/file_set_id\"]")
+        end
+      end
+
+      context "when the file is a not  pdf" do
+        before do
+          allow(file_set).to receive(:pdf?).and_return(false)
+          render 'hyrax/file_sets/actions', file_set: file_set
+        end
+
+        it "includes google analytics data in the download link" do
+          expect(rendered).to have_css("a#file_download")
+          expect(rendered).to have_selector("a[data-label=\"#{file_set.id}\"]")
+        end
+
+        it "does not allow the user to read and annotate the file" do
+          expect(rendered).not_to have_css("a#file_read")
+        end
       end
     end
   end
