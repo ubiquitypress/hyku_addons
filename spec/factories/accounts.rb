fa47ed5941cf219f9bb65e002109e669127bbe1a
Tom Giles <tom.giles@gmail.com>
2021-11-17 17:05:40 +0000

    Flaky Hyrax::DOI::RegisterDOIJob fix (#536)
    Hyrax::DOI::RegisterDOIJob regularly fails because the cname attribute of account has not been set. Since we are not particularly concerned about this being randomised for this test, we can fix it first and create an account with a matching cname. Then the cname will always have a value and this test hopefully will stop failing.
    
    Remove double definition of account
    Fix cname value
    Refactor test for brevity
    Refactor factories

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
-FactoryBot.define do
-  factory :datacite_endpoint do
-    options { Hash.new(mode: "test", prefix: "10.1234", username: "user123", password: "pass123") }
-  end
-end
 
 FactoryBot.modify do
   factory :account do
