0847d10a306e760f5c2dd82576efbaaf131ed8bb
thenapking <tom.giles@gmail.com>
2021-11-18 14:17:57 +0000

    use symbols not strings consistently

--- spec/factories/datacite_endpoint.rb
+++ spec/factories/datacite_endpoint.rb
@@ -2,6 +2,6 @@
 
 FactoryBot.define do
   factory :datacite_endpoint do
-    options { Hash.new(mode: "test", prefix: "10.1234", username: "user123", password: "pass123") }
+    options { Hash.new(mode: :test, prefix: "10.1234", username: "user123", password: "pass123") }
   end
 end
