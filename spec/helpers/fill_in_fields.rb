6377f2bec2ef07f9f2bce65992ac02bac82b6f46
Paul Danelli <prdanelli@gmail.com>
2021-11-23 18:11:11 +0000

    Rubocop

--- spec/helpers/fill_in_fields.rb
+++ spec/helpers/fill_in_fields.rb
@@ -62,6 +62,7 @@ def fill_in_cloneable_date_field(field, values)
     end
   end
 end
+
 # Fill in a cloneable element like Creator/Contributor/Editor
 # @param field [Symbol] the field that we wish to target
 # @param values [Hash, Array] the hash (or array of hashes) where each key is the name of the subfield
