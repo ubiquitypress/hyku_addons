97c0d1ef8ee8f06c210594dd93d248445ea12ed7
Paul Danelli <prdanelli@gmail.com>
2021-11-24 17:03:22 +0000

    Rubocop

--- spec/features/ubiquity_template_work_schema_spec.rb
+++ spec/features/ubiquity_template_work_schema_spec.rb
@@ -153,7 +153,7 @@ RSpec.feature "Create a UbiquityTemplateWork", js: true do
       {
         current_he_institution_name: current_he_institution_element["label"],
         current_he_institution_ror: current_he_institution_service.select_active_options_ror[current_he_institution_index],
-        current_he_institution_isni: current_he_institution_service.select_active_options_isni[current_he_institution_index],
+        current_he_institution_isni: current_he_institution_service.select_active_options_isni[current_he_institution_index]
       }
     ]
   end
