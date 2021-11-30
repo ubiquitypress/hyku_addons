7dcc6f499a09b32c06b7613c205f0c6f87f10dc1
thenapking <tom.giles@gmail.com>
2021-11-30 13:25:20 +0000

    Refactor tests

--- spec/jobs/reindex_model_job_spec.rb
+++ spec/jobs/reindex_model_job_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe HykuAddons::ReindexModelJob do
   let(:work) { create(:work) }
 
   before do
-    ActiveJob::Base.queue_adapter = :inline
     allow(work.class).to receive(:reindex_everything)
+
     allow(Apartment::Tenant).to receive(:switch!).with(account.tenant) do |&block|
       block&.call
     end
@@ -14,11 +14,11 @@ RSpec.describe HykuAddons::ReindexModelJob do
     Apartment::Tenant.switch!(account.tenant) { work }
   end
 
-  it "#perform_later can enqueue model for reindex" do
-    expect { described_class.perform_later(work.class.to_s, account.cname) }.to have_enqueued_job(described_class)
+  it "#perform_later can enqueue model for reindex later" do
+    expect { described_class.perform_later(work.class.to_s, account.cname) }.to have_enqueued_job(described_class).exactly(:once)
   end
 
-  it "#perform_now can enqueue model for reindex" do
-    expect { described_class.perform_now(work.class.to_s, account.cname) }.not_to have_enqueued_job(described_class)
+  it "#perform_now will enqueue model for reindex" do
+    expect { described_class.perform_now(work.class.to_s, account.cname) }.to have_enqueued_job(described_class).exactly(:once)
   end
 end
