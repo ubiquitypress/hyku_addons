810a6da218a2e9fff198a395891ffaa231b51220
mankind <bauchiroad@gmail.com>
2021-12-07 16:59:16 +0000

    refactor reindex_available_works_job & reindex_model_jobs to accept ids for works to index (#566)
    * refactor reindex_available_works_job & reindex_model_jobs to accept ids as well
    
    * dont set work visibility to public
    
    * incorporating feedbacks
    
    * updated RSPEC test for ReindexAvailableWorksJob and ReindexModelJobs
    
    * changes from suggestions
    
    * refactor how hash set

--- spec/jobs/reindex_model_job_spec.rb
+++ spec/jobs/reindex_model_job_spec.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe HykuAddons::ReindexModelJob do
-  let(:account) { create(:account) }
-  let(:work) { create(:work) }
+  let(:account) { create(:account, cname: cname) }
+  let(:work) { create(:work, doi: []) }
+  let(:prefix) { "10.1234" }
+  let(:cname) { "123abc" }
+  let(:response_body) { File.read(HykuAddons::Engine.root.join("spec", "fixtures", "doi", "mint_doi_return_body.json")) }
+  let(:options) { { cname_doi_mint: [account.cname], use_work_ids: [work.id] } }
 
   before do
-    allow(work.class).to receive(:reindex_everything)
+    Hyrax::DOI::DataCiteRegistrar.username = "username"
+    Hyrax::DOI::DataCiteRegistrar.password = "password"
+    Hyrax::DOI::DataCiteRegistrar.prefix = prefix
+    Hyrax::DOI::DataCiteRegistrar.mode = :test
+
+    stub_request(:post, URI.join(Hyrax::DOI::DataCiteClient::TEST_BASE_URL, "dois"))
+      .with(body: "{\"data\":{\"type\":\"dois\",\"attributes\":{\"prefix\":\"#{prefix}\"}}}",
+            headers: { "Content-Type" => "application/vnd.api+json" },
+            basic_auth: ["username", "password"])
+      .to_return(status: 200, body: response_body)
 
     allow(Apartment::Tenant).to receive(:switch!).with(account.tenant) do |&block|
       block&.call
@@ -14,11 +27,8 @@ RSpec.describe HykuAddons::ReindexModelJob do
     Apartment::Tenant.switch!(account.tenant) { work }
   end
 
-  it "#perform_later can enqueue model for reindex later" do
-    expect { described_class.perform_later(work.class.to_s, account.cname) }.to have_enqueued_job(described_class).exactly(:once)
-  end
-
-  it "#perform_now will enqueue model for reindex" do
-    expect { described_class.perform_now(work.class.to_s, account.cname) }.to have_enqueued_job(described_class).exactly(:once)
+  it "sets doi_status_when_public to findable" do
+    described_class.perform_now(work.class.to_s, account.cname, limit: 1, options: options)
+    expect(work.reload.doi_status_when_public).to eq "findable"
   end
 end
