8174709cedbef7d31b81f749f58e938756b01a59
Paul Danelli <prdanelli@gmail.com>
2021-12-09 12:00:34 +0000

    Speed up the getter methods for ISNI and ROR

--- app/services/hyku_addons/current_he_institution_service.rb
+++ app/services/hyku_addons/current_he_institution_service.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
+
 module HykuAddons
   class CurrentHeInstitutionService < HykuAddons::QaSelectService
     def initialize(model: nil)
-      super('current_he_institution', model: model)
+      super("current_he_institution", model: model)
     end
 
     def select_active_options_isni
-      select_active_options.map { |e| authority.find(e[1])[:isni] }
+      quick_active_elements.map { |a| a["isni"] }
     end
 
     def select_active_options_ror
-      select_active_options.map { |e| authority.find(e[1])[:ror] }
+      quick_active_elements.map { |a| a["ror"] }
+    end
+
+    protected
+
+    # This method accesses a private method on the authority which returns the raw yaml data as an array of hashes
+    # without removing elements. This means we can increase the speed with which we get different sets of keys
+    def quick_active_elements
+      authority.send(:terms).select { |a| a["active"] == true }
     end
   end
 end
