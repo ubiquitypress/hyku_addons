61003826c25ea8b4333ecdc98ad0f8d352ad32fb
Bertie Wooles <bertie.wooles@gmail.com>
2021-12-13 15:19:15 +0000

    Appeases rubocop and fixes spec

--- spec/requests/api_v1_collection_spec.rb
+++ spec/requests/api_v1_collection_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe Hyku::API::V1::CollectionController, type: :request, clean: true,
 
   let(:attributes) do
     {
-      # collection_banner_url: collection_banner_url,
       date_created: [date_created],
       collection_type_gid: collection_type_gid,
       description: [description],
@@ -21,6 +20,9 @@ RSpec.describe Hyku::API::V1::CollectionController, type: :request, clean: true,
     }
   end
 
+  let(:collection) { Collection.create(attributes) }
+  let(:collection_branding) { CollectionBrandingInfo.new(collection_id: collection.id, role: "banner", filename: "nypl-hydra-of-lerna.jpg").save("spec/internal_test_hyku/spec/fixtures/images/nypl-hydra-of-lerna.jpg", false) }
+
   before do
     allow(Apartment::Tenant).to receive(:switch!).with(account.tenant) do |&block|
       block&.call
@@ -28,38 +30,39 @@ RSpec.describe Hyku::API::V1::CollectionController, type: :request, clean: true,
 
     Apartment::Tenant.switch!(account.tenant) do
       Site.update(account: account)
+      collection
+      collection_branding
     end
   end
 
   describe "/collection" do
-    context 'when repository has content' do
-        let(:collection) { Collection.create(attributes) }
-        let(:collection_branding) { CollectionBrandingInfo.new(collection_id: collection.id, role: "banner", filename: "nypl-hydra-of-lerna.jpg").save("spec/internal_test_hyku/spec/fixtures/images/nypl-hydra-of-lerna.jpg", false)}
-        it "Returns collection json" do
-          puts collection_branding.inspect
-          get "/api/v1/tenant/#{account.tenant}/collection"
-          puts json_response.inspect
-          expect(json_response['items']).to include ({"cname"=> cname.to_s,
-            "collection_banner_url"=> "http://#{cname}/branding/#{collection.id}/banner/nypl-hydra-of-lerna.jpg",
-            "date_created"=>date_created,
-            "date_published"=>nil,
-            "description"=> "This is a test collection",
-            "keywords"=> nil,
-            "language"=>nil,
-            "license_for_api_tesim"=>nil,
-            "publisher"=>nil,
-            "related_url"=>nil,
-            "resource_type"=>nil,
-            "rights_statements_for_api_tesim"=>nil,
-            "thumbnail_base64_string"=>nil,
-            "thumbnail_url"=>nil,
-            "title"=> collection.title.first,
-            "total_works"=>nil,
-            "type"=>"collection",
-            "uuid"=> collection.id.to_s,
-            "visibility"=>"open",
-            "volumes"=>nil})
-        end
+    context "when repository has content" do
+      # rubocop:disable  RSpec/ExampleLength
+      it "Returns collection json" do
+        get "/api/v1/tenant/#{account.tenant}/collection"
+        puts "JSON RESPONSE IS #{json_response}"
+        expect(json_response["items"]).to include("cname" => cname.to_s,
+                                                  "collection_banner_url" => "http://#{cname}/branding/#{collection.id}/banner/nypl-hydra-of-lerna.jpg",
+                                                  "date_created" => date_created,
+                                                  "date_published" => nil,
+                                                  "description" => "This is a test collection",
+                                                  "keywords" => nil,
+                                                  "language" => nil,
+                                                  "license_for_api_tesim" => nil,
+                                                  "publisher" => nil,
+                                                  "related_url" => nil,
+                                                  "resource_type" => nil,
+                                                  "rights_statements_for_api_tesim" => nil,
+                                                  "thumbnail_base64_string" => nil,
+                                                  "thumbnail_url" => nil,
+                                                  "title" => collection.title.first,
+                                                  "total_works" => nil,
+                                                  "type" => "collection",
+                                                  "uuid" => collection.id.to_s,
+                                                  "visibility" => "open",
+                                                  "volumes" => nil)
+        # rubocop:enable RSpec/ExampleLength
       end
     end
   end
+end
