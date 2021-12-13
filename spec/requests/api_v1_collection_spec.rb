f93ec9dcb56932d3a99ef0b0ae9f869032488f77
Bertie Wooles <bertie.wooles@gmail.com>
2021-12-13 14:18:01 +0000

    Changes tests to test for additional fields on api response

--- spec/requests/api_v1_collection_spec.rb
+++ spec/requests/api_v1_collection_spec.rb
@@ -3,17 +3,23 @@ require "rails_helper"
 
 RSpec.describe Hyku::API::V1::CollectionController, type: :request, clean: true, multitenant: true do
   let(:account) { create(:account) }
-  let(:collection) { create(:collection, visibility: "open") }
-  let(:collection_branding_instance) do
-    instance_double(CollectionBrandingInfo,
-                    collection_id: collection.id, role: "banner",
-                    local_path: "/fake/path/to/banner.png",
-                    alt_text: "This is the banner",
-                    target_url: "http://example.com/",
-                    height: "", width: "")
-  end
+  let(:collection_type) { create(:collection_type, brandable: true) }
+  let(:collection_type_gid) { collection_type.to_global_id.to_s }
+  let(:json_response) { JSON.parse(response.body) }
+  let(:cname) { (account.search_only? ? work.to_solr.dig("account_cname_tesim")&.first : account.cname) }
+  let(:date_created) { "1992-12-31" }
+  let(:description) { "This is a test collection" }
 
-  let(:collection_branding_list) { class_double(CollectionBrandingInfo) }
+  let(:attributes) do
+    {
+      # collection_banner_url: collection_banner_url,
+      date_created: [date_created],
+      collection_type_gid: collection_type_gid,
+      description: [description],
+      title: ["Test Title"],
+      visibility: "open"
+    }
+  end
 
   before do
     allow(Apartment::Tenant).to receive(:switch!).with(account.tenant) do |&block|
@@ -22,26 +28,38 @@ RSpec.describe Hyku::API::V1::CollectionController, type: :request, clean: true,
 
     Apartment::Tenant.switch!(account.tenant) do
       Site.update(account: account)
-      collection
     end
   end
 
-  context "when repository has content" do
-    let(:collection_branding_info) { class_double("CollectionBrandingInfo") }
-    let(:json_response) { JSON.parse(response.body) }
-    let(:banner_url) { json_response.dig("collection_banner_url") || "" }
-    let(:collection_banner_url) { URI.parse(banner_url) }
-
-    before do
-      allow(CollectionBrandingInfo).to receive(:where).with(collection_id: collection.id) { collection_branding_list }
-      allow(collection_branding_list).to receive(:where).and_return(collection_branding_list)
-      allow(collection_branding_list).to receive(:first).and_return(collection_branding_instance)
-    end
-
-    it "returns correct collection json" do
-      get "/api/v1/tenant/#{account.tenant}/collection/#{collection.id}"
-      expect(response.status).to eq(200)
-      expect(collection_banner_url.path).to eq(collection_branding_instance.local_path)
+  describe "/collection" do
+    context 'when repository has content' do
+        let(:collection) { Collection.create(attributes) }
+        let(:collection_branding) { CollectionBrandingInfo.new(collection_id: collection.id, role: "banner", filename: "nypl-hydra-of-lerna.jpg").save("spec/internal_test_hyku/spec/fixtures/images/nypl-hydra-of-lerna.jpg", false)}
+        it "Returns collection json" do
+          puts collection_branding.inspect
+          get "/api/v1/tenant/#{account.tenant}/collection"
+          puts json_response.inspect
+          expect(json_response['items']).to include ({"cname"=> cname.to_s,
+            "collection_banner_url"=> "http://#{cname}/branding/#{collection.id}/banner/nypl-hydra-of-lerna.jpg",
+            "date_created"=>date_created,
+            "date_published"=>nil,
+            "description"=> "This is a test collection",
+            "keywords"=> nil,
+            "language"=>nil,
+            "license_for_api_tesim"=>nil,
+            "publisher"=>nil,
+            "related_url"=>nil,
+            "resource_type"=>nil,
+            "rights_statements_for_api_tesim"=>nil,
+            "thumbnail_base64_string"=>nil,
+            "thumbnail_url"=>nil,
+            "title"=> collection.title.first,
+            "total_works"=>nil,
+            "type"=>"collection",
+            "uuid"=> collection.id.to_s,
+            "visibility"=>"open",
+            "volumes"=>nil})
+        end
+      end
     end
   end
-end
