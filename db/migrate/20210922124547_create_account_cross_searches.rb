be7b10eec0a9843b7fa013d5e45da6cc9548eca0
Paul Danelli <prdanelli@gmail.com>
2021-12-17 13:39:17 +0000

    Add conditions to migration (#584)

--- db/migrate/20210922124547_create_account_cross_searches.rb
+++ db/migrate/20210922124547_create_account_cross_searches.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 class CreateAccountCrossSearches < ActiveRecord::Migration[5.2]
-  def change
+  def self.up
+    return if table_exists?(:account_cross_searches)
+
     create_table :account_cross_searches do |t|
       t.references :search_account, foreign_key: { to_table: :accounts }
       t.references :full_account, foreign_key: { to_table: :accounts }
 
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table(:account_cross_searches)
   end
 end
