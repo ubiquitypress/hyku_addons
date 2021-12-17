f3262d0ac4ac82866b36b15067173f7fc7d3abde
thenapking <tom.giles@gmail.com>
2021-12-17 16:06:44 +0000

    Refactor action list
    r

--- spec/views/hyrax/file_sets/_actions.html.erb_spec.rb
+++ spec/views/hyrax/file_sets/_actions.html.erb_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe "hyrax/file_sets/_actions.html.erb", type: :view do
       end
 
       it "renders nothing" do
-        expect(rendered).to eq("")
+        expect(rendered.squish).to be_empty
       end
     end
 
@@ -136,7 +136,7 @@ RSpec.describe "hyrax/file_sets/_actions.html.erb", type: :view do
         end
 
         it "renders nothing" do
-          expect(rendered).to eq("")
+          expect(rendered.squish).to be_empty
         end
       end
     end
