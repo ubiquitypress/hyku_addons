bea917a58218657d46c8296bbb134e55026c659c
Bertie Wooles <bertie.wooles@gmail.com>
2022-01-05 16:12:34 +0000

    Fixes args typo and removes unneeded cop disable

--- app/controllers/concerns/hyku_addons/users_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/users_controller_behavior.rb
@@ -15,7 +15,7 @@ module HykuAddons
       return render json: { status: 403, code: 'forbidden', message: t("errors.users_forbidden") } if @user.blank?
 
       query_string = "(creator_tesim:\"*#{@user.email}*\") AND visibility_ssi:open"
-      @user_document_id_list = ActiveFedora::SolrService.get(query_string, rowy: 1_000_000)['response']['docs'].pluck('id')
+      @user_document_id_list = ActiveFedora::SolrService.get(query_string, rows: 1_000_000)['response']['docs'].pluck('id')
       @user_works = Hyrax::PresenterFactory.build_for(ids: @user_document_id_list,
                                                       presenter_class: Hyku::WorkShowPresenter,
                                                       presenter_args: current_ability)
