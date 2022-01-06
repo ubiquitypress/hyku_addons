ced70adc3524cc54d581821f95096a110d8f0095
Paul Danelli <prdanelli@gmail.com>
2022-01-06 17:36:47 +0000

    Resolve issues with the note field for schema works (#595)
    * Remove notes tab if enabled but work doesn't support notes
    
    * Create Note Behaviors for forms and work models as the note field cannot be set within the schema yaml
    
    * Remove note from UP work template and update readme
    
    * Update README.md

--- app/models/uva_work.rb
+++ app/models/uva_work.rb
@@ -4,6 +4,7 @@ class UvaWork < ActiveFedora::Base
   include Hyrax::WorkBehavior
   include Hyrax::DOI::DOIBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
+  include HykuAddons::NoteBehavior
 
   include HykuAddons::Schema::WorkBase
   include Hyrax::Schema(:uva_work)
@@ -12,5 +13,5 @@ class UvaWork < ActiveFedora::Base
 
   self.indexer = UbiquityTemplateWorkIndexer
 
-  validates :title, presence: { message: 'Your work must have a title.' }
+  validates :title, presence: { message: "Your work must have a title." }
 end
