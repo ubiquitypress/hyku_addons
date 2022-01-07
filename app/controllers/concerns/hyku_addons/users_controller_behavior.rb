556103692b4ff836052269526c629e21a224afcf
Bertie Wooles <bertie.wooles@gmail.com>
2022-01-07 16:52:48 +0100

    Removes uneeded instance variable

--- app/controllers/concerns/hyku_addons/users_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/users_controller_behavior.rb
@@ -15,8 +15,8 @@ module HykuAddons
       return render json: { status: 403, code: 'forbidden', message: t("errors.users_forbidden") } if @user.blank?
 
       query_string = "(creator_tesim:\"*#{@user.email}*\") AND visibility_ssi:open"
-      @user_document_id_list = ActiveFedora::SolrService.get(query_string, rows: 1_000_000)['response']['docs'].pluck('id')
-      @user_works = Hyrax::PresenterFactory.build_for(ids: @user_document_id_list,
+      user_document_id_list = ActiveFedora::SolrService.get(query_string, rows: 1_000_000)['response']['docs'].pluck('id')
+      @user_works = Hyrax::PresenterFactory.build_for(ids: user_document_id_list,
                                                       presenter_class: Hyku::WorkShowPresenter,
                                                       presenter_args: current_ability)
       collection_search_builder = Hyrax::CollectionSearchBuilder.new(self).with_access(:read).rows(1_000_000)
