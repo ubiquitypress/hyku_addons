f39ff0f1f69b87f078bc6af3593ca19fe908d471
Bertie Wooles <bertie.wooles@gmail.com>
2022-01-10 13:41:35 +0100

    Adds creator_institutional_email to work endpoint spec and refactors helper

--- app/helpers/hyku_addons/api_works_helper.rb
+++ app/helpers/hyku_addons/api_works_helper.rb
@@ -6,17 +6,23 @@ module HykuAddons
     def creator_from_json(creator_json)
       creator_hash = JSON.parse(creator_json || "{}")
 
-      return if creator_hash.blank?
+      return [] if creator_hash.blank?
+
+      key = "creator_institutional_email"
 
       creator_hash.map do |creator|
-        if creator["creator_institutional_email"]&.blank?
-          creator.delete("creator_institutional_email")
+        if creator[key].blank?
+          creator.delete(key)
         else
-          user = User.find_by(email: creator["creator_institutional_email"])
-          creator.delete("creator_institutional_email") unless user.present? && user.display_profile
+          user = find_by_user_email(creator[key])
+          creator.delete(key) unless user.present? && user.display_profile
         end
         creator
       end
+    end
+
+    def find_by_user_email(email)
+      User.find_by(email: email)
     end
   end
 end
