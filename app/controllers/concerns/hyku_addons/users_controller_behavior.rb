bb7a95ae1abd8efa456d37c4a8f065ede3943243
Bertie Wooles <bertie.wooles@gmail.com>
2022-01-11 11:31:40 +0100

    Switches to double quotes

--- app/controllers/concerns/hyku_addons/users_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/users_controller_behavior.rb
@@ -12,10 +12,10 @@ module HykuAddons
 
     def show
       @user = User.find_by(email: params[:email], display_profile: true)
-      return render json: { status: 403, code: 'forbidden', message: t("errors.users_forbidden") } if @user.blank?
+      return render json: { status: 403, code: "forbidden", message: t("errors.users_forbidden") } if @user.blank?
 
       query_string = "(creator_tesim:\"*#{@user.email}*\") AND visibility_ssi:open"
-      user_document_id_list = ActiveFedora::SolrService.get(query_string, rows: 1_000_000)['response']['docs'].pluck('id')
+      user_document_id_list = ActiveFedora::SolrService.get(query_string, rows: 1_000_000)["response"]["docs"].pluck("id")
       @user_works = Hyrax::PresenterFactory.build_for(ids: user_document_id_list,
                                                       presenter_class: Hyku::WorkShowPresenter,
                                                       presenter_args: current_ability)
