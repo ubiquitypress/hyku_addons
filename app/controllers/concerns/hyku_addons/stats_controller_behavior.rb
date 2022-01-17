ded0e763a3ff422f03e7f0fd071bb72c0e5c2a47
Paul Danelli <prdanelli@gmail.com>
2022-01-17 14:18:18 +0000

    Admin only

--- app/controllers/concerns/hyku_addons/stats_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/stats_controller_behavior.rb
@@ -8,10 +8,19 @@ module HykuAddons
       with_themed_layout "dashboard"
 
       before_action :build_breadcrumbs, only: [:work, :file, :reports]
+      before_action :has_permission?, only: :reports
     end
 
     def reports
       @reports = Site.instance.account.dashboard_gds_charts.lines
+    end
+
+    protected
+
+    def has_permission?
+      return if current_user.has_role?(:admin, Site.instance)
+
+      raise ActionController::RoutingError, "Not found"
     end
   end
 end
