e1381c11862f8f962b9f39ae1772d5ddb2bc119f
edward <bauchiroad@gmail.com>
2022-01-18 16:54:50 +0000

    remove memoization from private method

--- app/services/hyku_addons/collection_branding_fetcher.rb
+++ app/services/hyku_addons/collection_branding_fetcher.rb
@@ -7,10 +7,10 @@ module HykuAddons
       @_collection_banner ||= collection_branding_info(id).where(role: 'banner')
     end
 
-    private_class_method
+    private
 
-    def collection_branding_info(id)
-      @_collection_branding_info ||= CollectionBrandingInfo.where(collection_id: id.to_s)
-    end
+      def collection_branding_info(id)
+        CollectionBrandingInfo.where(collection_id: id.to_s)
+      end
   end
 end
