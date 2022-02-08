cd51309c51bbf99a7e626886db6dca2967876d13
Bertie Wooles <bertie.wooles@gmail.com>
2022-02-08 10:23:24 +0000

    Rubocop

--- spec/actors/hyku_addons/actors/creator_display_profile_visibility_actor_spec.rb
+++ spec/actors/hyku_addons/actors/creator_display_profile_visibility_actor_spec.rb
@@ -81,7 +81,7 @@ RSpec.describe HykuAddons::Actors::CreatorProfileVisibilityActor do
 
     context "when the display profile is false" do
       it "doesn't change the value" do
-        expect {  middleware.update(env) }
+        expect { middleware.update(env) }
           .not_to change { JSON.parse(env.attributes[:creator].first).first.dig(key) }
       end
     end
