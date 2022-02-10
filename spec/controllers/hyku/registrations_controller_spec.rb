e50a032cfd34969ff58a35f2afc224dc7ca0433a
Paul Danelli <prdanelli@gmail.com>
2022-02-10 17:29:43 +0000

    Redirect user to profile after registration (#651)
    * Add redirect to profile after registration
    
    * Rubocop
    
    * Rubocop

---
+++ spec/controllers/hyku/registrations_controller_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe Hyku::RegistrationsController, type: :controller do
+  let(:account) { create(:account) }
+  let(:site) { Site.new(account: account) }
+
+  before do
+    allow(Site).to receive(:instance).and_return(site)
+
+    @request.env["devise.mapping"] = Devise.mappings[:user] # rubocop:disable RSpec/InstanceVariable
+  end
+
+  context "with account signup enabled" do
+    let(:account_signup_enabled) { "true" }
+    let(:valid_params) do
+      {
+        user: {
+          email: "user@test.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+
+    describe "#create" do
+      it "processes the form" do
+        post :create, params: valid_params
+
+        profile_path = Hyrax::Engine.routes.url_helpers.dashboard_profile_path(User.first, locale: :en)
+        expect(response).to redirect_to(profile_path)
+        expect(flash[:notice]).to eq "Welcome! You have signed up successfully."
+      end
+    end
+  end
+end
