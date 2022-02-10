4c2bc27f525e3b0521c489040cb60b96c3cfa8d3
Paul Danelli <prdanelli@gmail.com>
2022-02-10 16:21:37 +0000

    Add redirect to profile after registration

--- lib/hyku_addons/engine.rb
+++ lib/hyku_addons/engine.rb
@@ -49,17 +49,26 @@ module HykuAddons
       Hyku::RegistrationsController.class_eval do
         def new
           return super if current_account&.allow_signup == "true"
-          redirect_to root_path, alert: t(:'hyku.account.signup_disabled')
+
+          redirect_to root_path, alert: t("hyku.account.signup_disabled")
         end
 
         def create
           return super if current_account&.allow_signup == "true"
-          redirect_to root_path, alert: t(:'hyku.account.signup_disabled')
+
+          redirect_to root_path, alert: t("hyku.account.signup_disabled")
         end
 
         def current_account
           Site.account
         end
+
+        protected
+
+          # Redirect the user to the profile after registration
+          def after_sign_up_path_for(resource)
+            hyrax.dashboard_profile_path(resource)
+          end
       end
 
       # Using a concern doesn't actually override the original method so inlining it here
