1ff81e65255db2dc2f1e1646061eae528f056d65
Paul Danelli <prdanelli@gmail.com>
2022-02-11 09:54:01 +0000

    Clean up the spec and remove uncessessary context block

--- spec/controllers/hyku/registrations_controller_spec.rb
+++ spec/controllers/hyku/registrations_controller_spec.rb
@@ -3,6 +3,16 @@
 RSpec.describe Hyku::RegistrationsController, type: :controller do
   let(:account) { create(:account) }
   let(:site) { Site.new(account: account) }
+  let(:account_signup_enabled) { "true" }
+  let(:valid_params) do
+    {
+      user: {
+        email: "user@test.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+    }
+  end
 
   before do
     allow(Site).to receive(:instance).and_return(site)
@@ -10,26 +20,16 @@ RSpec.describe Hyku::RegistrationsController, type: :controller do
     @request.env["devise.mapping"] = Devise.mappings[:user] # rubocop:disable RSpec/InstanceVariable
   end
 
-  context "with account signup enabled" do
-    let(:account_signup_enabled) { "true" }
-    let(:valid_params) do
-      {
-        user: {
-          email: "user@test.com",
-          password: "password",
-          password_confirmation: "password"
-        }
-      }
+  describe "#create" do
+    before do
+      post :create, params: valid_params
     end
 
-    describe "#create" do
-      it "processes the form" do
-        post :create, params: valid_params
+    it "redirects to the user to their profile" do
+      profile_path = Hyrax::Engine.routes.url_helpers.dashboard_profile_path(User.first, locale: :en)
 
-        profile_path = Hyrax::Engine.routes.url_helpers.dashboard_profile_path(User.first, locale: :en)
-        expect(response).to redirect_to(profile_path)
-        expect(flash[:notice]).to eq "Welcome! You have signed up successfully."
-      end
+      expect(response).to redirect_to(profile_path)
+      expect(flash[:notice]).to eq "Welcome! You have signed up successfully."
     end
   end
 end
