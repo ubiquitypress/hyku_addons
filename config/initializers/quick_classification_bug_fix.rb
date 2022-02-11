b9e1b68c242d3e81bdcbc27b03f775caafa4549b
Paul Danelli <prdanelli@gmail.com>
2022-02-11 14:42:22 +0000

    Move engine initializer to file

---
+++ config/initializers/quick_classification_bug_fix.rb
@@ -0,0 +1,9 @@
+# NOTE: This issue only seems to present in development, and not consistently.
+# Compact the available work types to remove `nil` and prevent an `Nil location provided. Can't build URI.` error
+# when not all options are selected, thrown from `SelectTypePresenter#switch_to_new_work_path`
+Hyrax::QuickClassificationQuery.class_eval do
+  def normalized_model_names
+    models.map { |name| concern_name_normalizer.call(name) if Site.first.available_works.include?(name) }.compact
+  end
+end
+
--- lib/hyku_addons/engine.rb
+++ lib/hyku_addons/engine.rb
@@ -116,17 +116,6 @@ module HykuAddons
       end
     end
 
-    # NOTE: This issue only seems to present in development, and not consistently.
-    # Compact the available work types to remove `nil` and prevent an `Nil location provided. Can't build URI.` error
-    # when not all options are selected, thrown from `SelectTypePresenter#switch_to_new_work_path`
-    initializer "hyku_addon.available_work_type_bug_fix" do
-      Hyrax::QuickClassificationQuery.class_eval do
-        def normalized_model_names
-          models.map { |name| concern_name_normalizer.call(name) if Site.first.available_works.include?(name) }.compact
-        end
-      end
-    end
-
     initializer "hyku_addons.append_migrations" do |app|
       # Add migrations to parent app paths
       unless app.root.to_s.match?(root.to_s)
