7821eac12942b44bbdcdca5493fa725bc10d7906
Paul Danelli <prdanelli@gmail.com>
2022-02-17 12:01:01 +0000

    Rubocop

--- spec/lib/hyku_addons/engine_spec.rb
+++ spec/lib/hyku_addons/engine_spec.rb
@@ -7,9 +7,16 @@ RSpec.describe HykuAddons::Engine do
       expect(Mime::Type.lookup_by_extension(:ris)).to be_a(Mime::Type)
     end
   end
+
+  describe "setting the session store" do
+    it "sets the correct session store" do
+      expect(Rails.application.config.session_options).to eq(cookie_only: true, key: "_hyku_session", same_site: :lax)
+    end
+  end
+
   describe "adding Flipflop features" do
     it "adds the engine to the flipflop paths" do
-      path = HykuAddons::Engine.root.join("config", "features.rb").to_s
+      path = described_class.root.join("config", "features.rb").to_s
 
       expect(Flipflop::FeatureLoader.current.instance_variable_get(:@paths)).to include(path)
     end
