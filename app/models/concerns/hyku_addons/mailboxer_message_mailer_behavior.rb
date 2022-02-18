6ff1fd64a82c7736b8e909a78e810317cfcbf9dc
Paul Danelli <prdanelli@gmail.com>
2022-02-18 13:54:54 +0000

    Dry up mail boxer email methods

--- app/models/concerns/hyku_addons/mailboxer_message_mailer_behavior.rb
+++ app/models/concerns/hyku_addons/mailboxer_message_mailer_behavior.rb
@@ -7,20 +7,26 @@ module HykuAddons
     def new_message_email(message, receiver)
       @message  = message
       @receiver = receiver
-      set_subject(message)
-      mail to: receiver.send(Mailboxer.email_method, message),
-           subject: t("mailboxer.message_mailer.subject_new", subject: @subject, tenant_name: Site.instance.application_name),
-           template_name: "hyku_addons_new_message_email"
+
+      message_email("mailboxer.message_mailer.subject_new",  "hyku_addons_new_message_email")
     end
 
     # Sends an email for indicating a reply in an already created conversation
     def reply_message_email(message, receiver)
       @message  = message
       @receiver = receiver
-      set_subject(message)
-      mail to: receiver.send(Mailboxer.email_method, message),
-           subject: t("mailboxer.message_mailer.subject_reply", subject: @subject, tenant_name: Site.instance.application_name),
-           template_name: "hyku_addons_reply_message_email"
+
+      message_email("mailboxer.message_mailer.subject_reply",  "hyku_addons_reply_message_email")
     end
+
+    protected
+
+      def message_email(subject_key, template)
+        set_subject(@message)
+
+        mail to: receiver.send(Mailboxer.email_method, @message),
+             subject: t(subject_key, subject: @subject, tenant_name: Site.instance.application_name),
+             template_name: template
+      end
   end
 end
