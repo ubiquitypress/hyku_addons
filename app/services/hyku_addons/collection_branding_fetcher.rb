a7f215b9e5b3ed3f8f0da84f10179de14e8dd6fd
Tom Giles <tom.giles@gmail.com>
2022-02-18 12:47:52 +0000

    Add collection Logo to API (#655)
    * Add logo
    
    * Refactor
    
    * Refactor

--- app/services/hyku_addons/collection_branding_fetcher.rb
+++ app/services/hyku_addons/collection_branding_fetcher.rb
@@ -2,15 +2,43 @@
 
 module HykuAddons
   class CollectionBrandingFetcher
-    def collection_banner(id, cname)
-      AccountElevator.switch!(cname)
-      @_collection_banner ||= collection_branding_info(id).where(role: "banner")
+    attr_accessor :collection_id, :cname
+
+    def initialize(collection_id, cname)
+      @collection_id = collection_id
+      @cname = cname
+    end
+
+    def url_for(role)
+      return unless ["banner", "logo"].include? role
+
+      format_url(fetch_collection_branding(role))
     end
 
     private
 
-      def collection_branding_info(id)
-        CollectionBrandingInfo.where(collection_id: id.to_s)
+      # Fetch first banner or logo file information attached to collection if it exists
+      def fetch_collection_branding(role)
+        return if @cname.nil?
+
+        AccountElevator.switch!(cname)
+        CollectionBrandingInfo.where(collection_id: @collection_id.to_s, role: role)&.first
+      end
+
+      # Build component for collection json
+      def format_url(role)
+        return unless role.present?
+
+        URI::Generic.build(component(role)).to_s
+      end
+
+      def component(role)
+        {
+          scheme: Rails.application.routes.default_url_options.fetch(:protocol, "http"),
+          host: @cname,
+          path: role.local_path,
+          alt_text: role.alt_text
+        }
       end
   end
 end
