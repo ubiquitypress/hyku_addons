e46071099183977b53528d60725bc11c37bc7e31
Paul Danelli <prdanelli@gmail.com>
2022-02-21 15:49:09 +0000

    Create a helper method to ensure that schema work types are checked

---
+++ spec/support/misc_helpers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Provides a list of schema work types
+#
+# Requires you set up a Site instance:
+# let(:account) { build_stubbed(:account) }
+# let(:site) { Site.new(account: account) }
+# before do
+#   allow(Site).to receive(:instance).and_return(site)
+# end
+def schema_work_types
+   Site.instance.available_works.select { |wt| wt.constantize.new.schema_driven? }
+end
