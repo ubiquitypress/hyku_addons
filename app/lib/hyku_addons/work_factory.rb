050a9969068102f608917b025a1c9d167667ed8a
Bertie Wooles <bertie.wooles@gmail.com>
2022-03-18 10:13:16 +0000

    Work Factory Registers both frontend URLS (#694)

--- app/lib/hyku_addons/work_factory.rb
+++ app/lib/hyku_addons/work_factory.rb
@@ -5,7 +5,8 @@ module HykuAddons
       work = Hyrax::Hirmeos::Client::Work.new
       work.title = resource.title
       work.uri = [{ uri: resource_url(resource) },
-                  { uri: frontend_resource_url(resource) },
+                  { uri: build_scholarly_link(resource) },
+                  { uri: build_non_scholarly_link(resource) },
                   { uri: "urn:uuid:#{resource.id}", canonical: true }]
       work.type = "repository-work"
       work
@@ -13,12 +14,6 @@ module HykuAddons
 
     def self.resource_url(work)
       Rails.application.routes.url_helpers.polymorphic_url(work)
-    end
-
-    def self.frontend_resource_url(work)
-      work_types = current_account.google_scholarly_work_types
-      work_types ||= []
-      work_types.include?(work.model_name.name.to_s) ? build_scholarly_link(work) : build_non_scholarly_link(work)
     end
 
     def self.build_scholarly_link(work)
