87489042a7772561d6017b18616522c030f662d4
edward <bauchiroad@gmail.com>
2022-07-13 16:34:29 +0100

    change comments and update ltu-article

--- app/actors/concerns/hyku_addons/hyrax_embargo_actor_override.rb
+++ app/actors/concerns/hyku_addons/hyrax_embargo_actor_override.rb
@@ -7,14 +7,13 @@ module HykuAddons
     # Update the visibility of the work to match the correct state of the embargo, then clear the embargo date, etc.
     # Saves the embargo and the work
     def destroy
-      # duplicating visibility because sometimes it is
-      # not set correctly after expiration
+      # ensure work visibility is correctly after embargo expiration
       visibility_after = work.visibility_after_embargo.clone
 
       work.embargo_visibility! # If the embargo has lapsed, update the current visibility.
       work.deactivate_embargo!
       work.embargo.save!
-      # set visibility if incorrect
+      # use correct visibility if incorrect
       work.visibility = visibility_after if work.visibility != visibility_after
       work.save!
     end
--- app/actors/concerns/hyku_addons/hyrax_lease_actor_override.rb
+++ app/actors/concerns/hyku_addons/hyrax_lease_actor_override.rb
@@ -4,11 +4,10 @@ module HykuAddons
   module HyraxLeaseActorOverride
     extend ActiveSupport::Concern
 
-    # Update the visibility of the work to match the correct state of the embargo, then clear the embargo date, etc.
-    # Saves the embargo and the work
+    # Ensure the visibility of the work matches the correct state of the embargo, then clear the embargo date, etc.
+    # Persist the lease and the work
     def destroy
-      # duplicating visibility because sometimes it is
-      # not set correctly after expiration
+      # should correctly set work visibility after lease expiration
       visibility_after = work.visibility_after_lease.clone
 
       work.lease_visibility! # If the lease has lapsed, update the current visibility.
