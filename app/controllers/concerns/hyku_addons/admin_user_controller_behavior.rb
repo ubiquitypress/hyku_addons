ca860a2a970bf5c6e9569a787754e232e6795899
edward <bauchiroad@gmail.com>
2022-08-10 13:17:35 +0100

    change to using .positive? to check if number greater than zero

--- app/controllers/concerns/hyku_addons/admin_user_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/admin_user_controller_behavior.rb
@@ -9,7 +9,7 @@ module HykuAddons
       if @user.present? && @user.works_count.zero?
         @user.destroy
         redirect_to hyrax.admin_users_path, notice: t("hyrax.admin.users.destroy.success", user: @user)
-      elsif @user.present? && @user.uploaded_files.present? || @user.works_count > 0
+      elsif @user.present? && @user.uploaded_files.present? || @user.works_count.positive?
         @user.roles.clear
         @user.add_role("inactive", Site.instance)
         redirect_to hyrax.admin_users_path, notice: t("hyrax.admin.users.destroy.success", user: @user)
