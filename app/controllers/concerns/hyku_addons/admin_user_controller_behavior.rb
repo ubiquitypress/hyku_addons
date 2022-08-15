eebe5dc4218851ea3e18f138feca5168f62b4846
edward <bauchiroad@gmail.com>
2022-08-15 13:30:17 +0100

    refactor controller destroy action

--- app/controllers/concerns/hyku_addons/admin_user_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/admin_user_controller_behavior.rb
@@ -6,22 +6,28 @@ module HykuAddons
 
     # Delete a user from the site
     def destroy
-      if @user.present? && @user.works_count.zero?
-        @user.destroy
-        redirect_to hyrax.admin_users_path, notice: t("hyrax.admin.users.destroy.success", user: @user)
-      elsif @user.present? && @user.uploaded_files.present? || @user.works_count.positive?
-        handle_user_with_works(@user)
-      else
-        redirect_to hyrax.admin_users_path flash: { error: t("hyrax.admin.users.destroy.failure", user: @user) }
-      end
+      return handle_users_without_works(@user) if @user.present? && @user.works_count.zero?
+      handle_user_with_works(@user)
     end
 
     private
 
       def handle_user_with_works(user)
-        user.roles.clear
-        user.add_role("inactive", Site.instance)
-        redirect_to hyrax.admin_users_path, notice: t("hyrax.admin.users.destroy.success", user: user)
+        if user.present? && user.uploaded_files.present? || @user.works_count.positive?
+          user.roles.clear
+          user.add_role("inactive", Site.instance)
+          redirect_to hyrax.admin_users_path, notice: t("hyrax.admin.users.destroy.success", user: user)
+        else
+          redirect_to hyrax.admin_users_path flash: { error: t("hyrax.admin.users.destroy.failure", user: user) }
+        end
+      end
+
+      def handle_users_without_works(user)
+        if user.destroy
+          redirect_to hyrax.admin_users_path, notice: t("hyrax.admin.users.destroy.success", user: user)
+        else
+          redirect_to hyrax.admin_users_path flash: { error: t("hyrax.admin.users.destroy.failure", user: user) }
+        end
       end
   end
 end
