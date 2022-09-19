bfc78ac57ccc08952f937ca300a740497a1aea6c
Delaney Burke <delaney@vidtreon.com>
2022-09-19 07:56:09 +0300

    Add indexer

---
+++ app/indexers/ltu_serial_indexer.rb
@@ -0,0 +1,7 @@
+  # frozen_string_literal: true
+
+class LtuSerialIndexer < Hyrax::WorkIndexer
+  include Hyrax::Indexer(:ltu_serial)
+  include Hyrax::IndexesBasicMetadata
+  include Hyrax::IndexesLinkedMetadata
+end
