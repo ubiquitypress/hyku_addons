c1a219a35b35c84cc929c7dfc880a8370752d739
Delaney Burke <delaney@vidtreon.com>
2022-09-20 17:13:24 +0300

    Add ltu_serial
    - add missing mnodel info
    - add views

--- app/models/ltu_serial.rb
+++ app/models/ltu_serial.rb
@@ -1,4 +1,20 @@
 class LtuSerial < ActiveFedora::Base
+  include ::Hyrax::WorkBehavior
+
+  # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
+  include Hyrax::DOI::DataCiteDOIBehavior
+
   include HykuAddons::Schema::WorkBase
   include Hyrax::Schema(:ltu_serial)
+  # This must be included at the end, because it finalizes the metadata
+  # schema (by adding accepts_nested_attributes)
+  include Hyrax::BasicMetadata
+
+  self.indexer = LtuSerialIndexer
+
+  validates :title, presence: { message: "Your work must have a title." }
+
+  def doi_registrar_opts
+    {}
+  end
 end
