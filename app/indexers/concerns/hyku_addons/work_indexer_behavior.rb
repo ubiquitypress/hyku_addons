387c52cf41f3c481b11f06a8a965bcf414be2b96
edward <bauchiroad@gmail.com>
2022-10-11 11:05:40 +0100

    index date_published only if it has a value in order for sort by date_published ascending to work

--- app/indexers/concerns/hyku_addons/work_indexer_behavior.rb
+++ app/indexers/concerns/hyku_addons/work_indexer_behavior.rb
@@ -10,7 +10,7 @@ module HykuAddons
         solr_doc["editor_display_ssim"] = format_names(:editor) if object.respond_to? :editor
         solr_doc[Solrizer.solr_name("account_cname", :stored_searchable)] = Site.instance&.account&.cname
         # creates key with "date_published_ssi"
-        solr_doc[Solrizer.solr_name("date_published", :stored_sortable)] = object.date_published
+        solr_doc[Solrizer.solr_name("date_published", :stored_sortable)] = object.date_published if object.date_published.present?
       end
     end
 
