4b34110f91caf1d822c7d0e1d83f752a4b35cb0f
edward <bauchiroad@gmail.com>
2023-01-16 12:44:16 +0000

    REPO-1899 hide inactive works from search and dashboard

---
+++ spec/search_builders/hyrax/dashboard_works_search_builder_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe Hyrax::Dashboard::WorksSearchBuilder do
+  let(:user) { create(:user) }
+  let(:ability) { ::Ability.new(user) }
+  let(:context) { instance_double(current_ability: ability, current_user: user, blacklight_config: CatalogController.blacklight_config) }
+  let(:builder) { described_class.new(context) }
+
+  describe "search builder .default_processor_chain" do
+    it "includes :only_active_works in #default_processor_chain" do
+      expect(builder.default_processor_chain).to include(:only_active_works)
+    end
+  end
+
+  describe "#query method filter include fq parameter which" do
+    it "includes suppressed_bsi in the solr query" do
+      expect(builder.query["fq"].compact).to include("-suppressed_bsi:true")
+    end
+  end
+end
