5cdfe0eaf7012501f5a53d24e6fb1e6561a1df7c
Delaney Burke <deaney.burke@ubiquitypress.com>
2023-03-30 18:23:03 +0300

    HOTFIX
    This fix fixes broken sso workflow from front end

--- lib/hyku_addons/sso.rb
+++ lib/hyku_addons/sso.rb
@@ -48,7 +48,7 @@ module HykuAddons
         WorkOS::SSO.authorization_url(
           client_id: Sso.configuration.client_id,
           organization: @account.work_os_organisation,
-          redirect_uri: "https://#{@account.cname.gsub(".dashboard","")}/sso/callback" 
+          redirect_uri: "https://#{@account.cname.gsub("dashboard.","")}/sso/callback" 
         )
       end
 
