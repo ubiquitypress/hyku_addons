c995de12f4395d164b4054bb30584df24350bb22
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-04-20 13:21:11 +0100

    REPO-2055 - More refined code in controller, plus pulling alert message from en.yml

--- app/controllers/concerns/hyku_addons/invitations_controller_behavior.rb
+++ app/controllers/concerns/hyku_addons/invitations_controller_behavior.rb
@@ -7,11 +7,12 @@ module HykuAddons
       email_domain = params[:user][:email].split("@").last
       managed_domain = Site.account&.work_os_managed_domain
 
-      if managed_domain == email_domain
-        redirect_to request.referer, alert: "Email domain must not be equal to WorkOs managed domain:  #{managed_domain}"
-      else
-        super
-      end
+      return super if managed_domain.nil?
+
+      return super if managed_domain != email_domain
+
+      redirect_to request.referer, alert: t("errors.invalid_domain", managed_domain: managed_domain)
+
     end
   end
 end
