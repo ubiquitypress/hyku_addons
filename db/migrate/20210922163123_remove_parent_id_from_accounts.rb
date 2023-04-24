4b5234f4e7cc354fbd9bbf2ad0902e9a0f362dfb
Delaney Burke <deaney.burke@ubiquitypress.com>
2023-04-24 04:24:37 +0300

    remove migrations

--- db/migrate/20210922163123_remove_parent_id_from_accounts.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class RemoveParentIdFromAccounts < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :accounts, :parent, foreign_key: { to_table: :accounts }
-  end
-end
--- db/migrate/20211214153900_add_bio_to_users.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddBioToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :biography, :text
-  end
-end
