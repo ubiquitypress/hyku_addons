115eb08a65e75c2f2c68bf085d3474fb7428f016
Delaney Burke <deaney.burke@ubiquitypress.com>
2023-04-24 04:24:37 +0300

    remove migrations

--- db/migrate/20230301152739_add_sso_attributes_to_accounts.rb
+++
@@ -1,6 +0,0 @@
-class AddSsoAttributesToAccounts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :accounts, :enable_sso, :boolean
-    add_column :accounts, :work_os_orgnaisation, :string
-  end
-end
