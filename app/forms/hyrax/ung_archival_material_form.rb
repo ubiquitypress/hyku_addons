40b39bbb864ce166c68aab7b88eb5878b40fb4f0
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-07-20 13:57:50 +0100

    REPO-2118 - Addition of missing DOI for Ung Archival Material work type

--- app/forms/hyrax/ung_archival_material_form.rb
+++ app/forms/hyrax/ung_archival_material_form.rb
@@ -2,6 +2,8 @@
 
 module Hyrax
   class UngArchivalMaterialForm < Hyrax::Forms::WorkForm
+    include Hyrax::DOI::DataCiteDOIFormBehavior
+
     include ::HykuAddons::Schema::WorkForm
     include Hyrax::FormFields(:ung_archival_material)
 
