a68ed1a63b412fa4773381798e023bf8536dd4ff
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-08-24 14:47:56 +0100

    REPO-2122 - Conversion of Denver Thesis Dissertation Capstone in to schema yaml

--- app/forms/hyrax/denver_thesis_dissertation_capstone_form.rb
+++ app/forms/hyrax/denver_thesis_dissertation_capstone_form.rb
@@ -4,33 +4,11 @@
 module Hyrax
   # Generated form for DenverThesisDissertationCapstone
   class DenverThesisDissertationCapstoneForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::DenverThesisDissertationCapstone
-    add_terms %i[title resource_type creator institution abstract keyword subject degree qualification_level
-                 qualification_name advisor committee_member org_unit
-                 date_published pagination alternate_identifier library_of_congress_classification
-                 related_identifier publisher place_of_publication license rights_holder rights_statement
-                 contributor table_of_contents references medium extent language add_info]
-    self.terms -= %i[related_url source]
-    self.required_fields = %i[title creator resource_type abstract license date_published]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def primary_terms
-      %i[title resource_type creator] | super
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:denver_thesis_dissertation_capstone)
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:contributor, :extent, :language, :alternate_identifier,
-                             :library_of_congress_classification, :publisher,
-                             :related_identifier, :place_of_publication,
-                             :license, :rights_holder, :rights_statement,
-                             :table_of_contents, :medium, :extent, :advisor]
-      end
-    end
+    self.model_class = ::DenverThesisDissertationCapstone
   end
 end
