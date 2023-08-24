493769b9a115b681ad7ca866167ddfe35b8ff5ac
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-08-24 12:22:12 +0100

    REPO-2122 - Conversion of Denver Book in to schema yaml

--- app/models/denver_book.rb
+++ app/models/denver_book.rb
@@ -2,66 +2,19 @@
 # Generated via
 #  `rails generate hyrax:work DenverBook`
 class DenverBook < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  include Hyrax::DOI::DOIBehavior
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AltTitleMultiple
-  include ::HykuAddons::AddInfoSingular
-  include ::HykuAddons::InstitutionSingular
 
-  property :subject_text, predicate: ::RDF::Vocab::DC.subject, multiple: true do |index|
-    index.as :stored_searchable
-  end
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:denver_book)
 
-  property :place_of_publication, predicate: ::RDF::Vocab::BF2.term(:Place) do |index|
-    index.as :stored_searchable, :facetable
-  end
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
 
-  property :mesh, predicate: ::RDF::Vocab::DC.term(:MESH) do |index|
-    index.as :stored_searchable
-  end
-
-  property :edition, predicate: ::RDF::Vocab::BF2.edition, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :isbn, predicate: ::RDF::Vocab::BIBO.isbn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :table_of_contents, predicate: ::RDF::Vocab::Bibframe.term(:tableOfContents), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :extent, predicate: ::RDF::Vocab::DC.extent, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :medium, predicate: ::RDF::Vocab::DC.medium do |index|
-    index.as :stored_searchable
-  end
-
-  property :time, predicate: ::RDF::Vocab::DC.temporal, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :refereed, predicate: ::RDF::Vocab::BIBO.term("status/peerReviewed"), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :editor, predicate: ::RDF::Vocab::SCHEMA.Person do |index|
-    index.as :stored_searchable
-  end
-
-  self.json_fields += %i[editor]
   self.indexer = DenverBookIndexer
 
-  # Change this to restrict which works can be added as a child.
-  # self.valid_child_concerns = []
   validates :title, presence: { message: "Your work must have a title." }
-
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
+  def doi_registrar_opts
+    {}
+  end
 end
