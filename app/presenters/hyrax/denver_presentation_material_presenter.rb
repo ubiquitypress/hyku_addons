793a7d0b95f29a3f68ee9800e5ac75ea3657d0e8
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-08-24 14:30:56 +0100

    REPO-2122 - Conversion of Denver Presentation Material in to schema yaml

--- app/presenters/hyrax/denver_presentation_material_presenter.rb
+++ app/presenters/hyrax/denver_presentation_material_presenter.rb
@@ -3,13 +3,8 @@
 #  `rails generate hyrax:work DenverPresentationMaterial`
 module Hyrax
   class DenverPresentationMaterialPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:denver_presentation_material)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title resource_type creator abstract keyword subject org_unit
-         date_published related_identifier event_title event_location event_date
-         license rights_holder rights_statement contributor language add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
