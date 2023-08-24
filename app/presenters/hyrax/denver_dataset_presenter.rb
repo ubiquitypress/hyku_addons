865139069773bdc5cc070a3a9df017a8bf7c8186
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-08-24 13:52:32 +0100

    REPO-2122 - Conversion of Denver Dataset in to schema yaml

--- app/presenters/hyrax/denver_dataset_presenter.rb
+++ app/presenters/hyrax/denver_dataset_presenter.rb
@@ -3,14 +3,8 @@
 #  `rails generate hyrax:work DenverDataset`
 module Hyrax
   class DenverDatasetPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:denver_dataset)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title alt_title resource_type creator institution abstract keyword
-         subject_text org_unit date_published version_number alternate_identifier
-         related_identifier license contributor medium extent language location
-         longitude latitude time add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
