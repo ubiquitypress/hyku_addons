c5f4262037ec32ee0b3b474b01c47c133778e192
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-09-26 15:27:59 +0100

    REPO-2181 - Conversion of Redlands Chapters And Book Section in to schema yaml

--- app/forms/hyrax/redlands_chapters_and_book_section_form.rb
+++ app/forms/hyrax/redlands_chapters_and_book_section_form.rb
@@ -4,33 +4,11 @@
 module Hyrax
   # Generated form for RedlandsChaptersAndBookSection
   class RedlandsChaptersAndBookSectionForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::RedlandsChaptersAndBookSection
-    add_terms %i[title alt_title resource_type creator alt_email abstract keyword subject
-                 org_unit language license version_number publisher place_of_publication date_published book_title
-                 alt_book_title edition pagination isbn official_link series_name related_identifier contributor location
-                 longitude latitude add_info buy_book]
-    self.terms -= %i[rights_statement related_url]
-    self.required_fields = %i[title creator alt_email resource_type abstract keyword subject
-                              org_unit language publisher place_of_publication
-                              date_published book_title]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def primary_terms
-      %i[title alt_title resource_type creator contributor alt_email abstract keyword subject
-         org_unit language license version_number publisher place_of_publication date_published book_title] | super
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:redlands_chapters_and_book_section)
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :alt_email, :edition, :book_title, :org_unit, :subject, :keyword,
-                             :language, :version_number, :issue, :pagination, :location, :longitude,
-                             :latitude, :license, :buy_book]
-      end
-    end
+    self.model_class = ::RedlandsChaptersAndBookSection
   end
 end
--- app/models/redlands_chapters_and_book_section.rb
+++ app/models/redlands_chapters_and_book_section.rb
@@ -2,76 +2,19 @@
 # Generated via
 #  `rails generate hyrax:work RedlandsChaptersAndBookSection`
 class RedlandsChaptersAndBookSection < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  # Adds behaviors for hyrax-doi plugin.
-  include Hyrax::DOI::DOIBehavior
-  # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AddInfoSingular
 
-  property :alt_title, predicate: ::RDF::Vocab::DC.alternative, multiple: false do |index|
-    index.as :stored_searchable
-  end
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:redlands_chapters_and_book_section)
 
-  property :alt_email, predicate: ::RDF::Vocab::SCHEMA.email do |index|
-    index.as :stored_searchable
-  end
-
-  property :series_name, predicate: ::RDF::Vocab::BF2.subseriesOf do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :book_title, predicate: ::RDF::Vocab::BIBO.term(:Proceedings), multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :alt_book_title, predicate: ::RDF::Vocab::BIBO.term(:shortTitle), multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :place_of_publication, predicate: ::RDF::Vocab::BF2.term(:Place) do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :edition, predicate: ::RDF::Vocab::BF2.edition, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :pagination, predicate: ::RDF::Vocab::BIBO.numPages, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :longitude, predicate: ::RDF::Vocab::SCHEMA.longitude, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :location, predicate: ::RDF::Vocab::BF2.physicalLocation, multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :latitude, predicate: ::RDF::Vocab::SCHEMA.latitude, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :version_number, predicate: ::RDF::Vocab::SCHEMA.version, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :isbn, predicate: ::RDF::Vocab::BIBO.isbn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :buy_book, predicate: ::RDF::Vocab::SCHEMA.BuyAction, multiple: false do |index|
-    index.as :stored_searchable
-  end
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
 
   self.indexer = RedlandsChaptersAndBookSectionIndexer
-  # Change this to restrict which works can be added as a child.
-  # self.valid_child_concerns = []
-  validates :title, presence: { message: "Your work must have a title." }
 
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
+  validates :title, presence: { message: "Your work must have a title." }
+  def doi_registrar_opts
+    {}
+  end
 end
