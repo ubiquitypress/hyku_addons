6cf3ed3452cbc3dfa52657dd198987f9137c8e02
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-09-26 15:28:32 +0100

    REPO-2181 - Conversion of Redlands Media in to schema yaml

--- app/forms/hyrax/redlands_media_form.rb
+++ app/forms/hyrax/redlands_media_form.rb
@@ -4,31 +4,11 @@
 module Hyrax
   # Generated form for RedlandsMedia
   class RedlandsMediaForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::RedlandsMedia
-    add_terms %i[title alt_title resource_type creator alt_email abstract keyword subject
-                 org_unit language license date_published event_location official_link contributor location
-                 longitude latitude add_info]
-    self.terms -= %i[rights_statement related_url publisher]
-    self.required_fields = %i[title resource_type creator alt_email abstract keyword subject
-                              org_unit language date_published event_location]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def primary_terms
-      %i[title alt_title resource_type creator alt_email abstract keyword subject
-         org_unit language license date_published event_location] | super
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:redlands_media)
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :alt_email, :edition, :book_title, :org_unit, :subject, :keyword,
-                             :language, :location, :longitude,
-                             :latitude, :license, :event_location]
-      end
-    end
+    self.model_class = ::RedlandsMedia
   end
 end
--- app/presenters/hyrax/redlands_media_presenter.rb
+++ app/presenters/hyrax/redlands_media_presenter.rb
@@ -3,13 +3,8 @@
 #  `rails generate hyrax:work RedlandsMedia`
 module Hyrax
   class RedlandsMediaPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:redlands_media)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title alt_title resource_type creator alt_email abstract keyword subject
-         org_unit language license date_published event_location official_link contributor location
-         longitude latitude add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
