707d12be59afea4f5c121ff136afb6e33784ddfd
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-09-27 02:38:11 +0100

    REPO-2181 - Conversion of Redlands Student Work in to schema yaml

--- app/forms/hyrax/redlands_student_work_form.rb
+++ app/forms/hyrax/redlands_student_work_form.rb
@@ -4,30 +4,11 @@
 module Hyrax
   # Generated form for RedlandsStudentWork
   class RedlandsStudentWorkForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::RedlandsStudentWork
-    add_terms %i[title alt_title resource_type creator alt_email abstract keyword subject
-                 qualification_name org_unit language license publisher date_published contributor location
-                 longitude latitude advisor add_info]
-    self.terms -= %i[rights_statement related_url]
-    self.required_fields = %i[title resource_type creator alt_email abstract keyword subject
-                              org_unit language publisher date_published]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def primary_terms
-      %i[title alt_title resource_type creator alt_email abstract keyword subject
-         qualification_name org_unit language license publisher date_published] | super
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:redlands_student_work)
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :alt_email, :volume, :journal_title, :org_unit, :subject, :keyword,
-                             :language, :location, :longitude, :latitude, :license, :advisor, :qualification_name]
-      end
-    end
+    self.model_class = ::RedlandsStudentWork
   end
 end
