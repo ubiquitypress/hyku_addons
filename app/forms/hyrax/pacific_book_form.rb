8008edeb0185aac24a97805c1fd346691b7d8aa1
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-10-12 17:33:39 +0100

    REPO-2180 - Conversion of Pacific Book in to schema yaml

--- app/forms/hyrax/pacific_book_form.rb
+++ app/forms/hyrax/pacific_book_form.rb
@@ -2,25 +2,11 @@
 
 module Hyrax
   class PacificBookForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::PacificBook
-    add_terms %i[title alt_title resource_type creator contributor abstract date_published
-                 pagination is_included_in volume buy_book publisher isbn issn additional_links
-                 rights_holder license org_unit official_link subject keyword refereed add_info]
-    self.terms -= %i[language rights_statement]
-    self.required_fields = %i[title creator resource_type abstract org_unit pagination publisher]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :pagination, :is_included_in, :volume,
-                             :buy_book, :publisher, :isbn, :issn, :additional_links,
-                             :org_unit, :subject, :keyword, :refereed]
-      end
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:pacific_book)
+
+    self.model_class = ::PacificBook
   end
 end
--- app/models/pacific_book.rb
+++ app/models/pacific_book.rb
@@ -1,59 +1,19 @@
 # frozen_string_literal: true
 
 class PacificBook < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  include Hyrax::DOI::DOIBehavior
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AltTitleMultiple
-  include ::HykuAddons::AddInfoSingular
 
-  property :volume, predicate: ::RDF::Vocab::BIBO.volume do |index|
-    index.as :stored_searchable
-  end
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:pacific_book)
 
-  property :pagination, predicate: ::RDF::Vocab::BIBO.numPages, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :issn, predicate: ::RDF::Vocab::BIBO.issn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :refereed, predicate: ::RDF::Vocab::BIBO.term("status/peerReviewed"), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :additional_links, predicate: ::RDF::Vocab::SCHEMA.significantLinks, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :is_included_in, predicate: ::RDF::Vocab::BF2.part, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :buy_book, predicate: ::RDF::Vocab::SCHEMA.BuyAction, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :isbn, predicate: ::RDF::Vocab::BIBO.isbn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :location, predicate: ::RDF::Vocab::BF2.physicalLocation, multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :page_display_order_number, predicate: ::RDF::Vocab::SCHEMA.orderNumber, multiple: false do |index|
-    index.as :stored_searchable
-  end
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
 
   self.indexer = PacificBookIndexer
-  # Change this to restrict which works can be added as a child.
-  # self.valid_child_concerns = []
-  validates :title, presence: { message: "Your work must have a title." }
 
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
+  validates :title, presence: { message: "Your work must have a title." }
+  def doi_registrar_opts
+    {}
+  end
 end
