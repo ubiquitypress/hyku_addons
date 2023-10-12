dd4bacd440b5f0d05914cb9793aecb0ba9c02dde
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-10-12 21:34:36 +0100

    REPO-2180 - Conversion of Pacific Text Work in to schema yaml

--- app/models/pacific_text_work.rb
+++ app/models/pacific_text_work.rb
@@ -2,53 +2,19 @@
 # Generated via
 #  `rails generate hyrax:work PacificTextWork`
 class PacificTextWork < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  # Adds behaviors for hyrax-doi plugin.
-  include Hyrax::DOI::DOIBehavior
-  # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AltTitleMultiple
-  include ::HykuAddons::AddInfoSingular
 
-  property :volume, predicate: ::RDF::Vocab::BIBO.volume do |index|
-    index.as :stored_searchable
-  end
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:pacific_text_work)
 
-  property :pagination, predicate: ::RDF::Vocab::BIBO.numPages, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :issn, predicate: ::RDF::Vocab::BIBO.issn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :refereed, predicate: ::RDF::Vocab::BIBO.term("status/peerReviewed"), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :additional_links, predicate: ::RDF::Vocab::SCHEMA.significantLinks, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :is_included_in, predicate: ::RDF::Vocab::BF2.part, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :location, predicate: ::RDF::Vocab::BF2.physicalLocation, multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :page_display_order_number, predicate: ::RDF::Vocab::SCHEMA.orderNumber, multiple: false do |index|
-    index.as :stored_searchable
-  end
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
 
   self.indexer = PacificTextWorkIndexer
-  # Change this to restrict which works can be added as a child.
-  # self.valid_child_concerns = []
-  validates :title, presence: { message: "Your work must have a title." }
 
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
+  validates :title, presence: { message: "Your work must have a title." }
+  def doi_registrar_opts
+    {}
+  end
 end
--- app/presenters/hyrax/pacific_text_work_presenter.rb
+++ app/presenters/hyrax/pacific_text_work_presenter.rb
@@ -2,13 +2,8 @@
 
 module Hyrax
   class PacificTextWorkPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:pacific_text_work)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title alt_title resource_type creator contributor abstract page_display_order_number official_link
-         date_published pagination is_included_in volume publisher issn source additional_links rights_holder license
-         location org_unit doi subject keyword refereed add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
