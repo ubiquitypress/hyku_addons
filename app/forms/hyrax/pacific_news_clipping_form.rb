8bc1613ebadd65080c32111b0b10f342f1af9c60
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-10-12 19:39:52 +0100

    REPO-2180 - Conversion of Pacific News Clipping in to schema yaml

--- app/forms/hyrax/pacific_news_clipping_form.rb
+++ app/forms/hyrax/pacific_news_clipping_form.rb
@@ -1,27 +1,11 @@
 # frozen_string_literal: true
 module Hyrax
   class PacificNewsClippingForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::PacificNewsClipping
-    add_terms %i[title alt_title resource_type creator contributor abstract
-                 date_published challenged location outcome participant reading_level
-                 photo_caption photo_description pagination is_included_in additional_links
-                 rights_holder license official_link subject keyword add_info]
-    self.terms -= %i[language rights_statement]
-    self.required_fields = %i[title creator resource_type abstract]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :creator, :contributor, :date_published,
-                             :challenged, :location, :outcome, :participant, :reading_level,
-                             :photo_caption, :photo_description, :pagination, :is_included_in,
-                             :additional_links, :subject, :keyword]
-      end
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:pacific_news_clipping)
+
+    self.model_class = ::PacificNewsClipping
   end
 end
--- app/presenters/hyrax/pacific_news_clipping_presenter.rb
+++ app/presenters/hyrax/pacific_news_clipping_presenter.rb
@@ -2,14 +2,8 @@
 
 module Hyrax
   class PacificNewsClippingPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:pacific_news_clipping)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title alt_title resource_type creator contributor abstract official_link
-         date_published challenged location outcome participant reading_level
-         photo_caption photo_description pagination is_included_in additional_links
-         page_display_order_number rights_holder license doi subject keyword add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
