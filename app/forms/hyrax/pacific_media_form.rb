96b1b3984bb45d40efbb2ccee3a61b3f64135d2b
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-10-12 19:14:51 +0100

    REPO-2180 - Conversion of Pacific Media in to schema yaml

--- app/forms/hyrax/pacific_media_form.rb
+++ app/forms/hyrax/pacific_media_form.rb
@@ -3,26 +3,11 @@
 #  `rails generate hyrax:work PacificMedia`
 module Hyrax
   class PacificMediaForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::PacificMedia
-    add_terms %i[title alt_title resource_type creator contributor abstract
-                 date_published duration version_number is_included_in
-                 publisher additional_links rights_holder license
-                 org_unit official_link subject keyword refereed add_info]
-    self.terms -= %i[language rights_statement]
-    self.required_fields = %i[title creator resource_type abstract org_unit]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :duration, :version_number, :is_included_in,
-                             :publisher, :additional_links, :org_unit, :subject,
-                             :keyword, :refereed, :official_link]
-      end
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:pacific_media)
+
+    self.model_class = ::PacificMedia
   end
 end
--- app/presenters/hyrax/pacific_media_presenter.rb
+++ app/presenters/hyrax/pacific_media_presenter.rb
@@ -2,14 +2,8 @@
 
 module Hyrax
   class PacificMediaPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:pacific_media)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title alt_title resource_type creator contributor institution abstract
-         date_published duration version_number is_included_in page_display_order_number
-         publisher additional_links rights_holder rights_holder license location
-         org_unit official_link subject keyword refereed add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
