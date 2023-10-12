f2e3bfef91ddd3caa2faf0034f31bc53afc025c1
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-10-12 21:08:23 +0100

    REPO-2180 - Conversion of Pacific Presentation in to schema yaml

--- app/forms/hyrax/pacific_presentation_form.rb
+++ app/forms/hyrax/pacific_presentation_form.rb
@@ -4,25 +4,11 @@
 module Hyrax
   # Generated form for PacificPresentation
   class PacificPresentationForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
-
-    self.model_class = ::PacificPresentation
-    add_terms %i[title alt_title resource_type creator contributor abstract
-                 date_published pagination is_included_in volume publisher issn additional_links rights_holder license
-                 org_unit official_link subject keyword refereed add_info]
-    self.terms -= %i[language rights_statement]
-    self.required_fields = %i[title creator resource_type abstract org_unit]
-
-    include Hyrax::DOI::DOIFormBehavior
     include Hyrax::DOI::DataCiteDOIFormBehavior
 
-    def self.build_permitted_params
-      super.tap do |permitted_params|
-        permitted_params << common_fields
-        permitted_params << [:alt_title, :creator, :contributor, :date_published,
-                             :pagination, :is_included_in, :volume, :publisher,
-                             :issn, :additional_links, :org_unit, :subject, :keyword, :refereed]
-      end
-    end
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:pacific_presentation)
+
+    self.model_class = ::PacificPresentation
   end
 end
--- app/presenters/hyrax/pacific_presentation_presenter.rb
+++ app/presenters/hyrax/pacific_presentation_presenter.rb
@@ -2,13 +2,8 @@
 
 module Hyrax
   class PacificPresentationPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:pacific_presentation)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title alt_title resource_type creator contributor abstract page_display_order_number official_link
-         date_published pagination is_included_in volume publisher issn additional_links rights_holder license
-         org_unit doi subject keyword refereed add_info location].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
