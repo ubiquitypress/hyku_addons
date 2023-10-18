43e214c69dc2bdce3914ef47d08a9703f038c73b
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-10-18 11:46:00 +0100

    REPO-2206 - Convert UNA Time Based Media work type in to schema yaml.

--- app/presenters/hyrax/una_time_based_media_presenter.rb
+++ app/presenters/hyrax/una_time_based_media_presenter.rb
@@ -1,17 +1,8 @@
 # frozen_string_literal: true
 module Hyrax
   class UnaTimeBasedMediaPresenter < Hyrax::WorkShowPresenter
+    include ::HykuAddons::Schema::Presenter(:una_time_based_media)
     include ::HykuAddons::WorkPresenterBehavior
-
-    def self.delegated_methods
-      %i[title resource_type creator alt_email abstract keyword subject
-         org_unit date_published related_identifier additional_links related_material
-         related_url event_title event_location event_date related_exhibition
-         related_exhibition_venue related_exhibition_date license rights_holder
-         rights_statement rights_statement_text contributor duration
-         language location longitude latitude georeferenced time irb_number
-         irb_status add_info].freeze
-    end
     include ::HykuAddons::PresenterDelegatable
   end
 end
