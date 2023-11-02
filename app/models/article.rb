500ae91bbdff4c244455b02c66c117e7fb3316b4
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-02 10:38:20 +0000

    REPO-2207 - Conversion of Article default work type into schema yaml.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,59 +1,19 @@
 # frozen_string_literal: true
 
 class Article < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  include Hyrax::DOI::DOIBehavior
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AltTitleMultiple
-  include ::HykuAddons::AddInfoSingular
-  include ::HykuAddons::FunderProjectRefMultiple
 
-  self.indexer = HykuAddons::ArticleIndexer
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:article)
+
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
+
+  self.indexer = ::ArticleIndexer
 
   validates :title, presence: { message: "Your work must have a title." }
-
-  property :journal_title, predicate: ::RDF::Vocab::BIBO.Journal, multiple: false do |index|
-    index.as :stored_searchable, :facetable
+  def doi_registrar_opts
+    {}
   end
-
-  property :alternative_journal_title, predicate: ::RDF::Vocab::SCHEMA.alternativeHeadline, multiple: true do |index|
-    index.as :stored_searchable
-  end
-
-  property :volume, predicate: ::RDF::Vocab::BIBO.volume do |index|
-    index.as :stored_searchable
-  end
-
-  property :issue, predicate: ::RDF::Vocab::Bibframe.term(:Serial), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :pagination, predicate: ::RDF::Vocab::BIBO.numPages, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :article_num, predicate: ::RDF::Vocab::BIBO.number, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :place_of_publication, predicate: ::RDF::Vocab::BF2.term(:Place) do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :issn, predicate: ::RDF::Vocab::BIBO.issn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :eissn, predicate: ::RDF::Vocab::BIBO.eissn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :refereed, predicate: ::RDF::Vocab::BIBO.term("status/peerReviewed"), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
 end
