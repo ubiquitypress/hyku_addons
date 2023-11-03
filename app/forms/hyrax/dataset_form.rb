5e80b9e1b5decf7e59fb834553f6c92f372162b5
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-03 02:38:19 +0000

    REPO-2207 - Conversion of Dataset default work type into schema yaml.

--- app/forms/hyrax/dataset_form.rb
+++ app/forms/hyrax/dataset_form.rb
@@ -2,17 +2,11 @@
 
 module Hyrax
   class DatasetForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
+    include Hyrax::DOI::DataCiteDOIFormBehavior
+
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:dataset)
 
     self.model_class = ::Dataset
-    add_terms %i[title resource_type creator alt_title contributor rendering_ids abstract date_published
-                 institution org_unit project_name funder fndr_project_ref version_number publisher place_of_publication
-                 date_accepted date_submitted official_link related_url language license rights_statement rights_holder
-                 doi alternate_identifier related_identifier refereed keyword dewey library_of_congress_classification
-                 add_info]
-    self.required_fields = %i[title resource_type creator institution date_published]
-
-    include Hyrax::DOI::DOIFormBehavior
-    include Hyrax::DOI::DataCiteDOIFormBehavior
   end
 end
