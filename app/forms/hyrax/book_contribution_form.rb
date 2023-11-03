3f49ed7e27884fa02343f03adbf5c2f58956f131
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-03 02:12:02 +0000

    REPO-2207 - Conversion of Book Contribution default work type into schema yaml.

--- app/forms/hyrax/book_contribution_form.rb
+++ app/forms/hyrax/book_contribution_form.rb
@@ -2,17 +2,11 @@
 
 module Hyrax
   class BookContributionForm < Hyrax::Forms::WorkForm
-    include ::HykuAddons::WorkForm
+    include Hyrax::DOI::DataCiteDOIFormBehavior
+
+    include ::HykuAddons::Schema::WorkForm
+    include Hyrax::FormFields(:book_contribution)
 
     self.model_class = ::BookContribution
-    add_terms %i[title resource_type creator alt_title contributor rendering_ids abstract date_published
-                 institution org_unit project_name funder fndr_project_ref series_name book_title editor volume edition
-                 publisher place_of_publication isbn issn eissn date_accepted date_submitted official_link related_url
-                 language license rights_statement rights_holder doi alternate_identifier related_identifier refereed
-                 keyword dewey library_of_congress_classification add_info pagination]
-    self.required_fields = %i[title resource_type creator institution date_published]
-
-    include Hyrax::DOI::DOIFormBehavior
-    include Hyrax::DOI::DataCiteDOIFormBehavior
   end
 end
