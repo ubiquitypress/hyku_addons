1bcd3ee83b17d2270f00bc5b0929555c4fd51f65
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-03 03:24:21 +0000

    REPO-2207 - Conversion of Thesis Or Dissertation default work type into schema yaml.

--- app/models/thesis_or_dissertation.rb
+++ app/models/thesis_or_dissertation.rb
@@ -1,48 +1,19 @@
 # frozen_string_literal: true
 
 class ThesisOrDissertation < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  include Hyrax::DOI::DOIBehavior
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AltTitleMultiple
-  include ::HykuAddons::AddInfoSingular
-  include ::HykuAddons::FunderProjectRefMultiple
 
-  property :version, predicate: ::RDF::Vocab::SCHEMA.version do |index|
-    index.as :stored_searchable
-  end
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:thesis_or_dissertation)
 
-  property :version_number, predicate: ::RDF::Vocab::SCHEMA.version do |index|
-    index.as :stored_searchable
-  end
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
 
-  property :pagination, predicate: ::RDF::Vocab::BIBO.numPages, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :refereed, predicate: ::RDF::Vocab::BIBO.term("status/peerReviewed"), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :current_he_institution, predicate: ::RDF::Vocab::SCHEMA.EducationalOrganization, multiple: true do |index|
-    index.as :stored_searchable
-  end
-
-  property :qualification_name, predicate: ::RDF::Vocab::SCHEMA.qualifications, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :qualification_level, predicate: ::RDF::Vocab::BF2.degree, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  self.json_fields += %i[current_he_institution]
-  self.indexer = ThesisOrDissertationIndexer
+  self.indexer = ::ThesisOrDissertationIndexer
 
   validates :title, presence: { message: "Your work must have a title." }
-
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
+  def doi_registrar_opts
+    {}
+  end
 end
