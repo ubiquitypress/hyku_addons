c2acfebf1875061e032e7946a4d7310b4e85853e
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2023-11-03 02:30:47 +0000

    REPO-2207 - Conversion of Conference Item default work type into schema yaml.

--- app/models/conference_item.rb
+++ app/models/conference_item.rb
@@ -1,75 +1,19 @@
 # frozen_string_literal: true
 
 class ConferenceItem < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  include Hyrax::DOI::DOIBehavior
+  include Hyrax::WorkBehavior
   include Hyrax::DOI::DataCiteDOIBehavior
-  include ::HykuAddons::WorkBase
-  include ::HykuAddons::AltTitleMultiple
-  include ::HykuAddons::AddInfoSingular
-  include ::HykuAddons::FunderProjectRefMultiple
 
-  property :series_name, predicate: ::RDF::Vocab::BF2.subseriesOf do |index|
-    index.as :stored_searchable, :facetable
-  end
+  include HykuAddons::Schema::WorkBase
+  include Hyrax::Schema(:conference_item)
 
-  property :book_title, predicate: ::RDF::Vocab::BIBO.term(:Proceedings), multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
+  # Included after other field definitions
+  include Hyrax::BasicMetadata
 
-  property :editor, predicate: ::RDF::Vocab::SCHEMA.Person do |index|
-    index.as :stored_searchable
-  end
+  self.indexer = ::ConferenceItemIndexer
 
-  property :volume, predicate: ::RDF::Vocab::BIBO.volume do |index|
-    index.as :stored_searchable
-  end
-
-  property :event_title, predicate: ::RDF::Vocab::BF2.term(:Event) do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :event_location, predicate: ::RDF::Vocab::Bibframe.eventPlace do |index|
-    index.as :stored_searchable
-  end
-
-  property :event_date, predicate: ::RDF::Vocab::Bibframe.eventDate do |index|
-    index.as :stored_searchable
-  end
-
-  property :pagination, predicate: ::RDF::Vocab::BIBO.numPages, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :place_of_publication, predicate: ::RDF::Vocab::BF2.term(:Place) do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :isbn, predicate: ::RDF::Vocab::BIBO.isbn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :issn, predicate: ::RDF::Vocab::BIBO.issn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :eissn, predicate: ::RDF::Vocab::BIBO.eissn, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :refereed, predicate: ::RDF::Vocab::BIBO.term("status/peerReviewed"), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  self.json_fields += %i[editor]
-  self.date_fields += %i[event_date]
-
-  self.indexer = ConferenceItemIndexer
-  # Change this to restrict which works can be added as a child.
-  # self.valid_child_concerns = []
   validates :title, presence: { message: "Your work must have a title." }
-
-  # This must be included at the end, because it finalizes the metadata
-  # schema (by adding accepts_nested_attributes)
-  include ::Hyrax::BasicMetadata
+  def doi_registrar_opts
+    {}
+  end
 end
