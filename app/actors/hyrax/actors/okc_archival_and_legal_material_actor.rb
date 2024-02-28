0c571da9e2f08dde0c582ddbf6f318313851fdff
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-02-28 16:26:17 +0000

    REPO-2337 - Work type for Okc Archival And Legal Materials

---
+++ app/actors/hyrax/actors/okc_archival_and_legal_material_actor.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# Generated via
+#  `rails generate hyrax:work OkcArchivalAndLegalMaterial`
+module Hyrax
+  module Actors
+    class OkcArchivalAndLegalMaterialActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
