7bcc9c7073f12883edb3caf772feabf47a3964c3
Martin Simpson <martin.simpson@ubiquitypress.com>
2024-04-15 22:05:39 +0700

    Added logging to work file set permissions behavior

--- app/jobs/concerns/hyku_addons/work_file_set_persmissions_behavior.rb
+++ app/jobs/concerns/hyku_addons/work_file_set_persmissions_behavior.rb
@@ -8,26 +8,35 @@ module HykuAddons
     # @param [Array<Hyrax::UploadedFile>] uploaded_files - an array of files to attach
     # rubocop:disable Metrics/MethodLength
     def perform(work, uploaded_files, **work_attributes)
+      puts "[DEBUG - PERM] Entering method"
       validate_files!(uploaded_files)
       depositor = proxy_or_depositor(work)
       user = User.find_by_user_key(depositor)
       work_permissions = work.permissions.map(&:to_hash)
 
+      puts "[DEBUG - PERM] Get Permissions: #{work_permissions}"
       uploaded_files.each do |uploaded_file|
         next if uploaded_file.file_set_uri.present?
 
         actor = Hyrax::Actors::FileSetActor.new(FileSet.create, user)
         file_set_attributes = file_set_attrs(work_attributes, uploaded_file)
         metadata = visibility_attributes(work_attributes, file_set_attributes)
+
+        puts "[DEBUG - PERM] Got metadata #{metadata}"
         uploaded_file.update(file_set_uri: actor.file_set.uri)
+
+        puts "[DEBUG - PERM] Setting Permissions #{work_permissions}"
         actor.file_set.permissions_attributes = work_permissions
         # NOTE: The next line is not included in the upstream PR
         # This line allows the setting of a file's title from a bulkrax import
         actor.file_set.title = Array(file_set_attributes[:title].presence)
         actor.create_metadata(metadata)
         actor.create_content(uploaded_file)
+        puts "[DEBUG - PERM] Attaching to work"
         actor.attach_to_work(work, metadata)
       end
+
+      puts "[DEBUG - PERM] DONE"
     end
     # rubocop:enable Metrics/MethodLength
 
